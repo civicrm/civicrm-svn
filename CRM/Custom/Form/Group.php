603b85e1ae54c3a1d65a23f2009dd17d11117fa9
Dave Greenberg <dave@civicrm.org>
2009-11-03 00:29:57 +0000

    CRM-5143 - fixed missing closing bracket in CRM_Custom_Form_Group::setDefaults - fatal syntax error

--- CRM/Custom/Form/Group.php
+++ CRM/Custom/Form/Group.php
@@ -382,7 +382,8 @@ class CRM_Custom_Form_Group extends CRM_Core_Form
                 $defaults['extends'][1] = $relationshipDefaults;
             }
             
-     
+        }
+        
         return $defaults;
     }
     
