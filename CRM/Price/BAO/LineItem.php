5831e0e337f22b0a1685dc5a6b609bd263381c65
Yashodha Chaku <yashodha.chaku@gmail.com>
2009-11-03 11:11:37 +0000

    #header fixes

--- CRM/Price/BAO/LineItem.php
+++ CRM/Price/BAO/LineItem.php
@@ -1,7 +1,36 @@
 <?php
 
+/*
+ +--------------------------------------------------------------------+
+ | CiviCRM version 3.1                                                |
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC (c) 2004-2009                                |
+ +--------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                    |
+ |                                                                    |
+ | CiviCRM is free software; you can copy, modify, and distribute it  |
+ | under the terms of the GNU Affero General Public License           |
+ | Version 3, 19 November 2007.                                       |
+ |                                                                    |
+ | CiviCRM is distributed in the hope that it will be useful, but     |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+ | See the GNU Affero General Public License for more details.        |
+ |                                                                    |
+ | You should have received a copy of the GNU Affero General Public   |
+ | License along with this program; if not, contact CiviCRM LLC       |
+ | at info[AT]civicrm[DOT]org. If you have questions about the        |
+ | GNU Affero General Public License or the licensing of CiviCRM,     |
+ | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
+ +--------------------------------------------------------------------+
+*/
+
 /**
- Insert copyright here
+ *
+ * @package CRM
+ * @copyright CiviCRM LLC (c) 2004-2009
+ * $Id$
+ *
  */
 
 /**
@@ -18,7 +47,6 @@ require_once 'CRM/Price/DAO/LineItem.php';
  */
 class CRM_Price_BAO_LineItem extends CRM_Price_DAO_LineItem 
 {
-
     /**
      * Creates a new entry in the database.
      *
@@ -134,7 +162,6 @@ WHERE     %2.id = %1";
     {
         if ( empty( $params["price_{$fid}"] ) ) {
             return;
-            
         }
         
         $optionIDs = implode( ',', array_keys( $params["price_{$fid}"] ) );
