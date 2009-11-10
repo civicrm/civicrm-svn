f754e26e13d9d4f81109bf138deb9103e64a6bb9
Kurund Jalmi <kurund@civicrm.org>
2009-11-10 21:48:20 +0000

    more date fixes CRM-4312

--- CRM/Core/SelectValues.php
+++ CRM/Core/SelectValues.php
@@ -605,12 +605,12 @@ class CRM_Core_SelectValues
         $dateInputFormats = array( 
                                   "mm/dd/yy"      => ts('mm/dd/yy (12/31/2009)'),
                                   "yy-mm-dd"      => ts('yy-mm-dd (2009-12-31)'),
+                                  "dd-mm-yy"      => ts('dd-mm-yy (31-12-2009)'),
                                   "M d, yy"       => ts('M d, yy (Dec 31, 2009)'),
                                   "MM d, yy"      => ts('MM d, yy (December 31, 2009)'),
                                   "DD, d MM yy"   => ts('DD, d MM yy (Thursday, 31 December 2009)'),
                                   "mm/dd"         => ts('mm/dd (12/31)'),
                                   "dd/mm"         => ts('dd/mm (31/12)'),
-                                  "dd.mm.yy"      => ts('dd.mm.yy (31.12.2009)')
                                    );
         
         /*
@@ -635,12 +635,12 @@ class CRM_Core_SelectValues
     static function datePluginToPHPFormats( ) {
         $dateInputFormats = array( "mm/dd/yy"      => 'm/d/Y',
                                    "yy-mm-dd"      => 'Y-m-d',
+                                   "dd-mm-yy"      => 'd-m-Y',                                   
                                    "M d, yy"       => 'M j, Y',
                                    "MM d, yy"      => 'F j, Y',
                                    "DD, d MM yy"   => 'l, j F Y',                                   
                                    "mm/dd"         => 'm/d',
                                    "dd/mm"         => 'd/m',
-                                   "dd.mm.yy"      => 'd.m.Y'                                   
                                   );
         return $dateInputFormats;
     }
