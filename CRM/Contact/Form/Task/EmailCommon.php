828a8b936182f9c54d0433474fc3ef9b4379e6db
Kurund Jalmi <kurund@civicrm.org>
2009-11-11 14:16:57 +0000

    notice fixes

--- CRM/Contact/Form/Task/EmailCommon.php
+++ CRM/Contact/Form/Task/EmailCommon.php
@@ -147,7 +147,7 @@ class CRM_Contact_Form_Task_EmailCommon
         
             foreach ( $form->_contactIds as $key => $contactId ) {
                 $value = $form->_contactDetails[$contactId];
-                if ( $value['do_not_email'] || empty( $value['email'] ) || $value['is_deceased'] || $value['on_hold'] ) {
+                if ( $value['do_not_email'] || empty( $value['email'] ) || CRM_Utils_Array::value( 'is_deceased', $value ) || $value['on_hold'] ) {
                     $suppressedEmails++;
 
                     // unset contact details for contacts that we won't be sending email. This is prevent extra computation 
