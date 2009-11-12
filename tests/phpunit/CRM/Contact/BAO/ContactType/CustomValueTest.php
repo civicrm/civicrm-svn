f904a2f4a398ebad6eea77ea96e3aeaa3ac17c3a
rahulbile <rahulbile@civicrm.org>
2009-11-12 12:21:06 +0000

    // CRM-5341

--- tests/phpunit/CRM/Contact/BAO/ContactType/CustomValueTest.php
+++ tests/phpunit/CRM/Contact/BAO/ContactType/CustomValueTest.php
@@ -23,14 +23,14 @@ class CRM_Contact_BAO_ContactType_CustomValueTest extends CiviUnitTestCase
     {
         
         parent::setUp();
-       
+        
         //  Create Group For Individual  Contact Type
         $groupIndividual   = array(
-                          'title'       => 'TestGroup For Individual',
-                          'name'        => 'testGroupIndividual',
-                          'extends'     => 'individual',
-                          'style'       => 'Inline',
-                          'is_active'   => 1
+                                   'title'       => 'TestGroup For Individual',
+                                   'name'        => 'testGroupIndividual',
+                                   'extends'     => 'individual',
+                                   'style'       => 'Inline',
+                                   'is_active'   => 1
                           );
         $this->CustomGroupIndividual = Custom::createGroup($groupIndividual );
         
@@ -43,31 +43,31 @@ class CRM_Contact_BAO_ContactType_CustomValueTest extends CiviUnitTestCase
                          'is_required'     => 1,
                          'is_searchable'   => 0,
                          'is_active'       => 1
-                         );
+                        );
         
         $this->IndividualField = Custom::createField($params );
         
         //  Create Group For Individual-Student  Contact Sub  Type
         $groupIndiStudent   = array(
-                          'title'       => 'TestGroup For Individual - Student',
-                          'name'        => 'testGroupIndividualStudent',
-                          'extends'     => 'individual',
-                          'extends_entity_column_value' => CRM_Core_DAO::VALUE_SEPARATOR.'Student'.CRM_Core_DAO::VALUE_SEPARATOR,
-                          'style'       => 'Inline',
-                          'is_active'   => 1
-                          );
+                                    'title'       => 'TestGroup For Individual - Student',
+                                    'name'        => 'testGroupIndividualStudent',
+                                    'extends'     => 'individual',
+                                    'extends_entity_column_value' => CRM_Core_DAO::VALUE_SEPARATOR.'Student'.CRM_Core_DAO::VALUE_SEPARATOR,
+                                    'style'       => 'Inline',
+                                    'is_active'   => 1
+                                    );
         $this->CustomGroupIndiStudent = Custom::createGroup($groupIndiStudent );
         
         $params = array(
-                         'custom_group_id' => $this->CustomGroupIndiStudent->id,
-                         'label'           => 'Individual-Student College',
-                         'html_type'       => 'Text',
-                         'data_type'       => 'String',
-                         'weight'          => 4,
-                         'is_required'     => 1,
+                        'custom_group_id' => $this->CustomGroupIndiStudent->id,
+                        'label'           => 'Individual-Student College',
+                        'html_type'       => 'Text',
+                        'data_type'       => 'String',
+                        'weight'          => 4,
+                        'is_required'     => 1,
                          'is_searchable'   => 0,
-                         'is_active'       => 1
-                         );
+                        'is_active'       => 1
+                        );
         
         $this->IndiStudentField = Custom::createField($params );
         
@@ -76,13 +76,20 @@ class CRM_Contact_BAO_ContactType_CustomValueTest extends CiviUnitTestCase
                          'contact_type' => 'Individual',
                          );
         $this->individual = Contact::create( $params );
-
+        
         $params = array( 'first_name'   => 'Steve',     
                          'last_name'    => 'Tosun',
                          'contact_type' => 'Individual',
                          'contact_sub_type' => 'Student'
                          );
         $this->individualStudent = Contact::create( $params );
+        
+        $params = array( 'first_name'   => 'Mark',     
+                         'last_name'    => 'Dawson',
+                         'contact_type' => 'Individual',
+                         'contact_sub_type' => 'Parent'
+                         );
+        $this->individualParent = Contact::create( $params );
         
         $params = array( 'organization_name' => 'Wellspring' ,     
                          'contact_type'      => 'Organization',
@@ -102,15 +109,15 @@ class CRM_Contact_BAO_ContactType_CustomValueTest extends CiviUnitTestCase
     function testAddIndividualCustomDataToOrganization() {
         
         $params = array(
-                         'contact_id'           => $this->organization ,
-                         "custom_{$this->IndividualField->id}" => 'Test String',  
+                        'contact_id'           => $this->organization ,
+                        "custom_{$this->IndividualField->id}" => 'Test String',  
                         );
-       
+        
         $contact =& civicrm_contact_add( $params );
-        $this->assertNull( $contact , 'In line '. __LINE__ );
+        $this->assertEquals( $contact['error_message'], 'Invalid Custom Field Contact Type: Organization' );
     }
-
-
+    
+    
     /**
      * Add valid  Empty params to a Contact Type : Individual
      */ 
@@ -120,10 +127,9 @@ class CRM_Contact_BAO_ContactType_CustomValueTest extends CiviUnitTestCase
         $contact =& civicrm_contact_add( $params );
         $this->assertEquals( $contact['is_error'], 1 );
         $this->assertEquals( $contact['error_message'], 'Contact Type not specified' );
-        
     }
 
-  
+    
     /**
      * Add valid custom data to a Contact Type : Individual
      */ 
@@ -154,7 +160,7 @@ class CRM_Contact_BAO_ContactType_CustomValueTest extends CiviUnitTestCase
                         );
         
         $contact =& civicrm_contact_add( $params );
-        $this->assertNull( $contact , 'In line '. __LINE__ );
+        $this->assertEquals( $contact['error_message'], 'Invalid Custom Field Contact Type: Organization or Mismatched SubType: Sponsor.' );
     }
     
     /**
@@ -172,12 +178,12 @@ class CRM_Contact_BAO_ContactType_CustomValueTest extends CiviUnitTestCase
         $this->assertNotNull( $contact['contact_id'] , 'In line '. __LINE__ );
         $entityValues =  CRM_Core_BAO_CustomValueTable::getEntityValues( $this->individualStudent);
         $elements["custom_{$this->IndiStudentField->id}"] = $entityValues["{$this->IndiStudentField->id}"];
-       
+        
         // Check the Value in Database 
         $this->assertEquals( $elements["custom_{$this->IndiStudentField->id}"], 'Test String' );
     }
     
-
+    
     /**
      * Add custom data(of Individual Student)to a Contact Type : Individual  
      */ 
@@ -189,38 +195,46 @@ class CRM_Contact_BAO_ContactType_CustomValueTest extends CiviUnitTestCase
                         );
         
         $contact =& civicrm_contact_add( $params );
-        
-        $this->assertNull( $contact['contact_id'] , 'In line '. __LINE__ );
-       
+        $this->assertEquals( $contact['error_message'], 'Invalid Custom Field Contact Type: Individual' );
     }
     
+    /**
+     * Add custom data of Individual Student to a Contact Type : Individual - parent   
+     */ 
+    function testAddIndividualStudentCustomDataToIndividualParent() {
+        
+        $params = array(
+                        'contact_id'           => $this->individualParent ,
+                        "custom_{$this->IndiStudentField->id}" => 'Test String',
+                        );
+        
+        $contact =& civicrm_contact_add( $params );
+        $this->assertEquals( $contact['error_message'], 'Invalid Custom Field Contact Type: Individual or Mismatched SubType: Parent.' );
+    }
+    
+    
+    
     // Retrieve Methods
-
+    
     /**
      * Retrieve Valid custom Data added to  Individual Contact Type
      */
     
     function testRetrieveValidCustomDataToIndividual() {
         
-        
         $params = array(
                         'contact_id'           => $this->individual ,
                         "custom_{$this->IndividualField->id}" => 'Test String',  
                         );
-       
         $contact =& civicrm_contact_add( $params );
-       
-        $params = array(  'contact_id'           => $this->individual ,
-                          "return.custom_{$this->IndividualField->id}"  => 1
-                           );
-        
+        $params = array( 
+                        'contact_id'           => $this->individual ,
+                        "return.custom_{$this->IndividualField->id}"  => 1
+                         );
         $getContact = civicrm_contact_get( $params );
-        
         $this->assertEquals( $getContact[$this->individual][ "custom_{$this->IndividualField->id}"], 'Test String' );
-       
     }
-
-
+    
     /**
      * Retrieve Valid custom Data added to  Individual Contact Type , Subtype : Student.
      */
@@ -231,22 +245,15 @@ class CRM_Contact_BAO_ContactType_CustomValueTest extends CiviUnitTestCase
                         'contact_id'           => $this->individualStudent ,
                         "custom_{$this->IndiStudentField->id}" => 'Test String',  
                         );
-        
         $contact =& civicrm_contact_add( $params );
-       
-        $params = array(  'contact_id'           => $this->individualStudent ,
-                          "return.custom_{$this->IndiStudentField->id}"  => 1
-                           );
-        
+        $params = array(  
+                        'contact_id'           => $this->individualStudent ,
+                        "return.custom_{$this->IndiStudentField->id}"  => 1
+                          );
         $getContact = civicrm_contact_get( $params );
         $this->assertEquals( $getContact[$this->individualStudent][ "custom_{$this->IndiStudentField->id}"], 'Test String' );
-       
     }
     
-
-
 }
-
-
 
 ?>
