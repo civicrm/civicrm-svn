e7c674ad7ad399b868b63ed909c44edc76ed1f6d
shailesh <shailesh@civicrm.org>
2009-12-03 06:22:40 +0000

    //CRM-5475 //minor fix of button type

--- CRM/Event/Form/Task/Print.php
+++ CRM/Event/Form/Task/Print.php
@@ -82,7 +82,7 @@ class CRM_Event_Form_Task_Print extends CRM_Event_Form_Task
         // just need to add a javacript to popup the window for printing
         // 
         $this->addButtons( array(
-                                 array ( 'type'      => 'next',
+                                 array ( 'type'      => 'back',
                                          'name'      => ts('Print Participant List'),
                                          'js'        => array( 'onclick' => 'window.print()' ),
                                          'isDefault' => true   ),
