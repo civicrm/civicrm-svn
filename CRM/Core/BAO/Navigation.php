56cf531f7d0b7875420aac5a665fca12f49d8cbd
Kurund Jalmi <kurund@civicrm.org>
2009-12-10 09:23:09 +0000

    minor fixes, CRM-5523

--- CRM/Core/BAO/Navigation.php
+++ CRM/Core/BAO/Navigation.php
@@ -93,7 +93,9 @@ class CRM_Core_BAO_Navigation extends CRM_Core_DAO_Navigation {
         require_once "CRM/Core/DAO/Navigation.php";
         $navigation  =& new CRM_Core_DAO_Navigation( );
         
-        $params['is_active'] = CRM_Utils_Array::value( 'is_active', $params, false );
+        $params['is_active'    ] = CRM_Utils_Array::value( 'is_active', $params, false );
+        $params['has_separator'] = CRM_Utils_Array::value( 'has_separator', $params, false );
+        
         if ( !isset( $params['id'] ) || 
              ( $params['parent_id'] !=  CRM_Utils_Array::value( 'current_parent_id', $params ) ) ) {
             /* re/calculate the weight, if the Parent ID changed OR create new menu */
@@ -341,7 +343,7 @@ ORDER BY parent_id, weight";
                 $navigationString .= '<ul>';  
             } else {
                 $navigationString .= '</li>';
-                if ( isset( $value['attributes']['separator'] ) ) {
+                if ( isset( $value['attributes']['separator'] ) && $value['attributes']['separator'] ) {
                     $navigationString .= '<li class="menu-separator"></li>';
                 } 
             }
