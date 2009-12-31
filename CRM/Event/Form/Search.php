41ab02dd92157cbfd639d849f55e2c67729872ff
Deepak Srivastava <deepak.srivastava.0303@gmail.com>
2009-12-31 10:59:52 +0000

    svn merge -r 25617:25734 http://svn.civicrm.org/civicrm/branches/v3.0
    includes CRM-3897, CRM-5589 work / fixes.

--- CRM/Event/Form/Search.php
+++ CRM/Event/Form/Search.php
@@ -334,6 +334,10 @@ class CRM_Event_Form_Search extends CRM_Core_Form
             $this->_formValues = $this->controller->exportValues($this->_name);
         } 
         
+        if ( ! empty( $_POST ) ) {
+            $this->_formValues = $this->controller->exportValues($this->_name);
+        }
+        
         if ( empty(  $this->_formValues ) ) {       
             $this->_formValues = $this->controller->exportValues($this->_name);
         }
