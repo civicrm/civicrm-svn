6a357ed14db049140af6526febffc3ca9a8f56ce
amit <amit@civicrm.org>
2010-01-05 13:48:37 +0000

    #CRM-5611 #fixes for default data / default Custom Group would be collapsed

--- CRM/Custom/Form/Group.php
+++ CRM/Custom/Form/Group.php
@@ -385,8 +385,8 @@ class CRM_Custom_Form_Group extends CRM_Core_Form
         if ($this->_action == CRM_Core_Action::ADD) {
             $defaults['weight'] = CRM_Utils_Weight::getDefaultWeight('CRM_Core_DAO_CustomGroup');
 
-            $defaults['is_multiple'] = $defaults['min_multiple'] = 0;
-            $defaults['is_active']   = 1;
+            $defaults['is_multiple'] = $defaults['min_multiple']     = 0;
+            $defaults['is_active']   = $defaults['collapse_display'] = 1;
             $defaults['style']       = 'Inline';
         }
 
