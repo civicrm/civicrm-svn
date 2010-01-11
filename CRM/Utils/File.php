3d16a6efc23d68bb4cc9cd3bb27d08870ad607c4
Donald Lobo <lobo@civicrm.org>
2010-01-11 22:30:39 +0000

    add period to delimiter between extension and filename

--- CRM/Utils/File.php
+++ CRM/Utils/File.php
@@ -281,7 +281,7 @@ class CRM_Utils_File {
             // We'll truncate at 240 to give some room for the extension.
             return CRM_Utils_String::munge( "{$basename}_". CRM_Utils_Array::value( 'extension', $info ) . "_{$uniqID}", '_',  240 ) . ".unknown";
         } else {
-            return CRM_Utils_String::munge( "{$basename}_{$uniqID}", '_',  240 ) . CRM_Utils_Array::value( 'extension', $info );
+            return CRM_Utils_String::munge( "{$basename}_{$uniqID}", '_',  240 ) . "." . CRM_Utils_Array::value( 'extension', $info );
         }
     }
 
