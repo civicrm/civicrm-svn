0811babbf381efb0a1c0b8e3dea6f89fc3f06c3e
neha <neha@civicrm.org>
2010-01-19 13:16:55 +0000

    // Added hook_civicrm_searchTasks() for Grant, CRM-5459

---
+++ CRM/Grant/Form/Task/SearchTaskHookSample.php
@@ -0,0 +1,96 @@
+<?php
+
+/*
+ +--------------------------------------------------------------------+
+ | CiviCRM version 3.1                                                |
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC (c) 2004-2009                                |
+ +--------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                    |
+ |                                                                    |
+ | CiviCRM is free software; you can copy, modify, and distribute it  |
+ | under the terms of the GNU Affero General Public License           |
+ | Version 3, 19 November 2007.                                       |
+ |                                                                    |
+ | CiviCRM is distributed in the hope that it will be useful, but     |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+ | See the GNU Affero General Public License for more details.        |
+ |                                                                    |
+ | You should have received a copy of the GNU Affero General Public   |
+ | License along with this program; if not, contact CiviCRM LLC       |
+ | at info[AT]civicrm[DOT]org. If you have questions about the        |
+ | GNU Affero General Public License or the licensing of CiviCRM,     |
+ | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
+ +--------------------------------------------------------------------+
+*/
+
+/**
+ *
+ * @package CRM
+ * @copyright CiviCRM LLC (c) 2004-2009
+ * $Id$
+ *
+ */
+
+require_once 'CRM/Grant/Form/Task.php';
+
+/**
+ * This class provides the functionality to save a search
+ * Saved Searches are used for saving frequently used queries
+ */
+class CRM_Grant_Form_Task_SearchTaskHookSample extends CRM_Grant_Form_Task 
+{
+    /**
+     * build all the data structures needed to build the form
+     *
+     * @return void
+     * @access public
+     */
+    function preProcess( ) 
+    {
+        parent::preProcess( );
+        $rows = array( );
+        // display name and email of all contact ids
+        $grantIDs = implode( ',', $this->_grantIds );      
+        
+        $query = "
+SELECT ct.display_name   as display_name,
+       cg.decision_date  as decision_date,
+       cg.amount_total   as amount_total,
+       cg.amount_granted as amount_granted      
+FROM   civicrm_contact ct,
+       civicrm_grant cg
+WHERE  cg.contact_id = ct.id
+AND    cg.id IN ( $grantIDs )";
+        
+        $dao = CRM_Core_DAO::executeQuery( $query, CRM_Core_DAO::$_nullArray );
+        
+        while ( $dao->fetch( ) ) {
+            $rows[]= array(
+                           'display_name'     =>  $dao->display_name,
+                           'decision_date'    =>  $dao->decision_date,
+                           'amount_requested' =>  $dao->amount_total,
+                           'amount_granted'   =>  $dao->amount_granted
+                           
+                           );
+        }
+        $this->assign( 'rows', $rows );
+    }
+    
+    /**
+     * Function to actually build the form
+     *
+     * @return None
+     * @access public
+     */
+    public function buildQuickForm( ) 
+    {
+        $this->addButtons( array(
+                                 array ( 'type'      => 'done',
+                                         'name'      => ts('Done'),
+                                         'isDefault' => true   ),
+                                 )
+                           );
+    }
+}
