8546539c41dde08d1685213c2e9918c56f2279f7
neha <neha@civicrm.org>
2010-01-20 08:49:52 +0000

    // Added hook_civicrm_searchTasks() for CiviCase, CRM-5459

--- CRM/Case/StateMachine/Search.php
+++ CRM/Case/StateMachine/Search.php
@@ -99,28 +99,7 @@ class CRM_Case_StateMachine_Search extends CRM_Core_StateMachine
         }
         $this->_controller->set( 'task', $value );
         
-        $result = false;
-
-        switch ( $value ) {
-        case CRM_Case_Task::DELETE_CASES:
-            $task   = 'CRM_Case_Form_Task_Delete';
-            break;
-            
-        case CRM_Case_Task::EXPORT_CASES:
-            $task   = array('CRM_Export_Form_Select',
-                            'CRM_Export_Form_Map');
-            break;
-
-        case CRM_Case_Task::RESTORE_CASES:
-            $task   = 'CRM_Case_Form_Task_Restore';
-            break;
-            
-        default: // the print task is the default and catch=all task
-            $task = 'CRM_Case_Form_Task_Print';
-            break;
-        }
-        
-        return array( $task, $result );
+        return CRM_Case_Task::getTask( $value );
     }
     
     /**
