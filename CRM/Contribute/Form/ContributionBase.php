e73787dcb4131c69aed2a9ebf9940f2208788eb9
Kurund Jalmi <kurund@civicrm.org>
2010-02-05 07:57:00 +0000

    worked on CRM-5759

--- CRM/Contribute/Form/ContributionBase.php
+++ CRM/Contribute/Form/ContributionBase.php
@@ -227,6 +227,7 @@ class CRM_Contribute_Form_ContributionBase extends CRM_Core_Form
                         if ( array_key_exists($membership->contact_id, $employers) ) {
                             $this->_membershipContactID = $membership->contact_id;
                             $this->assign('membershipContactID', $this->_membershipContactID);
+                            $this->assign('membershipContactName', $employers[$this->_membershipContactID]['name']);
                         } else {
                             CRM_Core_Session::setStatus( ts("Oops. The membership you're trying to renew appears to be invalid. Contact your site administrator if you need assistance. If you continue, you will be issued a new membership.") );
                         }
