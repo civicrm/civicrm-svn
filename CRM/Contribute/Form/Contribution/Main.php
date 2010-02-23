f69125df77131cbfbaee52636224424b381d429f
Donald Lobo <lobo@civicrm.org>
2010-02-23 19:31:19 +0000

    fix CRM-5879

--- CRM/Contribute/Form/Contribution/Main.php
+++ CRM/Contribute/Form/Contribution/Main.php
@@ -900,7 +900,7 @@ class CRM_Contribute_Form_Contribution_Main extends CRM_Contribute_Form_Contribu
             $params['amount_other'] = CRM_Utils_Rule::cleanMoney( $params['amount_other'] );
         }
         
-        if ( CRM_Utils_Array::value('amount',$params) == 'amount_other_radio' || ! empty( $params['amount_other'] ) ) {
+        if ( CRM_Utils_Array::value('amount',$params) == 'amount_other_radio' || ! isset( $params['amount_other'] ) ) {
             $amount = $params['amount_other'];
         } else if  ( !empty( $params['pledge_amount'] ) ) {
             $amount = 0;
