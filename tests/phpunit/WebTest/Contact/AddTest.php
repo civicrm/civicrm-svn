6e6b4e69ece3148e7c434423422442ad7e017d77
mover <mover@civicrm.org>
2010-03-09 15:52:51 +0000

    CRM-5548 work in progress

---
+++ tests/phpunit/WebTest/Contact/AddTest.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+ +--------------------------------------------------------------------+
+ | CiviCRM version 3.1                                                |
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC (c) 2004-2009                                |
+ +--------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                    |
+ |                                                                    |
+ | CiviCRM is free software; you can copy, modify, and distribute it  |
+ | under the terms of the GNU Affero General Public License           |
+ | Version 3, 19 November 2007 and the CiviCRM Licensing Exception.   |
+ |                                                                    |
+ | CiviCRM is distributed in the hope that it will be useful, but     |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+ | See the GNU Affero General Public License for more details.        |
+ |                                                                    |
+ | You should have received a copy of the GNU Affero General Public   |
+ | License along with this program; if not, contact CiviCRM LLC       |
+ | at info[AT]civicrm[DOT]org. If you have questions about the        |
+ | GNU Affero General Public License or the licensing of CiviCRM,     |
+ | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
+ +--------------------------------------------------------------------+
+*/
+
+require_once 'CiviTest/CiviSeleniumTestCase.php';
+
+
+ 
+class WebTest_Contact_AddTest extends CiviSeleniumTestCase {
+
+  protected $captureScreenshotOnFailure = TRUE;
+  protected $screenshotPath = '/var/www/api.dev.civicrm.org/public/sc';
+  protected $screenshotUrl = 'http://api.dev.civicrm.org/sc/';
+    
+  protected function setUp()
+  {
+      parent::setUp();
+  }
+
+  function testIndividualAdd()
+  {
+
+      $this->open("/drupal/");
+
+      $this->type("edit-name", "demo");
+      $this->type("edit-pass", "demo");
+      $this->click("edit-submit");
+      $this->waitForPageToLoad("30000");
+
+      // Go directly to the URL of the screen that you wiwll be testing.
+      $this->open("/drupal/civicrm/contact/add?ct=Individual&reset=1");
+
+  }
+
+}
+?>
