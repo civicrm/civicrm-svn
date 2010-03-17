6b2b4723ec2ced48fc8437e30b0e818eab4a3eb8
queue <queue@civicrm.org>
2010-03-17 19:46:25 +0000

    CRM-5942 - IE fix (graceful degradation - but tooltip pops up in the right place at least, and then stay visible on scroll)

--- packages/jquery/plugins/jquery.toolTip.js
+++ packages/jquery/plugins/jquery.toolTip.js
@@ -9,6 +9,16 @@
 		$("#crm-toolTip").remove();
 		if ( clickedElement == $(this).children().attr('id') ) { clickedElement = null; return; }
 		 $("body").append('<div id="crm-toolTip" style="z-index: 100;"><div id="hide-tooltip" class="ui-icon ui-icon-close"></div>' + text + "</div>");
+		  if ($.browser.msie && $.browser.version.substr(0,1)<7) {
+		  	$("#crm-toolTip").css('position','absolute');
+			$(window).bind('scroll', function() {
+				var windowheight = $(window).height();
+				var toolTipBottom = $(window).scrollTop() + 30;
+				var posFromTop = windowheight+toolTipBottom;
+				$("#crm-toolTip").css("top", toolTipBottom + "px");
+				});
+			};
+		
 		  $("#crm-toolTip").fadeIn("medium");
 		  clickedElement = cj(this).children().attr('id');
 	      })
