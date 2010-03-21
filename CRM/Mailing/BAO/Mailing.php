3161bcea4ce7ca69e022cd65f0fcbce2d4a5b713
Xavier Dutoit <xavier@tttp.eu>
2010-03-21 11:12:17 +0000

    CRM-5957 Renaming X-VERP as suggested by Piotr

--- CRM/Mailing/BAO/Mailing.php
+++ CRM/Mailing/BAO/Mailing.php
@@ -1057,7 +1057,7 @@ AND    civicrm_mailing.id = civicrm_mailing_job.mailing_id";
         $headers['To'] = $recipient;
         $headers['Precedence'] = 'bulk';
         //Will test in the mail processor if the X-VERP is set in the bounced email. (As an option to replace real VERP for those that can't set it up)
-        $headers['X-VERP'] = $verp['bounce'];
+        $headers['X-CiviMail-Bounce'] = $verp['bounce'];
 
         //CRM-5058
         //token replacement of subject
