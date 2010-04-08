128c991d13f5f53742c49059ee71b93fb1b82231
Piotr Szotkowski <chastell@chastell.net>
2010-04-08 14:13:45 +0000

    v3.1: ts()-tagging fix

--- CRM/Core/Config.php
+++ CRM/Core/Config.php
@@ -414,7 +414,7 @@ class CRM_Core_Config extends CRM_Core_Config_Variables
                     // CRM-4949
                     $value = $this->templateCompileDir;
                     $url = CRM_Utils_System::url( 'civicrm/admin/setting/path', 'reset=1' );
-                    CRM_Core_Session::setStatus( ts( "%1 has an incorrect directory path. Please go to the <a href=\"$url\">path setting page</a> and correct it.", array( 1 => $key ) ) . '<br/>' );
+                    CRM_Core_Session::setStatus(ts('%1 has an incorrect directory path. Please go to the <a href="%2">path setting page</a> and correct it.', array(1 => $key, 2 => $url)) . '<br/>');
                 }
             } else if ( $key == 'lcMessages' ) {
                 // reset the templateCompileDir to locale-specific and make sure it exists
