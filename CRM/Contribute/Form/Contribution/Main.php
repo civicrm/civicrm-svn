d98fee8ef81de64c2f480c58f2bc26f41986169e
rahulbile <rahulbile@civicrm.org>
2010-04-22 06:32:58 +0000

    // minor fix, CRM-6122

--- CRM/Contribute/Form/Contribution/Main.php
+++ CRM/Contribute/Form/Contribution/Main.php
@@ -455,7 +455,7 @@ class CRM_Contribute_Form_Contribution_Main extends CRM_Contribute_Form_Contribu
         }
 
         if ( $separateMembershipPayment ) {
-            $elements[''] = $this->createElement('radio',null,null,ts('No thank you'),'no_thanks', null);
+            $elements[''] = $this->createElement('radio',null,null,ts('No thank you'),'no_thanks', array('onclick'=>'clearAmountOther();') );
             $this->assign('is_separate_payment', true); 
         }
 
