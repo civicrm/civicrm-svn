aa4d26fd06773e04abf624577cbb4335f0dd2449
Xavier Dutoit <xavier@tttp.eu>
2010-04-25 00:36:43 +0000

    Fix the compatibility regression with 3.1 (by default used_for == Contacts)

--- api/v2/Tag.php
+++ api/v2/Tag.php
@@ -61,7 +61,10 @@ function civicrm_tag_create( &$params )
     if ( empty( $params ) ) {
         return civicrm_create_error( ts( 'No input parameters present' ) );
     }
-    
+   
+    if ( !array_key_exists ('used_for', $params)) {
+      $params ['used_for'] = "civicrm_contact";
+    }
     $error = _civicrm_check_required_fields($params, 'CRM_Core_DAO_Tag');
     
     if ( $error['is_error'] ) {
