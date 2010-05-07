027abccd89034d568a6968db598c3167b5bf4f9b
neha <neha@civicrm.org>
2010-05-07 13:04:13 +0000

    //Household contact webtest

--- tests/phpunit/WebTest/Contact/AddTest.php
+++ tests/phpunit/WebTest/Contact/AddTest.php
@@ -40,7 +40,7 @@ class WebTest_Contact_AddTest extends CiviSeleniumTestCase {
       parent::setUp();
   }
 
-  function testIndividualAdd()
+  function testIndividualAdd( )
   {
       // This is the path where our testing install resides. 
       // The rest of URL is defined in CiviSeleniumTestCase base class, in
@@ -168,6 +168,109 @@ class WebTest_Contact_AddTest extends CiviSeleniumTestCase {
       $this->waitForPageToLoad("30000");
       
       $this->assertTrue($this->isTextPresent("Your Individual contact record has been saved."));
+  }  
+
+  function testHouseholdAdd( )
+  {
+      // This is the path where our testing install resides. 
+      // The rest of URL is defined in CiviSeleniumTestCase base class, in
+      // class attributes.
+      $this->open( $this->sboxPath );
+      
+      // Logging in. Remember to wait for page to load. In most cases,
+      // you can rely on 30000 as the value that allows your test to pass, however,
+      // sometimes your test might fail because of this. In such cases, it's better to pick one element
+      // somewhere at the end of page and use waitForElementPresent on it - this assures you, that whole
+      // page contents loaded and you can continue your test execution.
+      $this->webtestLogin( );
+      
+      // Go directly to the URL of the screen that you will be testing (New Household).
+      $this->open($this->sboxPath . "civicrm/contact/add&reset=1&ct=Household");
+      
+      //contact details section
+      //fill in Household name
+      $this->click("household_name");
+      $this->type("household_name", "Fraddie Grant's home");
+      
+      //fill in nick name
+      $this->type("nick_name", "Grant's home");
+
+      //fill in email
+      $this->type("email_1_email", "fraddiegrantshome@web.com");
+      $this->click("Email_1_IsBulkmail");
+      
+      //fill in phone
+      $this->type("phone_1_phone", "444-4444");
+      $this->select("phone_1_phone_type_id", "value=4");
+      
+      
+      //fill in IM
+      $this->assertTrue($this->isTextPresent("Yahoo MSN AIM GTalk Jabber Skype"));
+      $this->type("im_1_name", "testSkype");
+      $this->select("im_1_location_type_id", "value=3");
+      $this->select("im_1_provider_id", "value=6");
+      
+      //fill in openID
+      $this->type("openid_1_openid", "http://www.grantshomeopenid.com");
+      
+      //fill in website url
+      $this->type("website_1_url", "http://www.fraddiegrantshome.com");
+      
+      //fill in contact source
+      $this->type("contact_source", "Grant's home source");
+      
+      //fill in external identifier
+      $this->type("external_identifier", "household_webtest");
+
+      //check for duplicate contact
+      $this->click("_qf_Contact_refresh_dedupe");
+      $this->waitForPageToLoad("30000");
+      
+      //address section
+      $this->click("//form[@id='Contact']/div[2]/div[4]/div[1]");
+      $this->type("address_1_street_address", "938U Bay Rd E");
+      $this->type("address_1_city", "Birmingham");
+      $this->type("address_1_postal_code", "35278");
+      $this->assertTrue($this->isTextPresent("Country\n - select - United States"));
+      $this->select("address_1_state_province_id", "label=New Mexico");
+      $this->type("address_1_geo_code_1", "5647");
+      $this->type("address_1_geo_code_2", "2843");
+      
+      
+      //Communication Preferences section
+      $this->click("//form[@id='Contact']/div[2]/div[5]/div[1]/div");
+      
+      //select greeting/addressee options
+      $this->select("addressee_id", "label=Customized");
+      $this->type("addressee_custom", "Grant's home");
+      
+      //Select preferred method(s) of communication
+      $this->click("preferred_communication_method[1]");
+      $this->click("preferred_communication_method[2]");
+      $this->click("preferred_communication_method[5]");
+      
+      //Select preferred method for Privacy
+      $this->click("privacy[do_not_sms]");
+      
+      //select preferred language
+      $this->select("preferred_language", "label=French");
+      
+      
+      //Notes section
+      $this->click("//form[@id='Contact']/div[2]/div[6]/div[1]");
+      $this->type("subject", "Grant's note");
+      $this->type("note", "This is a household contact webtest note.");
+      
+      //Tags and Groups section
+      $this->click("//form[@id='Contact']/div[2]/div[7]/div[1]/div");
+      $this->click("group[3]");
+      $this->click("tag[1]");
+      
+      // Clicking save.
+      $this->click("_qf_Contact_upload_view");
+      $this->waitForPageToLoad("30000");
+      
+      $this->assertTrue($this->isTextPresent("Your Household contact record has been saved."));    
   }
   
 }
