73ed7a3f6b36db181958344245cc81f99ebb5719
Donald Lobo <lobo@civicrm.org>
2010-05-24 18:09:55 +0000

    misc fixes

--- CRM/Core/Config.php
+++ CRM/Core/Config.php
@@ -584,7 +584,7 @@ class CRM_Core_Config extends CRM_Core_Config_Variables
                           'UPDATE civicrm_group SET cache_date = NULL',
                           'TRUNCATE TABLE civicrm_group_contact_cache',
                           'TRUNCATE TABLE civicrm_menu',
-                          'UPDATE civicrm_preferences SET navigation = NULL',
+                          'UPDATE civicrm_preferences SET navigation = NULL WHERE contact_id IS NOT NULL',
                           );
 
         foreach ( $queries as $query ) {
