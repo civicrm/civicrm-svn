546bd1004ed96af24437802c5f0666e620cb16da
Piotr Szotkowski <chastell@chastell.net>
2010-05-25 18:22:54 +0000

    CRM-6296 works

--- tests/phpunit/WebTest/Contribute/ContributionPageAddTest.php
+++ tests/phpunit/WebTest/Contribute/ContributionPageAddTest.php
@@ -43,12 +43,42 @@ class WebTest_Contribute_ContributionPageAddTest extends CiviSeleniumTestCase {
 
         // fill in step 1 (Title and Settings)
         $this->type('title', "Title $hash");
+        $this->select('contribution_type_id', 'value=1');
+
+        $this->click('is_organization');
+        $this->type('for_organization', "On behalf $hash");
+        $this->click('CIVICRM_QFID_2_Require');
+
+        // FIXME: handle Introductory/Footer Message
+
+        $this->type('goal_amount', 10 * $rand);
+
+        // FIXME: handle Start/End Date/Time
+
+        $this->click('honor_block_is_active');
+        $this->type('honor_block_title', "Honoree Section Title $hash");
+        $this->type('honor_block_text',  "Honoree Introductory Message $hash");
 
         // go to step 2
         $this->click('_qf_Settings_next');
         $this->waitForPageToLoad();
 
         // fill in step 2 (Amounts)
+        $this->click('is_pay_later');
+        $this->type('pay_later_text',    "Pay later label $hash");
+        $this->type('pay_later_receipt', "Pay later instructions $hash");
+
+        $this->click('is_pledge_active');
+        $this->click('pledge_frequency_unit[week]');
+        $this->click('is_pledge_interval');
+        $this->type('initial_reminder_day',    3);
+        $this->type('max_reminders',           2);
+        $this->type('additional_reminder_day', 1);
+
+        $this->click('is_allow_other_amount');
+        $this->type('min_amount', $rand / 2);
+        $this->type('max_amount', $rand * 2);
+
         $this->type('label_1', "Label $hash");
         $this->type('value_1', "$rand");
 
