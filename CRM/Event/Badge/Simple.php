24b165b13263f97d61543a930c935425c8b251dc
Xavier Dutoit <xavier@tttp.eu>
2010-05-25 07:32:58 +0000

    CRM-6294 Still ugly, but now at least it generates something

--- CRM/Event/Badge/Simple.php
+++ CRM/Event/Badge/Simple.php
@@ -1,9 +1,10 @@
 <?
+require_once 'CRM/Event/Badge.php';
 
 class CRM_Event_Badge_Simple extends CRM_Event_Badge {
 
    public function generateLabel($participant) {
-     $txt = $this->event['name']."\n".$participant['first_name']. " ".$participant['last_name'] ."\n". $participant['current_employer'];
+     $txt = $this->event->name."\n".$participant['first_name']. " ".$participant['last_name'] ."\n". $participant['current_employer'];
      $this->pdf->MultiCell ($this->pdf->width, $this->pdf->lineHeight, $txt);
    }
 
