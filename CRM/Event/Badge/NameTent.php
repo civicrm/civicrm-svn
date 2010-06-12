d97fedf63698b79f0b808d786467fb7f2b2c9094
Xavier Dutoit <xavier@tttp.eu>
2010-06-12 12:24:32 +0000

    Better calculation of the position

--- CRM/Event/Badge/NameTent.php
+++ CRM/Event/Badge/NameTent.php
@@ -41,10 +41,16 @@ require_once 'CRM/Event/Badge.php';
 class CRM_Event_Badge_NameTent extends CRM_Event_Badge {
 
     function __construct() {
-        parent::__construct();
-        $this->format = array('name' => 'L7163', 'paper-size' => 'A4', 'metric' => 'mm', 'lMargin' => 10,
-                              'tMargin' => 10, 'NX' => 1, 'NY' => 1, 'SpaceX' => 0, 'SpaceY' => 0,
-                              'width' => 277, 'height' => 190, 'font-size' => 36);
+      parent::__construct();
+      $pw=297; $ph=210;// A4
+      $this->lMargin = 10;
+      $this->tMargin = 0;
+      $w=$pw - 2*$this->lMarging; 
+      $h=$ph - 2*$this->tMargin;
+      $this->format = array('name' => 'A4 horiz', 'paper-size' => 'A4', 'metric' => 'mm', 'lMargin' => 0,
+                              'tMargin' => 0, 'NX' => 1, 'NY' => 1, 'SpaceX' => 0, 'SpaceY' => 0,
+                              'width' => $w, 'height' => $h, 'font-size' => 36);
+//      $this->setDebug ();
     }
 
     function pdfExtraFormat() {
@@ -52,6 +58,8 @@ class CRM_Event_Badge_NameTent extends CRM_Event_Badge {
     }
 
     protected function writeOneSide(&$participant) {
+        $this->pdf->SetXY(0,$this->pdf->height/2);
+        $this->printBackground (true);
         $txt = $participant['first_name']. " ".$participant['last_name'];
         $this->pdf->SetXY(0,$this->pdf->height/2+20);
         $this->pdf->SetFontSize(54);
@@ -65,7 +73,6 @@ class CRM_Event_Badge_NameTent extends CRM_Event_Badge {
     public function generateLabel($participant) {
         $this->writeOneSide($participant);
         $this->pdf->StartTransform();
-        $this->writeOneSide($participant);
         $this->pdf->Rotate(180,$this->pdf->width/2+$this->pdf->marginLeft,$this->pdf->height/2);
         $this->writeOneSide($participant);
         $this->pdf->StopTransform();
