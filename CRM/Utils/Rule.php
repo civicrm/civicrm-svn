9274af4517d18806c94127e8f2437955dbcd7530
Piotr Szotkowski <chastell@chastell.net>
2010-06-21 15:22:18 +0000

    fix CRM-6391

--- CRM/Utils/Rule.php
+++ CRM/Utils/Rule.php
@@ -319,7 +319,8 @@ class CRM_Utils_Rule
         $value = str_replace( array( ' ', "\t", "\n" ), '', $value );
 
         $config =& CRM_Core_Config::singleton( );
-        setlocale( LC_ALL, $config->lcMessages );
+
+        setlocale(LC_MONETARY, $config->lcMonetary);
         $localeInfo = localeconv( );
 
         if ( array_key_exists( 'mon_thousands_sep', $localeInfo ) ) {
@@ -328,7 +329,12 @@ class CRM_Utils_Rule
             $mon_thousands_sep = ',';
         }
 
-        $value = str_replace( $mon_thousands_sep, '', $value );
+        // ugly fix for CRM-6391: do not drop the thousand separator if
+        // it looks like it’s separating decimal part (because a given
+        // value undergoes a second cleanMoney() call, for example)
+        if (substr($value, -3, 1) != '.') {
+            $value = str_replace($mon_thousands_sep, '', $value);
+        }
 
         if ( array_key_exists( 'mon_decimal_point', $localeInfo ) ) {
             $mon_decimal_point = $localeInfo['mon_decimal_point'];
