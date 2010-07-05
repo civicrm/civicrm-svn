45dc71c89ff14c7e11cba719b757ed6bf439d1c9
Kurund Jalmi <kurund@civicrm.org>
2010-07-05 05:12:38 +0000

    reverting changes done for CRM-6306

--- CRM/Contribute/Form/Contribution/Main.php
+++ CRM/Contribute/Form/Contribution/Main.php
@@ -767,11 +767,11 @@ class CRM_Contribute_Form_Contribution_Main extends CRM_Contribute_Form_Contribu
         if ( $self->_values['is_monetary'] ) {
             //validate other amount.
             $checkOtherAmount = false;
-            if ( CRM_Utils_Array::value('amount', $fields ) == 'amount_other_radio' ) {
+            if ( CRM_Utils_Array::value('amount', $fields ) == 'amount_other_radio' || CRM_Utils_Array::value( 'amount_other', $fields ) ) {
                 $checkOtherAmount = true;
             }
             $otherAmountVal = CRM_Utils_Array::value( 'amount_other', $fields );
-            if ( $checkOtherAmount || !isset( $fields['amount'] ) ) {
+            if ( $checkOtherAmount || $otherAmountVal ) {
                 if ( !$otherAmountVal ) {
                     $errors['amount_other'] = ts('Amount is required field.');
                 }
