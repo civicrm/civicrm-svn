03e385bb72fb661a81c4fd13c28dcbc8a60dc1ed
Kurund Jalmi <kurund@civicrm.org>
2010-07-07 17:31:29 +0000

    worked on CRM-5757

--- CRM/Case/Audit/Audit.php
+++ CRM/Case/Audit/Audit.php
@@ -108,7 +108,7 @@ class Audit
 				}
 
                 if ( $printReport ) {                    
-                    $caseActivities[$activityindex] = CRM_Utils_Array::crmArrayMerge($activityStatusType, $caseActivities );
+                    $caseActivities[$activityindex] = CRM_Utils_Array::crmArrayMerge($activityStatusType[$activityindex], $caseActivities[$activityindex] );
                     $caseActivities[$activityindex]['sortValues'] = $sortValues;
                 }
 
