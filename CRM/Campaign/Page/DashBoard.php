0d0e5db7b82b49f8ee7f5a2afdc18473af2dbb37
Kiran <kiran@civicrm.org>
2010-08-04 09:17:54 +0000

    CRM-6582 -towards tab interface for campaign and survey dashboard.

---
+++ CRM/Campaign/Page/DashBoard.php
@@ -0,0 +1,220 @@
+<?php
+/*
+ +--------------------------------------------------------------------+
+ | CiviCRM version 3.2                                                |
+ --------------------------------------------------------------------+
+ | Copyright CiviCRM LLC (c) 2004-2010                                |
+ +--------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                    |
+ |                                                                    |
+ | CiviCRM is free software; you can copy, modify, and distribute it  |
+ | under the terms of the GNU Affero General Public License           |
+ | Version 3, 19 November 2007 and the CiviCRM Licensing Exception.   |
+ |                                                                    |
+ | CiviCRM is distributed in the hope that it will be useful, but     |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+ | See the GNU Affero General Public License for more details.        |
+ |                                                                    |
+ | You should have received a copy of the GNU Affero General Public   |
+ | License and the CiviCRM Licensing Exception along                  |
+ | with this program; if not, contact CiviCRM LLC                     |
+ | at info[AT]civicrm[DOT]org. If you have questions about the        |
+ | GNU Affero General Public License or the licensing of CiviCRM,     |
+ | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
+ +--------------------------------------------------------------------+
+ */
+  /**
+   *
+   * @package CRM
+   * @copyright CiviCRM LLC (c) 2004-2010
+   * $Id$
+   *
+   */
+
+require_once 'CRM/Core/Page.php';
+require_once 'CRM/Core/Permission.php';
+require_once 'CRM/Core/PseudoConstant.php';
+require_once 'CRM/Campaign/BAO/Survey.php';
+require_once 'CRM/Campaign/BAO/Campaign.php';
+
+/**
+ * Page for displaying Campaigns
+ */
+class CRM_Campaign_Page_DashBoard extends CRM_Core_Page 
+{
+    /**
+     * The action links that we need to display for the browse screen
+     *
+     * @var array
+     */
+    private static $_campaignActionLinks;
+    private static $_surveyActionLinks;
+    private $_dataType;
+    
+    /**
+     * Get the action links for this page.
+     *
+     * @return array $_campaignActionLinks
+     *
+     */
+    function &campaignActionLinks( )
+    {
+        // check if variable _actionsLinks is populated
+        if ( !isset( self::$_campaignActionLinks ) ) {
+            $deleteExtra = ts('Are you sure you want to delete this Campaign?');
+            self::$_campaignActionLinks = array(
+                                                CRM_Core_Action::UPDATE  => array(
+                                                                                  'name'  => ts('Edit'),
+                                                                                  'url'   => 'civicrm/campaign/add',
+                                                                                  'qs'    => 'reset=1&action=update&id=%%id%%',
+                                                                                  'title' => ts('Update Campaign') 
+                                                                                  ),
+                                                CRM_Core_Action::DISABLE => array(
+                                                                                  'name'  => ts('Disable'),
+                                                                                  'title' => ts('Disable Campaign'),
+                                                                                  'extra' => 'onclick = "enableDisable( %%id%%,\''. 'CRM_Campaign_BAO_Campaign' . '\',\'' . 'enable-disable' . '\' );"',
+                                                                                  'ref'   => 'disable-action'
+                                                                                  ),
+                                                CRM_Core_Action::ENABLE  => array(
+                                                                                  'name'  => ts('Enable'),
+                                                                                  'title' => ts('Enable Campaign'),
+                                                                                  'extra' => 'onclick = "enableDisable( %%id%%,\''. 'CRM_Campaign_BAO_Campaign' . '\',\'' . 'disable-enable' . '\' );"',
+                                                                                  'ref'   => 'enable-action',
+                                                                                  ),
+                                                CRM_Core_Action::DELETE  => array(
+                                                                                  'name'  => ts('Delete'),
+                                                                                  'url'   => 'civicrm/campaign/add',
+                                                                                  'qs'    => 'action=delete&reset=1&id=%%id%%',
+                                                                                  'title' => ts('Delete Campaign'),
+                                                                                  ),
+                                                );
+        }
+        
+        return self::$_campaignActionLinks;
+    }
+    
+    function &surveyActionLinks( )
+    {
+        // check if variable _actionsLinks is populated
+        if ( !isset( self::$_surveyActionLinks ) ) {
+            self::$_surveyActionLinks = array(
+                                              CRM_Core_Action::UPDATE  => array(
+                                                                                'name'  => ts('Edit'),
+                                                                                'url'   => 'civicrm/survey/add',
+                                                                                'qs'    => 'action=update&id=%%id%%&reset=1',
+                                                                                'title' => ts('Update Survey') 
+                                                                                ),
+                                              
+                                              CRM_Core_Action::DISABLE => array(
+                                                                                'name'  => ts('Disable'),
+                                                                                'extra' => 'onclick = "enableDisable( %%id%%,\''. 'CRM_Campaign_BAO_Survey' . '\',\'' . 'enable-disable' . '\' );"',
+                                                                                'ref'   => 'disable-action',
+                                                                                'title' => ts('Disable Survey')
+                                                                                ),
+                                              
+                                              CRM_Core_Action::ENABLE  => array(
+                                                                                'name'  => ts('Enable'),
+                                                                                'extra' => 'onclick = "enableDisable( %%id%%,\''. 'CRM_Campaign_BAO_Survey' . '\',\'' . 'disable-enable' . '\' );"',
+                                                                                'ref'   => 'enable-action',
+                                                                                'title' => ts('Enable Survey')
+                                                                                ),
+                                              
+                                              CRM_Core_Action::DELETE  => array(
+                                                                                'name'  => ts('Delete'),
+                                                                                'url'   => 'civicrm/survey/add',
+                                                                                'qs'    => 'action=delete&id=%%id%%&reset=1',
+                                                                                'title' => ts('Delete Survey'),
+                                                                                ), 
+                                              );
+        }
+        
+        return self::$_surveyActionLinks;
+    }
+    
+    function browseCampaign( ) 
+    {
+        $campaignsData = array( );
+        //get the campaigns.
+        $campaigns = CRM_Campaign_BAO_Campaign::getCampaign( true );
+        if ( !empty( $campaigns ) ) {
+            $campaignType    = CRM_Core_PseudoConstant::campaignType( );
+            $campaignStatus  = CRM_Core_PseudoConstant::campaignStatus( );
+            $properties      = array( 'id', 'name', 'title', 'status_id', 'description', 'campaign_type_id' );
+            foreach( $campaigns as $cmpid => $campaign ) { 
+                foreach ( $properties as $prop ) {
+                    $campaignsData[$cmpid][$prop] = $campaign[$prop];
+                }
+                $campaignsData[$cmpid]['status'       ] = $campaignStatus[$campaign['status_id']];
+                $campaignsData[$cmpid]['campaign_id'  ] = $campaign['id'];
+                $campaignsData[$cmpid]['campaign_type'] = $campaignType[$campaign['campaign_type_id']];
+                
+                $action = array_sum( array_keys( $this->campaignActionLinks( ) ) );
+                if ( $campaign['is_active'] ) {
+                    $action -= CRM_Core_Action::ENABLE;
+                } else {
+                    $action -= CRM_Core_Action::DISABLE;
+                }
+                $campaignsData[$cmpid]['action'] = CRM_Core_Action::formLink( self::campaignActionLinks( ), 
+                                                                              $action, 
+                                                                              array('id' => $campaign['id'] ) );
+            }
+        }
+        
+        $this->assign( 'campaigns',      $campaignsData );
+        $this->assign( 'addCampaignUrl', CRM_Utils_System::url( 'civicrm/campaign/add', 'reset=1&action=add' ) );
+    }
+    
+    function browseSurvey( ) 
+    {
+        $surveysData = array( );
+        //get the survey.
+        $surveys = CRM_Campaign_BAO_Survey::getSurvey( true );
+        if ( !empty( $surveys ) ) {
+            $campaigns     = CRM_Campaign_BAO_Campaign::getAllCampaign( );
+            $surveyType    = CRM_Campaign_BAO_Survey::getSurveyActivityType( );
+            foreach( $surveys as $sid => $survey ) {
+                $surveysData[$sid] = $survey;
+                $surveysData[$sid]['campaign_id']       = $campaigns[$survey['campaign_id']];
+                $surveysData[$sid]['activity_type']     = $surveyType[$survey['activity_type_id']];
+                $surveysData[$sid]['release_frequency'] = $survey['release_frequency_interval'].' Day(s)';
+                if ( $survey['is_active'] ) {
+                    $action -= CRM_Core_Action::ENABLE;
+                } else {
+                    $action -= CRM_Core_Action::DISABLE;
+                }
+                $surveysData[$sid]['action'] = CRM_Core_Action::formLink( $this->surveyActionLinks( ), 
+                                                                          $action, 
+                                                                          array('id' => $sid ) );
+            }
+        }
+        
+        $this->assign( 'surveys',      $surveysData );
+        $this->assign( 'addSurveyUrl', CRM_Utils_System::url( 'civicrm/survey/add', 'reset=1&action=add' ) );
+    }
+    
+    function browse( ) 
+    {
+        $this->browseSurvey( );
+        $this->browseCampaign( );
+    }
+    
+    function run( ) 
+    {
+        if ( !CRM_Core_Permission::check( 'administer CiviCampaign' ) ) {
+            CRM_Utils_System::permissionDenied( );
+        }
+        
+        $action = CRM_Utils_Request::retrieve( 'action', 'String', $this, false, 0 ); 
+        $this->assign('action', $action);
+        
+        $this->_dataType = CRM_Utils_Request::retrieve( 'type', 'String', $this );
+        $this->assign( 'dataType', $this->_dataType );
+        
+        $this->browse( );
+        
+        parent::run();
+    }
+    
+}
+
---
+++ templates/CRM/Campaign/Page/DashBoard.tpl
@@ -0,0 +1,190 @@
+{*
+ +--------------------------------------------------------------------+
+ | CiviCRM version 3.2                                                |
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC (c) 2004-2010                                |
+ +--------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                    |
+ |                                                                    |
+ | CiviCRM is free software; you can copy, modify, and distribute it  |
+ | under the terms of the GNU Affero General Public License           |
+ | Version 3, 19 November 2007 and the CiviCRM Licensing Exception.   |
+ |                                                                    |
+ | CiviCRM is distributed in the hope that it will be useful, but     |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+ | See the GNU Affero General Public License for more details.        |
+ |                                                                    |
+ | You should have received a copy of the GNU Affero General Public   |
+ | License and the CiviCRM Licensing Exception along                  |
+ | with this program; if not, contact CiviCRM LLC                     |
+ | at info[AT]civicrm[DOT]org. If you have questions about the        |
+ | GNU Affero General Public License or the licensing of CiviCRM,     |
+ | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
+ +--------------------------------------------------------------------+
+*}
+{* CiviCampaign DashBoard (launch page) *}
+
+{* build the campaign selector *}
+{if $dataType eq 'campaign'}
+
+{if $campaigns} 
+  <div class="action-link">
+      <a href="{$addCampaignUrl}" class="button"><span>&raquo; {ts}Add Campaign{/ts}</span></a>
+  </div>
+  {include file="CRM/common/enableDisable.tpl"}
+  <div id="campaignType">
+    <table id="options" class="display">
+      <thead>
+        <tr>      
+          <th>{ts}Campaign Title{/ts}</th>
+          <th>{ts}Description{/ts}</th>
+          <th>{ts}Start Date{/ts}</th> 
+          <th>{ts}End Date{/ts}</th>
+          <th>{ts}Campaign Type{/ts}</th>
+          <th>{ts}Status{/ts}</th>
+          <th>{ts}Active?{/ts}</th>
+          <th id="nosort"></th>
+	</tr>
+      </thead>
+      {foreach from=$campaigns item=campaign}
+        <tr id="row_{$campaign.campaign_id}" {if $campaign.is_active neq 1}class="disabled"{/if}>
+          <td>{$campaign.title}</td>
+          <td>{$campaign.description}</td>
+          <td>{$campaign.start_date}</td>
+          <td>{$campaign.end_date}</td>
+          <td>{$campaign.campaign_type}</td>
+          <td>{$campaign.status}</td>
+          <td id="row_{$campaign.id}_status">{if $campaign.is_active eq 1} {ts}Yes{/ts} {else} {ts}No{/ts} {/if}</td>
+          <td>{$campaign.action}</td>
+	</tr>
+      {/foreach}
+    </table>
+  </div>
+
+{else} 
+    <div class="messages status">
+        <div class="icon inform-icon"></div> &nbsp;
+        {ts}No Campaigns found.{/ts}
+    </div>
+{/if}
+<div class="action-link">
+   <a href="{$addCampaignUrl}" class="button"><span>&raquo; {ts}Add Campaign{/ts}</span></a>
+</div>
+
+{* build the survey selector *}
+{elseif $dataType eq 'survey'}
+
+{if $surveys} 
+  <div class="action-link">
+    <a href="{$addSurveyUrl}" class="button"><span>&raquo; {ts}Add Survey{/ts}</span></a>
+  </div>
+ {include file="CRM/common/enableDisable.tpl"}
+ {include file="CRM/common/jsortable.tpl"}
+  <div id="surveyList">
+    <table id="options" class="display">
+      <thead>
+        <tr>  
+          <th>{ts}Survey{/ts}</th>
+          <th>{ts}Campaign{/ts}</th>
+          <th>{ts}Survey Type{/ts}</th>   
+          <th>{ts}Release Frequency{/ts}</th>
+	  <th>{ts}Max Number Of Contacts{/ts}</th>
+	  <th>{ts}Default Number Of Contacts{/ts}</th>
+	  <th>{ts}Default?{/ts}</th>
+	  <th>{ts}Active?{/ts}</th>
+	  <th id="nosort"></th>
+        </tr>
+      </thead>
+      {foreach from=$surveys item=survey}
+        <tr id="row_{$survey.id}" {if $survey.is_active neq 1}class="disabled"{/if}>
+	  <td>{$survey.title}</td>
+          <td>{$survey.campaign_id}</td>
+          <td>{$survey.activity_type}</td>
+          <td>{$survey.release_frequency}</td>
+          <td>{$survey.max_number_of_contacts}</td>
+          <td>{$survey.default_number_of_contacts}</td>
+          <td>{if $survey.is_default}<img src="{$config->resourceBase}/i/check.gif" alt="{ts}Default{/ts}" /> {/if}</td>
+          <td id="row_{$survey.id}_status">{if $survey.is_active}{ts}Yes{/ts}{else}{ts}No{/ts}{/if}</td>
+ 	  <td class="crm-report-optionList-action">{$survey.action}</td>
+        </tr>
+      {/foreach}
+    </table>
+  </div>
+
+{else} 
+  <div class="status">
+    <div class="icon inform-icon"></div>&nbsp;{ts}No survey found.{/ts}
+  </div> 
+{/if}
+<div class="action-link">
+   <a href="{$addSurveyUrl}" class="button"><span>&raquo; {ts}Add Survey{/ts}</span></a>
+</div>
+
+
+{* build normal page *}
+{else}
+
+ <div id="mainTabContainer" class="ui-tabs ui-widget ui-widget-content ui-corner-all">
+     <ul class="ui-tabs-nav ui-helper-reset ui-helper-clearfix ui-widget-header ui-corner-all">
+         <li id="campaign_view" class="crm-tab-button ui-state-active ui-corner-top ui-corner-bottom ui-tabs-selected"> 
+             <a href="#campaign"><span>&nbsp;</span>&nbsp;{ts}Campaign{/ts}&nbsp;</a> 
+	 </li>&nbsp;
+		
+	 <li id ="survey_view"  class="crm-tab-button ui-corner-top ui-corner-bottom ui-state-default" >
+             <a href="#survey"><span>&nbsp;</span>&nbsp;{ts}Survey{/ts}&nbsp;</a>
+         </li>
+     </ul>
+
+    <div id="campaignData"></div>
+    <div id="surveyData"></div>
+ </div>
+
+ <div class="spacer"></div>
+
+{literal}
+<script type="text/javascript">
+cj(document).ready( function( ) {
+
+    cj('#campaign_view').click( function( ) {
+        if ( cj('#campaign_view').hasClass('ui-state-default') ) { 
+            cj('#campaign_view').removeClass('ui-state-default').addClass('ui-state-active ui-tabs-selected');
+            cj('#survey_view').removeClass('ui-state-active ui-tabs-selected').addClass('ui-state-default');
+            cj('#surveyData').html('');
+	    
+	    //refill campaign data.
+	    buildDataView( 'campaign' );
+        }
+    });
+
+    cj('#survey_view').click( function( ) {
+        if ( cj('#survey_view').hasClass('ui-state-default') ) {
+            cj('#survey_view').removeClass('ui-state-default').addClass('ui-state-active ui-tabs-selected');
+            cj('#campaign_view').removeClass('ui-state-active ui-tabs-selected').addClass('ui-state-default');
+	    cj('#campaignData').html( '' );
+	    
+	    //refill survey data.
+	    buildDataView( 'survey' );	   
+        }
+
+    });
+
+    buildDataView( 'campaign' );
+});
+
+function buildDataView( dataType ) {
+    var dataUrl = {/literal}"{crmURL p='civicrm/campaign' h=0}"{literal};
+    dataUrl    += "&snippet=4&type=" + dataType;
+    
+    cj.ajax({
+        url      : dataUrl,
+        async    : false,
+        success  : function( html ) {
+	    cj( '#' + dataType + 'Data' ).html( html );
+        }	 
+    });
+}        
+           
+</script>
+{/literal}
+{/if}
