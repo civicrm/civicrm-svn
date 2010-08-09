07bbbde72047d547ecd9e179fdd35fb7d2013eea
Donald Lobo <lobo@civicrm.org>
2010-08-09 14:15:09 +0000

    revert bad commit of contribution.php CRM-6620

--- CRM/Contribute/BAO/Contribution.php
+++ CRM/Contribute/BAO/Contribution.php
@@ -735,21 +735,19 @@ GROUP BY p.id
                               'last_name'     => $params["honor_last_name"], 
                               'prefix_id'     => $params["honor_prefix_id"],
                               'email-Primary' => $params["honor_email"] );
-        if ( ! $honorId ) {
+        if ( !$honorId ) {
+            require_once "CRM/Core/BAO/UFGroup.php";
             $honorParams['email'] = $params["honor_email"];
-
-            require_once 'CRM/Dedupe/Finder.php';
-            $dedupeParams = CRM_Dedupe_Finder::formatParams($honorParams, 'Individual');
-            $dedupeParams['check_permission'] = false;
-            $ids = CRM_Dedupe_Finder::dupesByParams($dedupeParams, 'Individual');
-
-            // if we find more than one contact, use the first one
-            $honorId  = CRM_Utils_Array::value( 0, $ids );
+            $honorParams['check_permission'] = false;
+            $ids = CRM_Core_BAO_UFGroup::findContact( $honorParams, null, 'Individual' );
+            $contactsIds = explode( ',', $ids );
+            
+            if ( is_numeric( $contactsIds[0] ) && count ( $contactsIds ) ==  1 ) {
+                $honorId = $contactsIds[0];
+            }
         }
         
-        $contact =& CRM_Contact_BAO_Contact::createProfileContact( $honorParams,
-                                                                   CRM_Core_DAO::$_nullArray,
-                                                                   $honorId );
+        $contact =& CRM_Contact_BAO_Contact::createProfileContact( $honorParams, CRM_Core_DAO::$_nullArray, $honorId );
         return $contact;
     }
     
