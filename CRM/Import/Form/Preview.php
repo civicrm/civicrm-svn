5cb5f8ed3b562fd7b03d336517d420b9dbb65bfd
Sushant Paste <sushant.paste@webaccess.co.in>
2010-08-12 06:05:31 +0000

    #CRM-6568 validation for qfkey

--- CRM/Import/Form/Preview.php
+++ CRM/Import/Form/Preview.php
@@ -162,8 +162,8 @@ class CRM_Import_Form_Preview extends CRM_Core_Form {
         }
         
         $qfKey = CRM_Utils_Request::retrieve( 'qfKey', 'String', $form );
-        $previousURL = CRM_Utils_System::url('civicrm/import/contact', "_qf_MapField_display=true&qfKey=$qfKey", false, 
-                                             null, false);
+        if ( CRM_Utils_Rule::qfKey( $qfKey ) ) $path = "_qf_MapField_display=true&qfKey=$qfKey";
+        $previousURL = CRM_Utils_System::url('civicrm/import/contact', $path, false, null, false);
         $cancelURL   = CRM_Utils_System::url('civicrm/import/contact', 'reset=1');
         
         $buttons = array(
