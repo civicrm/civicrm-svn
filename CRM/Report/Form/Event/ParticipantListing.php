20cd0bbbfbe1dbe698f9fb61bead16de18a8c123
Sunny Sharma <sunny.sharma@webaccess.co.in>
2010-08-24 07:17:03 +0000

    // minor fix for CRM-6707

--- CRM/Report/Form/Event/ParticipantListing.php
+++ CRM/Report/Form/Event/ParticipantListing.php
@@ -204,7 +204,9 @@ class CRM_Report_Form_Event_ParticipantListing extends CRM_Report_Form {
         $this->_from = "
         FROM civicrm_participant {$this->_aliases['civicrm_participant']}
              LEFT JOIN civicrm_event {$this->_aliases['civicrm_event']} 
-                    ON ({$this->_aliases['civicrm_event']}.id = {$this->_aliases['civicrm_participant']}.event_id ) AND {$this->_aliases['civicrm_event']}.is_template IS NULL
+                    ON ({$this->_aliases['civicrm_event']}.id = {$this->_aliases['civicrm_participant']}.event_id ) AND 
+                       ({$this->_aliases['civicrm_event']}.is_template IS NULL OR  
+                        {$this->_aliases['civicrm_event']}.is_template = 0)
              LEFT JOIN civicrm_contact {$this->_aliases['civicrm_contact']} 
                     ON ({$this->_aliases['civicrm_participant']}.contact_id  = {$this->_aliases['civicrm_contact']}.id  )
              {$this->_aclFrom}
