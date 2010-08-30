a2b5b6598d65a401a600e783eeb97065efdd3352
Xavier Dutoit <xavier@tttp.eu>
2010-08-30 13:51:41 +0000

    CRM-6761 the cli mode is tested on php_sapi_name instead of argv

--- bin/EmailProcessor.php
+++ bin/EmailProcessor.php
@@ -228,7 +228,7 @@ class EmailProcessor {
 
 // bootstrap the environment and run the processor
 // you can run this program either from an apache command, or from the cli
-if (isset($argv)) {
+if ( php_sapi_name() == "cli" ) {
   require_once ("bin/cli.php");
   $cli=new civicrm_cli ();
   //if it doesn't die, it's authenticated 
