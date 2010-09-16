e5caa39e0cab8a73dd712e50ca6520dd726c9476
Yashodha Chaku <yashodha.chaku@gmail.com>
2010-09-16 11:45:41 +0000

    #CRM-6748

--- CRM/Contribute/Page/ContributionPage.php
+++ CRM/Contribute/Page/ContributionPage.php
@@ -314,6 +314,14 @@ class CRM_Contribute_Page_ContributionPage extends CRM_Core_Page
             CRM_Utils_System::setTitle( ts('Manage Contribution Page') );
             CRM_Utils_System::appendBreadCrumb( $breadCrumb );
             return $controller->run( );
+        } else if ( $action & CRM_Core_Action::UPDATE ) {
+            CRM_Utils_System::appendBreadCrumb( $breadCrumb );
+            $session = CRM_Core_Session::singleton( ); 
+            $session->pushUserContext( CRM_Utils_System::url( CRM_Utils_System::currentPath( ),
+                                                              "action=update&reset=1&id={$id}") );
+            require_once 'CRM/Contribute/Page/ContributionPageEdit.php';
+            $page = new CRM_Contribute_Page_ContributionPageEdit( );
+            return $page->run( );
         } else if ( $action & CRM_Core_Action::COPY ) {
             $session = CRM_Core_Session::singleton( );
             CRM_Core_Session::setStatus( ts('A copy of the contribution page has been created') );
