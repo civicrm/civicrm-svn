35b8c2e82e9f86068144b98621616e25aba9ce38
Piotr Szotkowski <chastell@chastell.net>
2010-09-29 18:51:01 +0000

    introduce CRM_Logging_Engine::createCustomTables()

--- CRM/Logging/Engine.php
+++ CRM/Logging/Engine.php
@@ -65,8 +65,50 @@ class CRM_Logging_Engine
         global $civicrm_root;
         if (!self::tablesExist()) {
             CRM_Utils_File::sourceSQLFile($config->dsn, "$civicrm_root/sql/logging_tables.sql");
+            self::createCustomTables();
         }
         CRM_Utils_File::sourceSQLFile($config->dsn, "$civicrm_root/sql/logging_triggers.sql");
+    }
+
+    private static function createCustomTables()
+    {
+        // fetch custom table names
+        $tables = array();
+        $dao = CRM_Core_DAO::executeQuery('SHOW TABLES LIKE "civicrm_value_%"');
+        while ($dao->fetch()) {
+            $tables[] = $dao->toValue('Tables_in_civicrm_(civicrm_value_%)');
+        }
+
+        // fetch CREATE TABLE queries
+        $queries = array();
+        foreach ($tables as $table) {
+            $dao = CRM_Core_DAO::executeQuery("SHOW CREATE TABLE $table");
+            $dao->fetch();
+            $queries[$table] = $dao->Create_Table;
+        }
+
+        // rewrite the queries into CREATE TABLE queries for log tables:
+        // - prepend the name with log_
+        // - drop AUTO_INCREMENT columns
+        // - drop non-column rows of the query (keys, constraints, etc.)
+        // - set the ENGINE to ARCHIVE
+        // - add log-specific columns (at the end of the table)
+        $cols = <<<COLS
+            log_date    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            log_conn_id INTEGER,
+            log_user_id INTEGER,
+            log_action  ENUM('Initialization', 'Insert', 'Update', 'Delete')
+COLS;
+        foreach ($queries as $table => $query) {
+            $query = preg_replace("/^CREATE TABLE `$table`/", "CREATE TABLE `log_$table`", $query);
+            $query = preg_replace("/ AUTO_INCREMENT/", '', $query);
+            $query = preg_replace("/^  [^`].*$/m", '', $query);
+            $query = preg_replace("/^\) ENGINE=[^ ]+ /m", ') ENGINE=ARCHIVE ', $query);
+            $query = preg_replace("/^\) /m", "$cols\n) ", $query);
+
+            CRM_Core_DAO::executeQuery("DROP TABLE IF EXISTS log_$table");
+            CRM_Core_DAO::executeQuery($query);
+        }
     }
 
     private static function isEnabled()
