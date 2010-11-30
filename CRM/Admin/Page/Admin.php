df4b4b6cd0ac896427fb01e3c436833e5835ab7e
Donald Lobo <lobo@civicrm.org>
2010-11-30 04:38:07 +0000

    fix CRM-7149

--- CRM/Admin/Page/Admin.php
+++ CRM/Admin/Page/Admin.php
@@ -44,11 +44,17 @@ class CRM_Admin_Page_Admin extends CRM_Core_Page
     function run ( ) 
     {
         // ensure that all CiviCRM tables are InnoDB, else abort
-        if ( CRM_Core_DAO::isDBMyISAM( ) ) {
-            $errorMessage = 'Your database is configured to use the MyISAM database engine. CiviCRM  requires InnoDB. You will need to convert any MyISAM tables in your database to InnoDB. Using MyISAM tables will result in data integrity issues. This will be a fatal error in CiviCRM v2.1.';
-            require_once 'CRM/Core/Session.php';
-            CRM_Core_Session::setStatus( $errorMessage );
+        // ensure that all CiviCRM tables are InnoDB, else abort
+        // this is not a very fast operation, so we do it randomly 10% of the times
+        // but we do it for most / all tables
+        // http://bugs.mysql.com/bug.php?id=43664
+        if ( rand( 1, 10 ) == 3 &&
+             CRM_Core_DAO::isDBMyISAM( 150 ) ) {
+            $errorMessage = 'Your database is configured to use the MyISAM database engine. CiviCRM  requires InnoDB. You will need to convert any MyISAM tables in your database to InnoDB. Using MyISAM tables will result in data integrity issues.';
+            require_once 'CRM/Core/Error.php';
+            CRM_Core_Error::fatal( $errorMessage );
         }
+
         if ( !CRM_Utils_System::isDBVersionValid( $errorMessage ) ) {
             CRM_Core_Session::setStatus( $errorMessage );
         }
