834c130ffadaf85e9c2d33186e6126dbd4cf4a3e
Eileen McNaughton <eileen@mcnaughty.com>
2011-01-02 00:35:37 +0000

    Added 'base' v3 tests based on v2

---
+++ tests/phpunit/api/v3/ActivityTypeTest.php
@@ -0,0 +1,110 @@
+<?php
+
+/**
+ *  File for the TestActivityType class
+ *
+ *  (PHP 5)
+ *  
+ *   @package   CiviCRM
+ *
+ *   This file is part of CiviCRM
+ *
+ *   CiviCRM is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Affero General Public License
+ *   as published by the Free Software Foundation; either version 3 of
+ *   the License, or (at your option) any later version.
+ *
+ *   CiviCRM is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Affero General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Affero General Public
+ *   License along with this program.  If not, see
+ *   <http://www.gnu.org/licenses/>.
+ */
+
+require_once 'CiviTest/CiviUnitTestCase.php';
+require_once 'api/v3/ActivityType.php';
+
+
+/**
+ *  Test APIv3 civicrm_activity_* functions
+ *
+ *  @package   CiviCRM
+ */
+
+
+class api_v3_ActivityTypeTest extends CiviUnitTestCase 
+{
+ 
+    function get_info( ) 
+    {
+        return array(
+                     'name'        => 'Activity Type',
+                     'description' => 'Test all ActivityType Get/Create/Delete methods.',
+                     'group'       => 'CiviCRM API Tests',
+                     );
+    }
+    
+    function setUp( ) 
+    {
+        parent::setUp();
+    }
+    
+    /**
+     *  Test civicrm_activity_type_get()
+     */
+    function testActivityTypeCheckValues()
+    {
+        $activitytypes = & civicrm_activity_type_get();
+        $this->assertEquals($activitytypes['1'],'Meeting', 'In line ' . __LINE__ );
+        $this->assertEquals($activitytypes['13'],'Open Case', 'In line ' . __LINE__ );
+        
+    }
+    
+    /**
+     *  Test civicrm_activity_type_create with no label()
+     */
+    function testActivityTypecreate( ) {
+        
+        $params = array(
+                        'weight'=> '2',
+                        );
+        $activitycreate = & civicrm_activity_type_create($params);
+        $this->assertEquals( $activitycreate['is_error'], 1);
+        $this->assertEquals( $activitycreate['error_message'],'Required parameter "label / weight" not found');
+        
+    }
+    
+    /**
+     *  Test civicrm_activity_type_create - check id
+     */
+    function testActivityTypecreatecheckId( ) {
+        
+        $params = array(
+                        'label' => 'type_create',
+                        'weight'=> '2',
+                        );
+        $activitycreate = & civicrm_activity_type_create($params);
+        $activityID = $activitycreate['id'];
+        $this->assertNotContains( 'is_error', $activitycreate );
+        $this->assertArrayHasKey( 'id', $activitycreate );
+        $this->assertArrayHasKey( 'option_group_id', $activitycreate );
+    }
+    
+    /**
+     *  Test civicrm_activity_type_delete()
+     */
+    function testActivityTypeDelete( ) {
+        
+        $params = array(
+                        'label' => 'type_create_delete',
+                        'weight'=> '2',
+                        );
+        $activitycreate = & civicrm_activity_type_create($params);
+        $params = array( 'activity_type_id' => $activitycreate['id'] );
+        $activitydelete = & civicrm_activity_type_delete($params);
+        $this->assertEquals($activitydelete, 1 , 'In line ' . __LINE__);
+    }
+}
---
+++ tests/phpunit/api/v3/MembershipTest.php
@@ -0,0 +1,138 @@
+<?php  
+/*
+ +--------------------------------------------------------------------+
+ | CiviCRM version 3.3                                                |
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC (c) 2004-2010                                |
+ +--------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                    |
+ |                                                                    |
+ | CiviCRM is free software; you can copy, modify, and distribute it  |
+ | under the terms of the GNU Affero General Public License           |
+ | Version 3, 19 November 2007 and the CiviCRM Licensing Exception.   |
+ |                                                                    |
+ | CiviCRM is distributed in the hope that it will be useful, but     |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+ | See the GNU Affero General Public License for more details.        |
+ |                                                                    |
+ | You should have received a copy of the GNU Affero General Public   |
+ | License and the CiviCRM Licensing Exception along                  |
+ | with this program; if not, contact CiviCRM LLC                     |
+ | at info[AT]civicrm[DOT]org. If you have questions about the        |
+ | GNU Affero General Public License or the licensing of CiviCRM,     |
+ | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
+ +--------------------------------------------------------------------+
+*/
+
+require_once 'CiviTest/CiviUnitTestCase.php';
+require_once 'api/v3/Membership.php';
+require_once 'CiviTest/CiviUnitTestCase.php';
+
+class api_v3_MembershipTest extends CiviUnitTestCase
+{
+
+    public function setUp()
+    {
+        //  Connect to the database
+        parent::setUp();
+
+        $this->_contactID           = $this->individualCreate( ) ;
+        $this->_membershipTypeID    = $this->membershipTypeCreate( $this->_contactID  );        
+        $this->_membershipStatusID  = $this->membershipStatusCreate( 'test status' );                
+
+        $params = array(
+                        'contact_id'         => $this->_contactID,  
+                        'membership_type_id' => $this->_membershipTypeID,
+                        'join_date'          => '2009-01-21',
+                        'start_date'         => '2009-01-21',
+                        'end_date'           => '2009-12-21',
+                        'source'             => 'Payment',
+                        'is_override'        => 1,
+                        'status_id'          => $this->_membershipStatusID
+                        );
+        
+        $this->_membershipID = $this->contactMembershipCreate( $params );
+
+    }
+
+    /**
+     *  Test civicrm_membership_delete()
+     */
+    function testMembershipDelete()
+    {
+        $result = civicrm_membership_delete($this->_membershipID);
+
+        $this->assertEquals( $result['is_error'], 0,
+                             "In line " . __LINE__ );      
+   
+        $this->assertEquals( $result['result'], 1,
+                             "In line " . __LINE__ );   
+    }
+
+    /**
+     * check civicrm_membership_delete() with empty parameter
+     */
+    function testMembershipDeleteEmpty( )
+    {
+        $membershipId = null;
+        $result = civicrm_membership_delete($membershipId);
+        $this->assertEquals( $result['is_error'], 1,
+                             "In line " . __LINE__ );
+    }
+
+
+    /**
+     *  Test civicrm_membership_delete() with invalid Membership Id
+     */
+    function testMembershipDeleteWithInvalidMembershipId( )
+    {
+        $membershipId = 'membership';
+        $result = civicrm_membership_delete($membershipId);
+        $this->assertEquals( $result['is_error'], 1,
+                             "In line " . __LINE__ );
+    }
+
+    /**
+     *  All other methods calls MembershipType and MembershipContact
+     *  api, but putting simple test methods to control existence of
+     *  these methods for backwards compatibility, also verifying basic
+     *  behaviour is the same as new methods.
+     */
+     
+     function testContactMembershipsGet()
+     {
+         $this->assertTrue( function_exists(civicrm_contact_memberships_get) );
+         $params = array();
+         $result = civicrm_contact_memberships_get( $params );
+         $this->assertEquals( 1, $result['is_error'],
+                              "In line " . __LINE__ );
+     }
+     
+     function testContactMembershipCreate()
+     {
+         $this->assertTrue( function_exists(civicrm_contact_membership_create) );
+         $params = array();
+         $result = civicrm_contact_membership_create( $params );
+         $this->assertEquals( 1, $result['is_error'],
+                              "In line " . __LINE__ );
+     }
+
+     function testContactMembershipGet()
+     {
+         $this->assertTrue( function_exists(civicrm_membership_types_get) );
+         $params = array();
+         $result = civicrm_membership_types_get( $params );
+         $this->assertEquals( 1, $result['is_error'],
+                              "In line " . __LINE__ );         
+     }
+    
+     function testMembershipStatusesGet()
+     {
+         $this->assertTrue( function_exists(civicrm_membership_statuses_get) );
+         $params = 'wrong type';
+         $result = civicrm_membership_statuses_get( $params );
+         $this->assertEquals( 1, $result['is_error'],
+                              "In line " . __LINE__ );         
+     }
+} 
