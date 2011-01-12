52dfe8b30af363dd97e60c64e5f7d0c79a2c90f6
rahulbile <rahulbile@civicrm.org>
2011-01-12 08:04:21 +0000

    // CRM-7262

--- CRM/Core/BAO/CustomGroup.php
+++ CRM/Core/BAO/CustomGroup.php
@@ -1730,6 +1730,13 @@ SELECT IF( EXISTS(SELECT name FROM civicrm_contact_type WHERE name like %1), 1, 
 
             $retValue = null;
             foreach ( $options as $optionValue => $optionLabel ) {
+                if ( $dataType == 'Money') {
+                    require_once 'CRM/Utils/Money.php';
+                    foreach ( $customData as $k => $v ){
+                        $customData[] = CRM_Utils_Money::format( $v, null, '%a');
+                    }
+                }  
+                
                 //to show only values that are checked
                 if ( in_array ( (string) $optionValue, $customData ) ) {
                     $checked = in_array ( $optionValue, $customData ) ? $freezeStringChecked : $freezeString;
