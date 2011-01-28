a5627fb84b8d344c6826b2291d4689a106396cb0
Donald Lobo <lobo@civicrm.org>
2011-01-28 19:52:31 +0000

    fix CRM-7451

--- tests/phpunit/CRM/Utils/MailTest.php
+++ tests/phpunit/CRM/Utils/MailTest.php
@@ -27,21 +27,32 @@ class CRM_Utils_MailTest extends CiviUnitTestCase
     {
         require_once 'CRM/Utils/Mail.php';
 
-        $values = array( array( 'name'   => "Test User"   ,
-                                'email'  => "foo@bar.com",
-                                'result' => "Test User <foo@bar.com>" ),
-                         array( 'name'   => '"Test User"' ,
-                                'email'  => "foo@bar.com" ,
-                                'result' => "Test User <foo@bar.com>"),
-                         array( 'name'   => "User, Test"  ,
-                                'email'  => "foo@bar.com",
-                                 'result' => '"User, Test" <foo@bar.com>' ),
-                         array( 'name'   => '"User, Test"',
-                                'email'  => "foo@bar.com" ,
-                                'result' => '"User, Test" <foo@bar.com>' ),
+        $values = array( array( 'name'     => "Test User"   ,
+                                'email'    => "foo@bar.com",
+                                'result'   => "Test User <foo@bar.com>" ),
+                         array( 'name'     => '"Test User"' ,
+                                'email'    => "foo@bar.com" ,
+                                'result'   => "Test User <foo@bar.com>"),
+                         array( 'name'     => "User, Test"  ,
+                                'email'    => "foo@bar.com",
+                                'result'   => '"User, Test" <foo@bar.com>' ),
+                         array( 'name'     => '"User, Test"',
+                                'email'    => "foo@bar.com" ,
+                                'result'   => '"User, Test" <foo@bar.com>' ),
+                         array( 'name'     => '"Test User"' ,
+                                'email'    => "foo@bar.com" ,
+                                'result'   => '"Test User" <foo@bar.com>' ,
+                                'useQuote' => true ),
+                         array( 'name'     => "User, Test"  ,
+                                'email'    => "foo@bar.com",
+                                'result'   => '"User, Test" <foo@bar.com>',
+                                'useQuote' => true ),
+
                          );
         foreach ( $values as $value ) {
-            $result = CRM_Utils_Mail::formatRFC822Email( $value['name'], $value['email'] );
+            $result = CRM_Utils_Mail::formatRFC822Email( $value['name'],
+                                                         $value['email'],
+                                                         CRM_Utils_Array::value( 'useQuote', $value, false ) );
             $this->assertEquals( $result, $value['result'], 'Expected encoding does not match' );
         }
     }
