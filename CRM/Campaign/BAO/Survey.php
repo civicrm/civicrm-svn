6d6e2adbd9b375cd9e06691a284bc35ee19a4c75
Donald Lobo <lobo@civicrm.org>
2011-02-09 19:02:38 +0000

    fix CRM-7525

--- CRM/Campaign/BAO/Survey.php
+++ CRM/Campaign/BAO/Survey.php
@@ -408,32 +408,52 @@ INNER JOIN  civicrm_activity_assignment activityAssignment ON ( activityAssignme
      * @return $activities an array of survey activity.
      * @static
      */
-    static function getSurveyActivities( $surveyId, $interviewerId = null, $statusIds = array( ) ) 
+    static function getSurveyActivities( $surveyId, $interviewerId = null, 
+                                         $statusIds = null, $voterIds = null, 
+                                         $count = false ) 
     {
         $activities = array( );
-        if ( !$surveyId ) return $activities; 
+        if ( ! $surveyId ) {
+            return $activities;
+        }
         
         $where = array( );
-        if ( is_array( $statusIds ) && !empty( $statusIds ) ) {
+        if ( ! empty( $statusIds ) ) {
             $where[] = '( activity.status_id IN ( '. implode( ',', array_values( $statusIds ) ) . ' ) )';
         }
+
         if ( $interviewerId ) {
             $where[] = "( activityAssignment.assignee_contact_id =  $interviewerId )";
         }
+
+        if ( ! empty( $voterIds ) ) {
+            $where[] = "( activityTarget.target_contact_id IN ( " . implode( ',', $voterIds ) . " ) )";
+        }
+
         $whereClause = null;
-        if ( !empty( $where ) ) {
+        if ( ! empty( $where ) ) {
             $whereClause = ' AND ( '. implode( ' AND ', $where ) . ' )';
         }
         
         $actTypeId = CRM_Core_DAO::getFieldValue( 'CRM_Campaign_DAO_Survey', $surveyId, 'activity_type_id' ); 
-        if ( !$actTypeId ) return $activities;
-        
-        $query = "
+        if ( !$actTypeId ) {
+            return $activities;
+        }
+
+        if ( $count ) {
+            $select = "SELECT count(activity.id)";
+        } else {
+            $select = "
     SELECT  activity.id, activity.status_id, 
             activityTarget.target_contact_id as voter_id,
             activityAssignment.assignee_contact_id as interviewer_id,
             activity.result as result,
             activity.activity_date_time as activity_date_time
+";
+        }
+
+        $query = "
+            $select
       FROM  civicrm_activity activity
 INNER JOIN  civicrm_activity_target activityTarget ON ( activityTarget.activity_id = activity.id )
 INNER JOIN  civicrm_activity_assignment activityAssignment ON ( activityAssignment.activity_id = activity.id )
@@ -441,16 +461,22 @@ INNER JOIN  civicrm_activity_assignment activityAssignment ON ( activityAssignme
        AND  activity.activity_type_id = %2
        AND  ( activity.is_deleted IS NULL OR activity.is_deleted = 0 )
             $whereClause";
-        
-        $activity = CRM_Core_DAO::executeQuery( $query, array( 1 => array( $surveyId,  'Integer'),
-                                                               2 => array( $actTypeId, 'Integer' ) ) );
-        
+
+        $params = array( 1 => array( $surveyId,  'Integer'),
+                         2 => array( $actTypeId, 'Integer' ) );
+
+        if ( $count ) {
+            return CRM_Core_DAO::singleValueQuery( $query, $params );
+        }
+
+        $activity = CRM_Core_DAO::executeQuery( $query, $params );
+    
         while ( $activity->fetch( ) ) {
-            $activities[$activity->id] = array( 'id'             => $activity->id,
-                                                'voter_id'       => $activity->voter_id,
-                                                'status_id'      => $activity->status_id,
-                                                'interviewer_id' => $activity->interviewer_id,
-                                                'result'         => $activity->result,
+            $activities[$activity->id] = array( 'id'                 => $activity->id,
+                                                'voter_id'           => $activity->voter_id,
+                                                'status_id'          => $activity->status_id,
+                                                'interviewer_id'     => $activity->interviewer_id,
+                                                'result'             => $activity->result,
                                                 'activity_date_time' => $activity->activity_date_time );
         }
         
