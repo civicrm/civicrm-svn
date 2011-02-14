6f4576fed2d5baa36ea78533015c26fb205c5e79
Donald Lobo <lobo@civicrm.org>
2011-02-14 22:21:32 +0000

    fixes towards CRM-7523

--- tests/phpunit/WebTest/Grant/ContactContextAddTest.php
+++ tests/phpunit/WebTest/Grant/ContactContextAddTest.php
@@ -135,10 +135,10 @@ class WebTest_Grant_ContactContextAddTest extends CiviSeleniumTestCase {
         $this->waitForPageToLoad('30000'); 
         
         // verify if grant is created with presence of view link
-        $this->waitForElementPresent("xpath=//div[@id='Grants']//table/tbody/tr[2]/td[8]/span/a[text()='View']");
+        $this->waitForElementPresent("xpath=//div[@id='Grants']//table/tbody/tr[1]/td[8]/span/a[text()='View']");
         
         // click through to the Grant view screen
-        $this->click("xpath=//div[@id='Grants']//table/tbody/tr[2]/td[8]/span/a[text()='View']");
+        $this->click("xpath=//div[@id='Grants']//table/tbody/tr[1]/td[8]/span/a[text()='View']");
         $this->waitForElementPresent('_qf_GrantView_cancel-bottom');
         
         $gDate = date('F jS, Y', strtotime('now'));
