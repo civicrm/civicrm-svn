256c1515fed61faa553c4a09f29a0c653bb34b0b
Donald Lobo <lobo@civicrm.org>
2011-02-28 16:12:04 +0000

    fix CRM-7580

--- CRM/Mailing/Form/Upload.php
+++ CRM/Mailing/Form/Upload.php
@@ -395,7 +395,8 @@ class CRM_Mailing_Form_Upload extends CRM_Core_Form
         require_once 'CRM/Mailing/BAO/Mailing.php';
         CRM_Mailing_BAO_Mailing::create($params, $ids);
      
-        if ( $this->_submitValues['_qf_Upload_upload_save'] == 'Save & Continue Later' ) {
+        if ( isset($this->_submitValues['_qf_Upload_upload_save']) &&
+             $this->_submitValues['_qf_Upload_upload_save'] == 'Save & Continue Later' ) {
             //when user perform mailing from search context 
             //redirect it to search result CRM-3711.
             $ssID    = $this->get( 'ssID' );
