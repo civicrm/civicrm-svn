3f1fde3b50bd071748df4ff2eaf7af4d87585345
Donald Lobo <lobo@civicrm.org>
2011-03-02 04:53:40 +0000

    add more combinations

--- tests/phpunit/CRM/Contact/BAO/QueryTestDataProvider.php
+++ tests/phpunit/CRM/Contact/BAO/QueryTestDataProvider.php
@@ -53,6 +53,39 @@ class CRM_Contact_BAO_QueryTestDataProvider implements Iterator
                      'id' => array(  '17', '18', '19', '20', '21',
                                      '22', '23', '24' )
                      ),
+
+              //  Include static group 5
+              array( 'fv' => array( 'group' => array( '5' => 1 ) ),
+                     'id' => array( '13', '14', '15', '16', '21',
+                                    '22', '23', '24' )
+                     ),
+
+              //  Include static groups 3 and 5
+              array( 'fv' => array( 'group' => array( '3' => 1,
+                                                      '5' => 1 ) ),
+                     'id' => array( '13', '14', '15', '16', '17', '18',
+                                    '19', '20', '21', '22', '23', '24' )
+                     ),
+
+              //  Include tag 7
+              array( 'fv' => array( 'tag' => array( '7' => 1 ) ),
+                     'id' => array( '11', '12', '15', '16',
+                                    '19', '20', '23', '24' )
+                     ),
+
+              //  Include tag 9
+              array( 'fv' => array( 'tag' => array( '9' => 1 ) ),
+                     'id' => array( '10', '12', '14', '16',
+                                    '18', '20', '22', '24' )
+                     ),
+
+              //  Include tags 7 and 9
+              array( 'fv' => array( 'tag' => array( '7' => 1,
+                                                    '9' => 1 ) ),
+                     'id' => array( '10', '11', '12', '14', '15', '16',
+                                    '18', '19', '20', '22', '23', '24' )
+                     ),
+
             );
 
     public function _construct( )
