4f87561735b15e8b408e82f40b3837ece918fc4e
Xavier Dutoit <xavier@tttp.eu>
2011-03-09 22:12:44 +0000

    Now that michau fixed my phpunit, can add tests and run them.

---
+++ tests/phpunit/api/v3/OptionValueTest.php
@@ -0,0 +1,45 @@
+<?php
+require_once 'api/v3/OptionValue.php';
+require_once 'CiviTest/CiviUnitTestCase.php';
+
+class api_v3_OptionValueTest extends CiviUnitTestCase 
+{
+    protected $_apiversion;
+
+    function setUp() 
+    {
+        $this->_apiversion = 3;
+        parent::setUp();
+    }
+
+    function tearDown() 
+    {  
+    }
+
+    public function testGetOptionValueByID () {
+        $result = civicrm_api3_option_value_get(array('id'=> 1, 'version' => 3));
+        $this->assertEquals( 0, $result['is_error'], 'In line ' . __LINE__ );
+        $this->assertEquals( 1, $result['count'], 'In line ' . __LINE__ );
+        $this->assertEquals( 1, $result['id'], 'In line ' . __LINE__ );
+    }
+
+    public function testGetOptionValueByValue () {
+        $result = civicrm_api3_option_value_get(array('option_group_id'=> 1, 'value' => '1', 'version' => 3));
+        $this->assertEquals( 0, $result['is_error'], 'In line ' . __LINE__ );
+        $this->assertEquals( 1, $result['count'], 'In line ' . __LINE__ );
+        $this->assertEquals( 1, $result['id'], 'In line ' . __LINE__ );
+    }
+
+    public function testGetOptionGroup () {
+        $result = civicrm_api3_option_value_get(array('option_group_id'=> 1, 'version' => 3));
+        $this->assertEquals( 0, $result['is_error'], 'In line ' . __LINE__ );
+        $this->assertGreaterThan( 1, $result['count'], 'In line ' . __LINE__ );
+    }
+
+    public function testGetOptionDoesNotExist () {
+        $result = civicrm_api3_option_value_get(array('label'=> 'FSIGUBSFGOMUUBSFGMOOUUBSFGMOOBUFSGMOOIIB','version' => 3));
+        $this->assertEquals( 0, $result['is_error'], 'In line ' . __LINE__ );
+        $this->assertEquals( 0, $result['count'], 'In line ' . __LINE__ );
+    }
+}
+
