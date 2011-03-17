00e30502d77c2d9d244ee71617d30691e92c3f16
Xavier Dutoit <xavier@tttp.eu>
2011-03-17 06:48:38 +0000

    test civicrm_api get without param, with an empty param and with a param of the wrong type

--- tests/phpunit/api/v3/SyntaxConformanceAllEntitiesTest.php
+++ tests/phpunit/api/v3/SyntaxConformanceAllEntitiesTest.php
@@ -6,8 +6,20 @@ class api_v3_SyntaxConformanceAllEntities extends CiviUnitTestCase
 {
     protected $_apiversion;
 
+    /* they are two types of missing APIs:
+       - Those that are to be implemented 
+         (in some future version when someone steps in -hint hint-). List the entities in toBeImplemented[ {$action} ]
+       Those that don't exist
+         and that will never exist (eg an obsoleted Entity
+         they need to be returned by the function toBeSkipped_{$action} (because it has to be a static method and therefore couldn't access a this->toBeSkipped)
+    */
+
     function setUp()    {
        parent::setUp();
+       
+       $this->toBeImplemented['get'] = array ('UFGroup','UFField','CustomGroup','ParticipantPayment');
+       $this->toBeImplemented['create'] = array ();
+       $this->toBeImplemented['delete'] = array ();
     }
 
     function tearDown()    {
@@ -16,23 +28,25 @@ class api_v3_SyntaxConformanceAllEntities extends CiviUnitTestCase
 
     public static function entities_get () {
       // all the entities, beside the ones flagged
-      return api_v3_SyntaxConformanceAllEntities::entities (api_v3_SyntaxConformanceAllEntities::not_implemented_get_apis (true));
+      return api_v3_SyntaxConformanceAllEntities::entities (api_v3_SyntaxConformanceAllEntities::toBeSkipped_get (true));
     }
 
-    public static function entities($skip = array () ) {
-//        return array(array ('Tag'), array ('Group')  );
+    public static function entities($skip = NULL ) {
+//        return array(array ('Tag'), array ('Group')  ); // uncomment to make a quicker run when adding a test
         $tmp = civicrm_api ('Entity','Get', array ('version' => 3 ));
+        if (!is_array ($skip)) {
+          $skip = array();
+        }
         $tmp = array_diff ($tmp['values'],$skip);
         $entities = array ();
         foreach ($tmp as $e) {
-//          if ($e == "Entity") continue;//Entity is a fake Entity, so we skip it
           $entities [] = array ($e);
           
         }
         return $entities;
     }
 
-    public static function not_implemented_get_apis ($sequential = false) {
+    public static function toBeSkipped_get ($sequential = false) {
       $entitiesWithoutGet = array ('Mailer','MailerGroup','Location');
       if ($sequential === true) {
         return $entitiesWithoutGet;
@@ -49,7 +63,8 @@ class api_v3_SyntaxConformanceAllEntities extends CiviUnitTestCase
 
 /** testing the _get **/ 
     /**
-     * @dataProvider not_implemented_get_apis
+     * @dataProvider toBeSkipped_get
+       entities that don't need a get action
      */
     public function testNotImplemented_get ($Entity) {
         $result = civicrm_api ($Entity,'Get',array('version' => 3));
@@ -58,28 +73,40 @@ class api_v3_SyntaxConformanceAllEntities extends CiviUnitTestCase
     }
 
     /**
+     * @dataProvider entities
+     * @expectedException PHPUnit_Framework_Error
+     */
+    public function testWithoutParam_get ($Entity) {
+        // should get php complaining that a param is missing
+        $result = civicrm_api ($Entity,'Get');
+    }
+
+
+    /**
      * @dataProvider entities_get
      */
     public function testEmptyParam_get ($Entity) {
+
+        if (in_array ($Entity,$this->toBeImplemented['get'])) {
+          //$this->markTestSkipped("civicrm_api3_{$Entity}_get to be implemented");
+          $this->markTestIncomplete("civicrm_api3_{$Entity}_get to be implemented");
+          return;
+        }
         $result = civicrm_api ($Entity,'Get',array());
         $this->assertEquals( 1, $result['is_error'], 'In line ' . __LINE__ );
         $this->assertContains ("Mandatory key(s) missing from params array", $result['error_message']);
     }
 
+    /**
+     * @dataProvider entities
+     */
+    public function testGetWrongTypeParamTag_get () {
+        $result = civicrm_api ("Tag",'Get','this is not a string');
+        $this->assertEquals( 1, $result['is_error'], 'In line ' . __LINE__ );
+        $this->assertEquals ("Input variable `params` is not an array",$result['error_message']);
+    }
 
 /*
-    public function testGetWrongTypeParamTag_get () {
-        $result = civicrm_api ($this->EntityName,'Get','aaaaaaaaaaaaaaa');
-print_r ($result);
-        $this->assertEquals( 1, $result['is_error'], 'In line ' . __LINE__ );
-    }
-
-
-    public function testWithoutVersionTag_get () {
-        $result = civicrm_api ($this->EntityName,'Get',array());
-print_r ($result);
-        $this->assertEquals( 1, $result['is_error'], 'In line ' . __LINE__ );
-    }
 
     public function testSimpleTag_get () {
         $result = civicrm_api ($this->EntityName,'Get',array('version' => 3));
