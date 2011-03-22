2c8fa8a7e88fe9f3a459b7a5dbe31c3cab141c88
Rajan <rajan@civicrm.org>
2011-03-22 06:35:37 +0000

    # CRM-7309, access only active price fields on offile event registration ( apart from online form )

--- CRM/Event/Form/Registration.php
+++ CRM/Event/Form/Registration.php
@@ -690,12 +690,9 @@ class CRM_Event_Form_Registration extends CRM_Core_Form
     {
         // get price info
         require_once 'CRM/Price/BAO/Set.php';
-        
-        $validFieldsOnly = true;
-        if ( CRM_Utils_System::getClassName($form) == 'CRM_Event_Form_Participant' ) {
-            $validFieldsOnly = false;
-        }
-        $price = CRM_Price_BAO_Set::initSet( $form, $eventID, 'civicrm_event', $validFieldsOnly );
+         
+        // retrive all active price set fields.
+        $price = CRM_Price_BAO_Set::initSet( $form, $eventID, 'civicrm_event', true );
         
         if ( $price == false ) {
             require_once 'CRM/Core/OptionGroup.php'; 
