2dac354f40777242a1bd7e1e3212f2ec3e4b4905
Xavier Dutoit <xavier@tttp.eu>
2011-03-25 21:27:30 +0000

    add the create action

--- api/v3/OptionValue.php
+++ api/v3/OptionValue.php
@@ -20,4 +20,52 @@ function civicrm_api3_option_value_get( $params ) {
     }
 }
 
+/**
+ *  Add a OptionValue. OptionValues are used to classify CRM entities (including Contacts, Groups and Actions).
+ *
+ * Allowed @params array keys are:
+ * {@schema Core/OptionValue.xml}
+ * {@example OptionValueCreate.php}
+ * @return array of newly created option_value property values.
+ * @access public
+ */
+function civicrm_api3_option_value_create( $params ) 
+{
+  _civicrm_api3_initialize( true );
+  try {
+    $weight=0;
+    if ( !array_key_exists ('is_active', $params)) {
+      $params ['is_active'] = 1;
+    }
+    if ( !array_key_exists ('label', $params) && array_key_exists ('name', $params)) {
+      $params ['label'] = $params ['name']; // no idea why that's a "mandatory" field
+    }
+    if ( !CRM_Utils_Array::value( 'value', $params ) && array_key_exists ('option_group_id', $params)) {
+       require_once 'CRM/Utils/Weight.php';
+       $fieldValues = array('option_group_id' =>  $params['option_group_id']);
+       // use the next available value
+       /* CONVERT(value, DECIMAL) is used to convert varchar
+       field 'value' to decimal->integer                    */
+      $params['value'] = (int) CRM_Utils_Weight::getDefaultWeight('CRM_Core_DAO_OptionValue',
+                                                                 $fieldValues,
+                                                           'CONVERT(value, DECIMAL)');
+      $weight= $params['value'];
+    }
+    if ( !array_key_exists ('weight', $params)) {
+      $params ['weight'] = $params['value']; // no idea why that's a "mandatory" field
+    }
+    civicrm_api3_verify_mandatory ($params,'CRM_Core_BAO_OptionValue');
+    
+    require_once 'CRM/Core/BAO/OptionValue.php';
+    $option_valueBAO = CRM_Core_BAO_OptionValue::add($params);
+
+    $values = array( );
+    _civicrm_api3_object_to_array($option_valueBAO, $values[$option_valueBAO->id]);
+    return civicrm_api3_create_success($values,$params);
+  } catch (Exception $e) {
+    return civicrm_api3_create_error( $e->getMessage() );
+  }
+}
+
+
 ?>
