5e8ed29747c55d58889600ad721210fe90d77c0e
Xavier Dutoit <xavier@tttp.eu>
2011-03-28 09:28:23 +0000

    add create and delete actions (that always return an error, obviously)

--- api/v3/Entity.php
+++ api/v3/Entity.php
@@ -26,3 +26,24 @@ function civicrm_api3_entity_get ($params) {
     return civicrm_api3_create_error( $e->getMessage() );
   }
 }
+
+/**
+ *  Placeholder function. This should never be called, as it doesn't have any meaning
+ */
+function civicrm_api3_entity_create ($params) {
+  return civicrm_api3_create_error ("API (Entity,Create) does not exist Creating a new entity means modifying the source code of civiCRM.");
+}
+
+/**
+ *  Placeholder function. This should never be called, as it doesn't have any meaning
+ */
+function civicrm_api3_entity_delete ($params) {
+  return civicrm_api3_create_error ("API (Entity,Delete) does not exist Deleting an entity means modifying the source code of civiCRM.");
+}
+
+/**
+ *  Placeholder function. This should never be called, as it doesn't have any meaning
+ */
+function civicrm_api3_entity_getfields ($params) {
+  return civicrm_api3_create_error ("API (Entity,getfields) does not exist entity_get only returns the list of entities you can access from the API, no fields");
+}
