2d136342d42b83da4e53c7c260bb4f42fb0cb7c1
Dave Greenberg <dave@civicrm.org>
2011-04-19 01:40:29 +0000

    Login as admin to set user perms.

--- tests/phpunit/WebTest/Campaign/MailingTest.php
+++ tests/phpunit/WebTest/Campaign/MailingTest.php
@@ -47,33 +47,9 @@ class WebTest_Campaign_MailingTest extends CiviSeleniumTestCase {
         // class attributes.
         $this->open( $this->sboxPath );
         
-        // Logging in. Remember to wait for page to load. In most cases,
-        // you can rely on 30000 as the value that allows your test to pass, however,
-        // sometimes your test might fail because of this. In such cases, it's better to pick one element
-        // somewhere at the end of page and use waitForElementPresent on it - this assures you, that whole
-        // page contents loaded and you can continue your test execution.
-        $this->webtestLogin();
-        
-        // Create new group
-        $title = substr(sha1(rand()), 0, 7);
-        $groupName = "group_$title";
-        $this->addGroup( $groupName );
-        
-        // Adding contact
-        // We're using Quick Add block on the main page for this.
-        $firstName = substr(sha1(rand()), 0, 7);
-        $this->webtestAddContact( $firstName, "Smith", "$firstName.smith@example.org" );
-        
-        // add contact to group
-        // visit group tab
-        $this->click("css=li#tab_group a");
-        $this->waitForElementPresent("group_id");
-        
-        // add to group
-        $this->select("group_id", "label=$groupName");
-        $this->click("_qf_GroupContact_next");
-        $this->waitForPageToLoad("30000");
-        
+        // Log in as admin first to verify permissions for CiviGrant
+        $this->webtestLogin( true );        
+
         // Enable CiviCampaign module if necessary
         $this->open($this->sboxPath . "civicrm/admin/setting/component?reset=1");
         $this->waitForPageToLoad('30000');
@@ -95,6 +71,33 @@ class WebTest_Campaign_MailingTest extends CiviSeleniumTestCase {
         $this->click('edit-submit');
         $this->waitForPageToLoad();
         $this->assertTrue($this->isTextPresent('The changes have been saved.'));
+        
+        $this->open($this->sboxPath . "civicrm/logout&reset=1");
+        $this->waitForPageToLoad('30000');          
+
+        // Log in as demo user
+        $this->open( $this->sboxPath );
+        $this->webtestLogin( );
+        
+        // Create new group
+        $title = substr(sha1(rand()), 0, 7);
+        $groupName = "group_$title";
+        $this->addGroup( $groupName );
+        
+        // Adding contact
+        // We're using Quick Add block on the main page for this.
+        $firstName = substr(sha1(rand()), 0, 7);
+        $this->webtestAddContact( $firstName, "Smith", "$firstName.smith@example.org" );
+        
+        // add contact to group
+        // visit group tab
+        $this->click("css=li#tab_group a");
+        $this->waitForElementPresent("group_id");
+        
+        // add to group
+        $this->select("group_id", "label=$groupName");
+        $this->click("_qf_GroupContact_next");
+        $this->waitForPageToLoad("30000");
         
         // Go directly to the URL of the screen that you will be testing
         $this->open($this->sboxPath . "civicrm/campaign/add&reset=1");
