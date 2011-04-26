ff2b2dfae6574fe37ec19f7366ad99e92e321dd9
Kurund Jalmi <kurund@civicrm.org>
2011-04-26 16:36:39 +0000

    cleaned up case dashlets, CRM-7796

--- CRM/Dashlet/Page/MyCases.php
+++ CRM/Dashlet/Page/MyCases.php
@@ -50,6 +50,9 @@ class CRM_Dashlet_Page_MyCases extends CRM_Core_Page
      * @access public
      */
     function run( ) {
+        $context = CRM_Utils_Request::retrieve( 'context', 'String', $this, false, 'dashlet' );
+        $this->assign('context', $context );
+        
         require_once 'CRM/Case/BAO/Case.php';
         //check for civicase access.
         if ( !CRM_Case_BAO_Case::accessCiviCase( ) ) {
