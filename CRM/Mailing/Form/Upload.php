e39e4796a4ba1cc5900a3e33133e039e77891159
Donald Lobo <lobo@civicrm.org>
2011-05-04 00:25:42 +0000

    fix CRM-8010

--- CRM/Mailing/Form/Upload.php
+++ CRM/Mailing/Form/Upload.php
@@ -467,8 +467,9 @@ class CRM_Mailing_Form_Upload extends CRM_Core_Form
         $mailing->find(true);
 
         $session = CRM_Core_Session::singleton();
-        $values = array('contact_id' => $session->get('userID'), 'version' => 3);
-        $contact =& civicrm_api('contact', 'get', $values);
+        $values = array('contact_id' => $session->get('userID') );
+        require_once 'api/v2/Contact.php';
+        $contact =& civicrm_contact_get( $values );
         
         //CRM-4524
         $contact = reset( $contact );
