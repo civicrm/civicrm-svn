76c234d2d901ace723120706c650c1d833846459
Joseph Murray <joe.murray@jmaconsulting.biz>
2011-05-28 04:05:33 +0000

    CRM-8222 fix for civicrm_contact_update fails when contact_id but not contact_type passed

--- api/v2/Contact.php
+++ api/v2/Contact.php
@@ -82,8 +82,11 @@ function civicrm_contact_update( &$params, $create_new = false )
         return civicrm_create_error($e->getMessage());
     }
     require_once 'CRM/Utils/Array.php';
-    $contactID = CRM_Utils_Array::value( 'contact_id', $params );
-
+	$entityId = CRM_Utils_Array::value( 'contact_id', $params, null );
+    if ( ! CRM_Utils_Array::value('contact_type', $params) &&
+         $entityId ) {
+        $params['contact_type'] = CRM_Contact_BAO_Contact::getContactType( $entityId );
+    }
     $dupeCheck = CRM_Utils_Array::value( 'dupe_check', $params, false );
     $values    = civicrm_contact_check_params( $params, $dupeCheck );
     if ( $values ) {
@@ -92,7 +95,7 @@ function civicrm_contact_update( &$params, $create_new = false )
     
     if ( $create_new ) {
         // Make sure nothing is screwed up before we create a new contact
-        if ( !empty( $contactID ) ) {
+        if ( !empty( $entityId ) ) {
             return civicrm_create_error( 'Cannot create new contact when contact_id is present' );
         }
         if ( empty( $params[ 'contact_type' ] ) ) {
@@ -154,13 +157,7 @@ function civicrm_contact_update( &$params, $create_new = false )
         return $error;
     }
     
-    $values   = array( );
-    $entityId = CRM_Utils_Array::value( 'contact_id', $params, null );
-
-    if ( ! CRM_Utils_Array::value('contact_type', $params) &&
-         $entityId ) {
-        $params['contact_type'] = CRM_Contact_BAO_Contact::getContactType( $entityId );
-    }
+    $values   = array( );    
     
     if ( ! ( $csType = CRM_Utils_Array::value('contact_sub_type', $params) ) &&
          $entityId ) {
@@ -169,7 +166,7 @@ function civicrm_contact_update( &$params, $create_new = false )
     }
     
     $customValue = civicrm_contact_check_custom_params( $params, $csType ); 
-
+    
     if ( $customValue ) {
         return $customValue;
     }
@@ -177,7 +174,7 @@ function civicrm_contact_update( &$params, $create_new = false )
 
     $params = array_merge( $params, $values );
 
-    $contact =& _civicrm_contact_update( $params, $contactID );
+    $contact =& _civicrm_contact_update( $params, $entityId );
 
     if ( is_a( $contact, 'CRM_Core_Error' ) ) {
         return civicrm_create_error( $contact->_errors[0]['message'] );
@@ -353,7 +350,7 @@ function civicrm_contact_get( &$params, $deprecated_behavior = false )
     if ($deprecated_behavior) {
         return _civicrm_contact_get_deprecated($params);
     }
-    
+   
     // fix for CRM-7384 cater for soft deleted contacts
     $params['contact_is_deleted'] = 0;
     if (isset($params['showAll'])) {
@@ -367,7 +364,7 @@ function civicrm_contact_get( &$params, $deprecated_behavior = false )
             unset($params['contact_is_deleted']);
         }
     }
-
+    
     $inputParams      = array( );
     $returnProperties = array( );
     $otherVars = array( 'sort', 'offset', 'rowCount', 'smartGroupCache' );
