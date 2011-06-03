8fe928383ae22fff653787227abe19f50f3c7635
Donald Lobo <lobo@civicrm.org>
2011-06-03 22:43:02 +0000

    fix CRM-8073

--- CRM/Export/BAO/Export.php
+++ CRM/Export/BAO/Export.php
@@ -1416,6 +1416,8 @@ LIMIT $offset, $limit
                 $addressOptions = CRM_Core_BAO_Preferences::valueOptions( 'address_options', true, null, true );
                 if ( CRM_Utils_Array::value( 'supplemental_address_1', $addressOptions ) ) {
                     $addressWhereClause .= " AND ( (supplemental_address_1 IS NULL) OR (supplemental_address_1 = '') ) ";
+                    // enclose it again, since we are doing an AND in between a set of ORs
+                    $addressWhereClause = "( $addressWhereClause )";
                 }
             }
             
