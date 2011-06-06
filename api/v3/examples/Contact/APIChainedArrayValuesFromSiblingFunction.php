3fd848a14672c476be41d4e3d81b9d61cae2645f
Eileen McNaughton <eileen@mcnaughty.com>
2011-06-06 04:24:29 +0000

    latest examples

--- api/v3/examples/Contact/APIChainedArrayValuesFromSiblingFunction.php
+++ api/v3/examples/Contact/APIChainedArrayValuesFromSiblingFunction.php
@@ -58,7 +58,7 @@ function contact_create_expectedresult(){
           'preferred_communication_method' => '',
           'preferred_language' => 'en_US',
           'preferred_mail_format' => '',
-          'hash' => '3a290814e081738ad212e8e96a0df029',
+          'hash' => '9e0c76a968a8c449b02c3a77a85452e7',
           'api_key' => '',
           'first_name' => '',
           'middle_name' => '',
