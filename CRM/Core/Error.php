a6db6b6d950ee657b1fa5c41bed420e637f0c136
Xavier Dutoit <xavier@tttp.eu>
2011-06-08 18:47:17 +0000

    CRM-8270 get rid of the html when displaying an error from a pgm running from the shell on a fatal

--- CRM/Core/Error.php
+++ CRM/Core/Error.php
@@ -271,6 +271,11 @@ class CRM_Core_Error extends PEAR_ErrorStack {
             $message = ts('We experienced an unexpected error. Please post a detailed description and the backtrace on the CiviCRM forums: %1', array(1 => 'http://forum.civicrm.org/')); 
         }
 
+        if ( php_sapi_name() == "cli" ) {
+          print ("Sorry. A non-recoverable error has occurred.\n$message \n$code\n$email\n\n");
+          debug_print_backtrace();
+          die ("\n");
+        }
         $vars = array( 'message' => $message,
                        'code'    => $code );
 
