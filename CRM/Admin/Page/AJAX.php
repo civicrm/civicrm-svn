445193f3d2d4b58140691fbd434e83a85a919a04
Deepak Srivastava <deepak.srivastava.0303@gmail.com>
2011-06-27 14:04:14 +0000

    - towards CRM-8258

--- CRM/Admin/Page/AJAX.php
+++ CRM/Admin/Page/AJAX.php
@@ -303,4 +303,16 @@ class CRM_Admin_Page_AJAX
         echo json_encode( $tagInfo );
         CRM_Utils_System::civiExit( );
     } 
+
+
+    static function mergeTags( ) {
+        $fromId = CRM_Utils_Type::escape( $_POST['fromId'], 'Integer' );
+        $toId   = CRM_Utils_Type::escape( $_POST['toId'], 'Integer' );
+        
+        require_once 'CRM/Core/BAO/EntityTag.php';
+        CRM_Core_BAO_EntityTag::mergeTags( $fromId, $toId );
+
+        CRM_Utils_System::civiExit( );
+    } 
+
 }
