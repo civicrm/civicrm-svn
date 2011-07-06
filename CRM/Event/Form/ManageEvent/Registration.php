274d7c825c0e46a088ee50402049732b4e0ed246
Yashodha Chaku <yashodha.chaku@gmail.com>
2011-07-06 04:02:28 +0000

    #CRM-8248

--- CRM/Event/Form/ManageEvent/Registration.php
+++ CRM/Event/Form/ManageEvent/Registration.php
@@ -215,7 +215,6 @@ class CRM_Event_Form_ManageEvent_Registration extends CRM_Event_Form_ManageEvent
             $this->_showHide->addHide( 'thankyou' );
             $this->_showHide->addHide( 'additional_profile_pre' );
             $this->_showHide->addHide( 'additional_profile_post' );
-            $this->_showHide->addHide( 'additional_profile_post_multiple' );
             $this->_showHide->addHide( 'id-approval-text' );
         } else {
             $this->_showHide->addShow( 'confirm' );
@@ -228,7 +227,6 @@ class CRM_Event_Form_ManageEvent_Registration extends CRM_Event_Form_ManageEvent
             if ( ! $defaults['is_multiple_registrations']) {
                 $this->_showHide->addHide( 'additional_profile_pre' );
                 $this->_showHide->addHide( 'additional_profile_post' );
-                $this->_showHide->addHide( 'additional_profile_post_multiple' );
             }
             if ( ! CRM_Utils_Array::value( 'requires_approval', $defaults ) ) {
                 $this->_showHide->addHide( 'id-approval-text' );
