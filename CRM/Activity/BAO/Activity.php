72471f273dc6eafea41a07306e538fb80684eb8f
Tim Otten <totten@civicrm.org>
2011-08-29 20:58:37 +0000

    CRM-8708

--- CRM/Activity/BAO/Activity.php
+++ CRM/Activity/BAO/Activity.php
@@ -222,7 +222,7 @@ class CRM_Activity_BAO_Activity extends CRM_Activity_DAO_Activity
         
         $transaction->commit( );
         if ( isset( $activity ) ) {
-            require_once 'CRM/Activity/BAO/Activity.php';
+            require_once 'CRM/Case/BAO/Case.php';
             $activity->case_id = CRM_Case_BAO_Case::getCaseIdByActivityId($activity->id); // CRM-8708
             CRM_Utils_Hook::post( 'delete','Activity', $activity->id, $activity );
         }
