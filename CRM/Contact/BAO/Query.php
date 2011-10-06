e7787e27a09de2b0dad48bd598322f958aa31f1f
Eileen McNaughton <eileen@mcnaughty.com>
2011-10-06 21:57:49 +0000

    CRM-8997 - future relationship should be treated as inactive

--- CRM/Contact/BAO/Query.php
+++ CRM/Contact/BAO/Query.php
@@ -3236,11 +3236,13 @@ WHERE  id IN ( $groupIDs )
         //check for active, inactive and all relation status
         $today = date( 'Ymd' );
         if ( $relStatus[2] == 0 ) {
-            $this->_where[$grouping][] = "civicrm_relationship.is_active = 1 AND ( civicrm_relationship.end_date is NULL OR civicrm_relationship.end_date >= {$today} )";
+            $this->_where[$grouping][] = "civicrm_relationship.is_active = 1 AND ( civicrm_relationship.end_date is NULL OR civicrm_relationship.end_date >= {$today} 
+            																																 AND ( civicrm_relationship.start_date is NULL OR civicrm_relationship.start_date <= {$today} ))";
             $this->_qill[$grouping][]  = ts( 'Relationship - Active');
             
         } else if ( $relStatus[2] == 1 ) {
-            $this->_where[$grouping][] = "(civicrm_relationship.is_active = 0 OR civicrm_relationship.end_date < {$today})";
+            $this->_where[$grouping][] = "(civicrm_relationship.is_active = 0 OR civicrm_relationship.end_date < {$today}
+            																																	OR civicrm_relationship.start_date > {$today}	)";
             $this->_qill[$grouping][]  = ts( 'Relationship - Inactive');
         }
         $this->_where[$grouping][] = 'civicrm_relationship.relationship_type_id = '.$rel[0];
