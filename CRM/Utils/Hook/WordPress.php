1d5664a50b1e18f49dfa4e61acd742d63ad74919
Kurund Jalmi <kurund@civicrm.org>
2011-10-07 18:31:16 +0000

    final fix, CRM-8354

--- CRM/Utils/Hook/WordPress.php
+++ CRM/Utils/Hook/WordPress.php
@@ -1,5 +1,4 @@
 <?php 
-
 /*
  +--------------------------------------------------------------------+
  | CiviCRM version 4.0                                                |
@@ -38,7 +37,7 @@ require_once 'CRM/Utils/Hook.php';
 
 class CRM_Utils_Hook_WordPress extends CRM_Utils_Hook {
 
-   static function invoke( $numParams,
+   function invoke( $numParams,
                            &$arg1, &$arg2, &$arg3, &$arg4, &$arg5,
                            $fnSuffix ) {
        return $this->commonInvoke( $numParams,
