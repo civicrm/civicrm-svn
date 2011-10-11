28d2691cd1259f79e87e83487cd4f6de5aaa216a
Dave Greenberg <dave@civicrm.org>
2011-10-11 22:35:01 +0000

    CRM-8796

--- CRM/Contribute/BAO/Query.php
+++ CRM/Contribute/BAO/Query.php
@@ -286,6 +286,7 @@ class CRM_Contribute_BAO_Query
             return;
 
         case 'contribution_in_honor_of':
+        case 'contribution_honor_contact_name':
             $name    = trim( $value ); 
             $newName = str_replace(',' , " " ,$name );
             $pieces  =  explode( ' ', $newName ); 
