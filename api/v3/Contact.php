c2303b0ec378796e30e027fe6ec5a6286a7c4f20
Dave Greenberg <dave@civicrm.org>
2011-10-12 13:59:58 +0000

    Fix typo in scheduledReminder

--- api/v3/Contact.php
+++ api/v3/Contact.php
@@ -860,7 +860,7 @@ function civicrm_api3_contact_geocode( $params )
 function civicrm_api3_contact_scheduledReminder( $params )
 {
     require_once 'CRM/Core/BAO/ScheduleReminders.php';
-    $result = CRM_Core_BAO_ScheduleReminders::processQueue( CRM_Utils_Array::value( 'now', $params _) );
+    $result = CRM_Core_BAO_ScheduleReminders::processQueue( CRM_Utils_Array::value( 'now', $params) );
 
     if ( $result['is_error'] == 0 ) {
         return civicrm_api3_create_success( );
