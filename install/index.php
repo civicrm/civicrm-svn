79435a2afb5095324fe6ad3dd09e54cc0b9fce01
Donald Lobo <lobo@civicrm.org>
2011-10-15 20:10:48 +0000

    fix CRM-9042

--- install/index.php
+++ install/index.php
@@ -975,18 +975,28 @@ function civicrm_install_set_drupal_perms() {
     if ( ! function_exists('db_select') ) {
         db_query( 'UPDATE {permission} SET perm = CONCAT( perm, \', access CiviMail subscribe/unsubscribe pages, access all custom data, access uploaded files, make online contributions, profile listings and forms, register for events, view event info, view event participants\') WHERE rid IN (1, 2)' );
     } else {
-        //this isn't working yet for D7:
         $perms =
             array(
-                  'access CiviMail subscribe/unsubscribe pages',
                   'access all custom data',
                   'access uploaded files',
-                  'make online contributions',
                   'profile listings and forms',
+                  'make online contributions',
                   'register for events',
                   'view event info',
                   'view event participants',
+                  'access CiviMail subscribe/unsubscribe pages',
                   );
+
+        // Adding a permission that has not yet been assigned to a module by
+        // a hook_permission implementation results in a database error.
+        // CRM-9042
+        $allPerms = array_keys(module_invoke_all('permission'));
+        foreach ( array_diff($perms, $allPerms) as $perm) {
+            watchdog('civicrm', 
+                     'Cannot grant the %perm permission because it does not yet exist.',
+                     array('%perm' => $perm), WATCHDOG_ERROR);
+        }
+        $perms = array_intersect($perms, $allPerms);
         user_role_grant_permissions( DRUPAL_AUTHENTICATED_RID, $perms );
         user_role_grant_permissions( DRUPAL_ANONYMOUS_RID    , $perms );    
     }
