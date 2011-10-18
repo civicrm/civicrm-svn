607885fb3880bbab8b2623015d3d8fbe34687a9f
Rohan Katkar <rohan.katkar@webaccessglobal.com>
2011-10-18 14:14:39 +0000

    CRM-9008 Minor fix.

--- CRM/Upgrade/Page/Upgrade.php
+++ CRM/Upgrade/Page/Upgrade.php
@@ -172,12 +172,13 @@ SELECT  count( id ) as statusCount
             // set pre-upgrade warnings if any -
             self::setPreUpgradeMessage( $preUpgradeMessage, $currentVer, $latestVer );
             
-            // check for changed message templates
-            self::checkMessageTemplate( &$template, &$preUpgradeMessage, $latestVer, $currentVer );
-
             //turning some tables to monolingual during 3.4.beta3, CRM-7869
             $upgradeTo   = str_replace( '4.0.', '3.4.', $latestVer  );
             $upgradeFrom = str_replace( '4.0.', '3.4.', $currentVer );
+            
+            // check for changed message templates
+            self::checkMessageTemplate( &$template, &$preUpgradeMessage, $upgradeTo, $upgradeFrom );
+
             if ( $upgrade->multilingual && 
                  version_compare( $upgradeFrom, '3.4.beta3'  ) == -1 &&
                  version_compare( $upgradeTo,   '3.4.beta3'  ) >=  0  ) {
