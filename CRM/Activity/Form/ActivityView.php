139235800611518b84d47a0d61c0ac5a732434e1
Kurund Jalmi <kurund@civicrm.org>
2011-10-26 09:35:19 +0000

    fixed CRM-8653

--- CRM/Activity/Form/ActivityView.php
+++ CRM/Activity/Form/ActivityView.php
@@ -64,7 +64,7 @@ class CRM_Activity_Form_ActivityView extends CRM_Core_Form
         }
         
         $session = CRM_Core_Session::singleton();
-        if ( $context != 'home' ) {
+        if ( !in_array( $context, array( 'home', 'dashlet', 'dashletFullscreen' ) ) ) {
             $url = CRM_Utils_System::url( 'civicrm/contact/view', "reset=1&cid={$cid}&selectedChild=activity");
         } else {
             $url = CRM_Utils_System::url('civicrm/dashboard', 'reset=1');
