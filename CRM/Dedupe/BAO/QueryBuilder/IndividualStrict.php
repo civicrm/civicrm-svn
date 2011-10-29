203862f8b7898a87bbcd2eb106a058443b0c281d
Donald Lobo <lobo@civicrm.org>
2011-10-29 13:59:41 +0000

    fix CRM-9112

--- CRM/Dedupe/BAO/QueryBuilder/IndividualStrict.php
+++ CRM/Dedupe/BAO/QueryBuilder/IndividualStrict.php
@@ -2,7 +2,6 @@
 
 require_once 'CRM/Dedupe/BAO/QueryBuilder.php';
 
-// TODO: How to handle NULL values/records?
 class CRM_Dedupe_BAO_QueryBuilder_IndividualStrict extends CRM_Dedupe_BAO_QueryBuilder {
 
     static function record($rg) {
@@ -37,6 +36,43 @@ class CRM_Dedupe_BAO_QueryBuilder_IndividualStrict extends CRM_Dedupe_BAO_QueryB
               AND ".self::internalFilters($rg);
         return array("civicrm_contact.{$rg->name}.{$rg->threshold}" => $query);
     }
+
+    /**
+     * An alternative version which might perform a lot better
+     * than the above. Will need to do some testing
+     */
+    static function internalOptimized( $rg ) {
+        $sql = "
+CREATE TEMPORARY TABLE emails (
+                               email varchar(255),
+                               contact_id1 int,
+                               contact_id2 int,
+                               INDEX(contact_id1),
+                               INDEX(contact_id2)
+                              ) ENGINE=MyISAM
+";
+        CRM_Core_DAO::executeQuery( $sql );
+
+        $sql = "
+INSERT INTO emails
+    SELECT email1.email as email, email1.contact_id as contact_id1, email2.contact_id as contact_id2
+    FROM civicrm_email as email1
+    JOIN civicrm_email as email2 USING (email)
+    WHERE email1.contact_id < email2.contact_id
+";
+        CRM_Core_DAO::executeQuery( $sql );
+
+        $query = "
+SELECT contact_id1, contact_id2, email
+FROM   emails
+JOIN   civicrm_contact as contact1 on contact1.id=contact_id1
+JOIN   civicrm_contact as contact2 on contact2.id=contact_id2
+WHERE  contact1.contact_type='Individual' 
+AND    contact2.contact_type='Individual'
+AND    " . self::internalFilters($rg);
+        return array("civicrm_contact.{$rg->name}.{$rg->threshold}" => $query);
+    }
+
 };
 
 ?>
