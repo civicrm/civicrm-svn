1f7c816184f12289e4e5959bd9a1e6de75205598
Dave Greenberg <dave@civicrm.org>
2011-11-09 14:53:46 +0000

    CRM-9121

---
+++ CRM/Admin/Form/Preferences/Event.php
@@ -0,0 +1,128 @@
+<?php
+
+/*
+ +--------------------------------------------------------------------+
+ | CiviCRM version 4.0                                                |
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC (c) 2004-2011                                |
+ +--------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                    |
+ |                                                                    |
+ | CiviCRM is free software; you can copy, modify, and distribute it  |
+ | under the terms of the GNU Affero General Public License           |
+ | Version 3, 19 November 2007 and the CiviCRM Licensing Exception.   |
+ |                                                                    |
+ | CiviCRM is distributed in the hope that it will be useful, but     |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+ | See the GNU Affero General Public License for more details.        |
+ |                                                                    |
+ | You should have received a copy of the GNU Affero General Public   |
+ | License and the CiviCRM Licensing Exception along                  |
+ | with this program; if not, contact CiviCRM LLC                     |
+ | at info[AT]civicrm[DOT]org. If you have questions about the        |
+ | GNU Affero General Public License or the licensing of CiviCRM,     |
+ | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
+ +--------------------------------------------------------------------+
+*/
+
+/**
+ *
+ * @package CRM
+ * @copyright CiviCRM LLC (c) 2004-2011
+ * $Id: Display.php 36505 2011-10-03 14:19:56Z lobo $
+ *
+ */
+
+require_once 'CRM/Admin/Form/Preferences.php';
+
+/**
+ * This class generates form components for the display preferences
+ * 
+ */
+class CRM_Admin_Form_Preferences_Mailing extends CRM_Admin_Form_Preferences
+{
+    function preProcess( ) {
+        CRM_Utils_System::setTitle(ts('CiviMail Component Settings'));
+        $this->_varNames = 
+            array( CRM_Core_BAO_Setting::MAILING_PREFERENCES_NAME =>
+                   array( 
+                         'profile_double_optin'               => array( 'html_type'    => 'checkbox',
+                                                                        'title'        => ts( 'Enable Double Opt-in for Profile Group(s) field' ),
+                                                                        'weight'       => 1,
+                                                                        'description'  => ts( 'When CiviMail is enabled, users who "subscribe" to a group from a profile Group(s) checkbox will receive a confirmation email. They must respond (opt-in) before they are added to the group.'),
+                                                                        ),
+                         'profile_add_to_group_double_optin'  => array( 'html_type'    => 'checkbox',
+                                                                        'title'        => ts( 'Enable Double Opt-in for Profiles which use the "Add to Group" setting' ),
+                                                                        'weight'       => 2,
+                                                                        'description'  => ts( 'When CiviMail is enabled and a profile uses the "Add to Group" setting, users who complete the profile form will receive a confirmation email. They must respond (opt-in) before they are added to the group.'),
+                                                                        ),
+                         'track_civimail_replies'             => array( 'html_type'    => 'checkbox',
+                                                                        'title'        => ts( 'Track CiviMail replies using VERP in Reply-To header' ),
+                                                                        'weight'       => 3,
+                                                                        'description'  => null,
+                                                                        ),
+                         'civimail_workflow'                  => array( 'html_type'    => 'checkbox',
+                                                                        'title'        => ts( 'Enable workflow support for CiviMail' ),
+                                                                        'weight'       => 4,
+                                                                        'description'  => ts( 'Drupal-only. Rules module must be enabled (beta feature - use with caution).' ),
+                                                                        ),
+                         'civimail_server_wide_lock'          => array( 'html_type'    => 'checkbox',
+                                                                        'title'        => ts( 'Enable global server wide lock for CiviMail' ),
+                                                                        'weight'       => 5,
+                                                                        'description'  => null,
+                                                                        ),
+                          )
+                   );
+
+        parent::preProcess( );
+    }
+
+    function setDefaultValues( ) {
+        $defaults = array( );
+
+        foreach ( $this->_varNames as $groupName => $settings ) {
+            foreach ( $settings as $settingName => $dontcare ) {
+                $defaults[$settingName] = 
+                    isset( $this->_config->$settingName ) ?
+                    $this->_config->$settingName :
+                    null;
+            }
+        }
+
+        return $defaults;
+    }
+
+    /**
+     * Function to build the form
+     *
+     * @return None
+     * @access public
+     */
+    public function buildQuickForm( ) 
+    {
+        parent::buildQuickForm( );
+    }
+
+       
+    /**
+     * Function to process the form
+     *
+     * @access public
+     * @return None
+     */
+    public function postProcess() 
+    {
+        $config = CRM_Core_Config::singleton();
+        if ( $this->_action == CRM_Core_Action::VIEW ) {
+            return;
+        }
+
+        $this->_params = $this->controller->exportValues( $this->_name );
+        
+        parent::postProcess( );
+    }//end of function
+
+}
+
+
