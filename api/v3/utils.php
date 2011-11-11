448ba10e4bb8f90f8a49df2545a6e8afef3c1080
Eileen McNaughton <eileen@mcnaughty.com>
2011-11-11 03:06:48 +0000

    e-notice fix

--- api/v3/utils.php
+++ api/v3/utils.php
@@ -940,7 +940,7 @@ function _civicrm_api3_swap_out_aliases(&$apiRequest ) {
            }
         }
         }elseif(empty($apiRequest['params'][$field]) && CRM_Utils_Array::value('name', $values) && $field != $values['name']){
-          $apiRequest['params'][$field] = $apiRequest['params'][$values['name']];
+          $apiRequest['params'][$field] = CRM_Utils_Array::value($values['name'],$apiRequest['params']);
           // note that it would make sense to unset the original field here but tests need to be in place first
         }
     }
