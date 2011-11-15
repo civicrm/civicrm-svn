4f4afb235215d8b8e6b5d9adabdd246fc2b1972a
Eileen McNaughton <eileen@mcnaughty.com>
2011-11-15 22:23:27 +0000

    remove call to deprecated api_include function

--- CRM/Contribute/Page/UserDashboard.php
+++ CRM/Contribute/Page/UserDashboard.php
@@ -93,7 +93,8 @@ class CRM_Contribute_Page_UserDashboard extends CRM_Contact_Page_View_UserDashBo
             // note that we are passing a CRM_Core_Page object ($this) as if it were a form here:
             $paymentObject = CRM_Core_Payment::singleton( $mode, $paymentProcessor, $this );
             
-            civicrm_api_include( 'utils', false, 3 );
+            require_once 'api/v3/utils.php';
+            //@todo calling api functions directly is not supported
             _civicrm_api3_object_to_array( $recur, $values );
 
             $values['cancelSubscriptionUrl'] = $paymentObject->cancelSubscriptionURL( );
