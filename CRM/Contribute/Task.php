1baf9161cfe72bc5e4b730d384a97c433999ea3d
Kurund Jalmi <kurund@civicrm.org>
2011-11-23 10:43:57 +0000

    fixed, CRM-9224

--- CRM/Contribute/Task.php
+++ CRM/Contribute/Task.php
@@ -107,10 +107,12 @@ class CRM_Contribute_Task
             if ( !CRM_Core_Permission::check( 'delete in CiviContribute' ) ) {
                 unset( self::$_tasks[1] );
             }
+        
+            require_once 'CRM/Utils/Hook.php';
+            CRM_Utils_Hook::searchTasks( 'contribution', self::$_tasks );
+            asort( self::$_tasks );
         }
-        require_once 'CRM/Utils/Hook.php';
-        CRM_Utils_Hook::searchTasks( 'contribution', self::$_tasks );
-        asort( self::$_tasks );
+        
         return self::$_tasks;
     }
 
