6daa338e5b26fb113fef8b619119a29876427b12
Donald Lobo <lobo@civicrm.org>
2011-12-08 02:08:08 +0000

    fix CRM-9310

--- CRM/Contact/Form/Edit/Address.php
+++ CRM/Contact/Form/Edit/Address.php
@@ -264,7 +264,7 @@ class CRM_Contact_Form_Edit_Address
      * @access public
      * @static
      */
-    static function formRule( $fields, $errors )
+    static function formRule( $fields, $files )
     {
         $errors = array( );
         // check for state/county match if not report error to user.
