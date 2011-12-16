220df833d7b422afbf2c1ed262a9fd71eb7ed901
Yashodha Chaku <yashodha.chaku@gmail.com>
2011-12-16 16:53:46 +0000

    CRM-8714 (webtest)

--- tests/phpunit/WebTest/Contact/CustomDataAddTest.php
+++ tests/phpunit/WebTest/Contact/CustomDataAddTest.php
@@ -173,6 +173,97 @@ class WebTest_Contact_CustomDataAddTest extends CiviSeleniumTestCase {
       $this->click("xpath=//table//tr/td/label[text()=\"$radioOptionLabel3\"]");
       $this->click("_qf_Contact_upload_view");
       $this->waitForPageToLoad("30000");
+  }
+  
+  function testCustomDataMoneyAdd( )
+  {
+      // This is the path where our testing install resides. 
+      // The rest of URL is defined in CiviSeleniumTestCase base class, in
+      // class attributes.
+      $this->open( $this->sboxPath );
+      
+      // Logging in. Remember to wait for page to load. In most cases,
+      // you can rely on 30000 as the value that allows your test to pass, however,
+      // sometimes your test might fail because of this. In such cases, it's better to pick one element
+      // somewhere at the end of page and use waitForElementPresent on it - this assures you, that whole
+      // page contents loaded and you can continue your test execution.
+      $this->webtestLogin( );
+      $this->waitForPageToLoad("30000");
+
+      // Go directly to the URL of the screen that you will be testing (New Custom Group).
+      $this->open($this->sboxPath . "civicrm/admin/custom/group?action=add&reset=1");
+
+      $this->waitForPageToLoad("30000");
+      
+      //fill custom group title
+      $customGroupTitle = 'custom_'.substr(sha1(rand()), 0, 7);
+      $this->waitForElementPresent("title");
+      $this->click("title");
+      $this->type("title", $customGroupTitle);
+
+      //custom group extends 
+      $this->click("extends[0]");
+      $this->select("extends[0]", "value=Contact");
+      $this->click("//option[@value='Contact']");
+      $this->click("_qf_Group_next-bottom");
+      $this->waitForElementPresent("_qf_Field_cancel-bottom");
+
+      //Is custom group created?
+      $this->assertTrue($this->isTextPresent("Your custom field set '{$customGroupTitle}' has been added. You can add custom fields now."));
+
+      //add custom field - money text
+      $moneyTextFieldLabel = 'money'.substr(sha1(rand()), 0, 4);
+      $this->click("label");
+      $this->type("label", $moneyTextFieldLabel);
+      $this->click("data_type[0]");
+      $this->select("data_type[0]", "value=Money");
+
+      $this->click("data_type[1]");
+      $this->select("data_type[1]", "value=Text");
+
+ 
+      //enter pre help message
+      $this->type("help_pre", "this is field pre help");
+
+      //enter post help message
+      $this->type("help_post", "this field post help");
+
+      //Is searchable?
+      $this->click("is_searchable");
+
+      //clicking save
+      $this->click("_qf_Field_next");
+      $this->waitForPageToLoad("30000");
+
+      //Is custom field created?
+      $this->assertTrue($this->isTextPresent("Your custom field '$moneyTextFieldLabel' has been saved."));
+
+     
+      //create Individual contact
+      $this->click("//ul[@id='civicrm-menu']/li[4]");
+      $this->click("//div[@id='root-menu-div']/div[5]/ul/li[1]/div/a");
+      $this->waitForPageToLoad("30000");
+
+      //expand all tabs
+      $this->click("expand");
+      $this->waitForElementPresent("address_1_street_address");
+      
+      //fill first name, last name, email id
+      $firstName = 'Ma'.substr(sha1(rand()), 0, 4);
+      $lastName  = 'An'.substr(sha1(rand()), 0, 7);
+      $emailId   = substr(sha1(rand()), 0, 7).'@web.com';
+      $this->click("first_name");
+      $this->type("first_name", $firstName);
+      $this->type("last_name", $lastName);
+      $this->type("email_1_email", $emailId);
+      
+      //fill custom values for the contact
+      $this->click("xpath=//table//tr/td/label[text()=\"$checkboxOptionLabel2\"]");
+      $this->click("xpath=//table//tr/td/label[text()=\"$radioOptionLabel3\"]");
+      $this->click("_qf_Contact_upload_view");
+      $this->waitForPageToLoad("30000");
+      $this->waitForElementPresent("address_1_street_address");
+    
   }  
 }
 ?>
