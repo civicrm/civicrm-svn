afefe516995269f1be39c68238294bc77e74023d
Donald Lobo <lobo@civicrm.org>
2011-12-27 04:55:46 +0000

    fix hook code so we follow same pattern for CRM-9395

--- CRM/Utils/Hook/Joomla.php
+++ CRM/Utils/Hook/Joomla.php
@@ -51,10 +51,6 @@ class CRM_Utils_Hook_Joomla extends CRM_Utils_Hook {
             $app = JFactory::getApplication();
             $app->triggerEvent( $fnSuffix,
                                 array( &$arg1, &$arg2, &$arg3, &$arg4, &$arg5 ) );
-            
-            return $this->commonInvoke( $numParams,
-                                       $arg1, $arg2, $arg3, $arg4, $arg5,
-                                        $fnSuffix, 'joomla' );
         }
     }
 }
