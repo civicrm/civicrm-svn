0df1b73bceb3c542e7c11ad48b13e4f1d087fdfe
Yashodha Chaku <yashodha.chaku@gmail.com>
2012-01-05 05:40:10 +0000

    CRM-9431

--- CRM/Contact/Form/Search/Criteria.php
+++ CRM/Contact/Form/Search/Criteria.php
@@ -293,7 +293,7 @@ class CRM_Contact_Form_Search_Criteria {
                         + CRM_Core_PseudoConstant::$select( );
                     $element = $form->addElement('select', $name, $title, $selectElements );   
                 } else if ( $select == 'county' ) { 
-                    if ( $form->_submitValues['state_province'] ) {
+                    if ( !CRM_Utils_System::isNull( $form->_submitValues['state_province'] )  ) {
                         $selectElements = array( '' => ts('- select -') ) 
                             + CRM_Core_PseudoConstant::countyForState( $form->_submitValues['state_province']   );
                     }
