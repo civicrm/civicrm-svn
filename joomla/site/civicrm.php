f2cf4323ee829a0c5f50002cc18a4a8140d6f56d
Donald Lobo <lobo@civicrm.org>
2012-01-11 04:44:00 +0000

    fix CRM-9461

--- joomla/site/civicrm.php
+++ joomla/site/civicrm.php
@@ -52,7 +52,6 @@ function civicrm_invoke( ) {
     if ( CRM_Utils_Array::value( 'Itemid', $_GET ) ) {
         $component  = JComponentHelper::getComponent('com_civicrm');
         $menu       = JSite::getMenu( );
-        $item       =  $menu->getItems('componentid', $component->id, true);
         $params     =  $menu->getParams( $_GET['Itemid'] );
         $args = array( 'task', 'id', 'gid', 'pageId', 'action', 'csid' ); 
 		$view =  CRM_Utils_Array::value( 'view', $_GET );
