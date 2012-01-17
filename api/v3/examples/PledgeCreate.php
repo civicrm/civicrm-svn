8fbecaa441648004c1ec9f9eb4ed529c9fa13a99
Kurund Jalmi <kurund@civicrm.org>
2012-01-17 09:28:47 +0000

    worked on CRM-9469

--- api/v3/examples/PledgeCreate.php
+++ api/v3/examples/PledgeCreate.php
@@ -8,9 +8,9 @@
 function pledge_create_example(){
 $params = array( 
   'contact_id' => 1,
-  'pledge_create_date' => '20111031',
-  'start_date' => '20111031',
-  'scheduled_date' => '20111102',
+  'pledge_create_date' => '20120116',
+  'start_date' => '20120116',
+  'scheduled_date' => '20120118',
   'amount' => '100',
   'pledge_status_id' => '2',
   'pledge_contribution_type_id' => '1',
@@ -52,8 +52,8 @@ function pledge_create_expectedresult(){
           'frequency_interval' => 5,
           'frequency_day' => 15,
           'installments' => 5,
-          'start_date' => '20111031000000',
-          'create_date' => '20111031000000',
+          'start_date' => '20120116000000',
+          'create_date' => '20120116000000',
           'acknowledge_date' => '',
           'modified_date' => '',
           'cancel_date' => '',
