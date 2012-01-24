d783035af113bb0a63a1476f88f5b6bc1d3eb0a9
Yashodha Chaku <yashodha.chaku@gmail.com>
2012-01-24 05:51:35 +0000

    CRM-9529

---
+++ CRM/Utils/Migrate/ImportJSON.php
@@ -0,0 +1,81 @@
+<?php
+
+/*
+ +--------------------------------------------------------------------+
+ | CiviCRM version 4.1                                                |
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC (c) 2004-2011                                |
+ +--------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                    |
+ |                                                                    |
+ | CiviCRM is free software; you can copy, modify, and distribute it  |
+ | under the terms of the GNU Affero General Public License           |
+ | Version 3, 19 November 2007 and the CiviCRM Licensing Exception.   |
+ |                                                                    |
+ | CiviCRM is distributed in the hope that it will be useful, but     |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+ | See the GNU Affero General Public License for more details.        |
+ |                                                                    |
+ | You should have received a copy of the GNU Affero General Public   |
+ | License and the CiviCRM Licensing Exception along                  |
+ | with this program; if not, contact CiviCRM LLC                     |
+ | at info[AT]civicrm[DOT]org. If you have questions about the        |
+ | GNU Affero General Public License or the licensing of CiviCRM,     |
+ | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
+ +--------------------------------------------------------------------+
+*/
+
+/**
+ *
+ * @package CRM
+ * @copyright CiviCRM LLC (c) 2004-2011
+ * $Id$
+ *
+ */
+
+class CRM_Utils_Migrate_ImportJSON {
+
+    function __construct( ) {
+    }
+
+    function run( $file ) {
+               
+        $file = "/users/yashodha/svn/civicrm/CRM/Utils/Migrate/contact.json";
+        $json =  file_get_contents($file);
+        $decodedContacts = json_decode($json);
+        
+        $this->mapContacts( $decodedContacts );  
+        EXIT();
+
+        // clean up all caches etc
+        CRM_Core_Config::clearDBCache( );
+    }
+
+    function copyContactData( &$contact ) {
+        require_once 'CRM/Contact/DAO/Contact.php';
+        $dao = new CRM_Contact_DAO_Contact;
+        $fields =& $dao->fields( );
+        
+        foreach ( $fields as $name => $dontCare ) {
+            if ( isset( $contact->$name ) ) {
+                $dao->$name = (string ) $contact->$name;
+            }
+        }
+        if ( $save ) {
+            $dao->save( );
+        }
+
+        return true;
+    }
+
+    function mapContacts( &$contacts ) {
+        
+        foreach ( $contacts as $contact ) {
+            $this->copyContactData( $contact );
+            CRM_CORE_ERROR::DEBUG( "contact", $contact );
+        }
+    }
+    
+    
+}
