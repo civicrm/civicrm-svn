9ef7c60af4c7e33aff709f65b5ffde1d2ead4799
Jamie McClelland <jamie@progressivetech.org>
2012-02-06 21:22:07 +0000

    CRM-9608

--- CRM/Event/Form/Participant.php
+++ CRM/Event/Form/Participant.php
@@ -1372,7 +1372,7 @@ loadCampaign( {$this->_eID}, {$eventCampaigns} );
         }
 
         //do cleanup line  items if participant edit the Event Fee.
-        if ( ( $this->_lineItem || !isset($params['proceSetId'] ) ) && !$this->_paymentId ) {
+        if ( ( $this->_lineItem || !isset($params['proceSetId'] ) ) && !$this->_paymentId && isset($params['participantid'])  ) {
             require_once 'CRM/Price/BAO/LineItem.php';
             CRM_Price_BAO_LineItem::deleteLineItems( $params['participant_id'], 'civicrm_participant' );
         }
