213362279b504ed601790b8be381707b0cf4b7c8
Pratik Joshi <pratik.joshi@webaccess.co.in>
2012-02-10 11:59:31 +0000

    -- CRM-9631 fix

--- CRM/Event/BAO/Event.php
+++ CRM/Event/BAO/Event.php
@@ -1056,6 +1056,7 @@ WHERE civicrm_event.is_active = 1
                                                                            $isTest, 
                                                                            true,
                                                                            $participantParams );  
+                        list( $profileValues ) = $profileValues;
                         $val = array(
                                      'id'     => $gId,
                                      'values' => $profileValues,
