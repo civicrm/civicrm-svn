c9cef38e758b1dd3e31bd0039675b2b163e22b8e
Eileen McNaughton <eileen@mcnaughty.com>
2012-03-13 23:45:00 +0000

    make subject required (fix for failing test testActivityCreateMissingSubject)

--- api/v3/Activity.php
+++ api/v3/Activity.php
@@ -191,8 +191,7 @@ function civicrm_api3_activity_create( $params ) {
  * and is used for pre-filling defaults and ensuring mandatory requirements are met.
  */
 function _civicrm_api3_activity_create_spec(&$params){
-    $params['id'] = CRM_Utils_Array::value( 'activity_id', $params );
-    unset ($params['activity_id']);
+    $params['subject']['api.required'] =1;
     $params['assignee_contact_id'] = array('name' => 'assignee_id',
                                            'title' => 'assigned to',
                                            'type' => 1,
