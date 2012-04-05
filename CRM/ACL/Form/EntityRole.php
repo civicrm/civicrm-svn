55bdb2b925e78f63c6b1ef7de336e45c77a108c6
Eileen McNaughton <eileen@mcnaughty.com>
2012-04-05 22:15:12 +0000

    Implement new coding whitespace standard CRM-9979

--- CRM/ACL/Form/EntityRole.php
+++ CRM/ACL/Form/EntityRole.php
@@ -1,4 +1,5 @@
 <?php
+// $Id$
 
 /*
  +--------------------------------------------------------------------+
@@ -34,7 +35,6 @@
  *
  */
 
-
 /**
  *
  * @package CRM
@@ -42,61 +42,60 @@
  * $Id$
  *
  */
-class CRM_ACL_Form_EntityRole extends CRM_Admin_Form
-{
-    /**
-     * Function to build the form
-     *
-     * @return None
-     * @access public
-     */
-    public function buildQuickForm( ) 
-    {
-        parent::buildQuickForm( );
+class CRM_ACL_Form_EntityRole extends CRM_Admin_Form {
 
-        if ($this->_action & CRM_Core_Action::DELETE ) {
-            return;
-        }
+  /**
+   * Function to build the form
+   *
+   * @return None
+   * @access public
+   */
+  public function buildQuickForm() {
+    parent::buildQuickForm();
 
-        $attributes = CRM_Core_DAO::getAttribute( 'CRM_ACL_DAO_EntityRole' );
-
-        $aclRoles = array( '' => ts( '- select -' ) ) + CRM_Core_OptionGroup::values( 'acl_role' );
-        $this->add( 'select', 'acl_role_id', ts( 'ACL Role' ),
-                           $aclRoles, true );
-
-        
-
-        $label = ts( 'Assigned To' );
-        $group = array( '' => ts('- select group -')) + CRM_Core_PseudoConstant::staticGroup( false, 'Access' );
-        $this->add( 'select', 'entity_id', $label, $group, true );
-
-        $this->add('checkbox', 'is_active', ts('Enabled?'));
+    if ($this->_action & CRM_Core_Action::DELETE) {
+      return;
     }
 
-       
-    /**
-     * Function to process the form
-     *
-     * @access public
-     * @return None
-     */
-    public function postProcess() 
-    {
-        CRM_ACL_BAO_Cache::resetCache( );
+    $attributes = CRM_Core_DAO::getAttribute('CRM_ACL_DAO_EntityRole');
 
-        if ( $this->_action & CRM_Core_Action::DELETE ) {
-            CRM_ACL_BAO_EntityRole::del($this->_id);
-            CRM_Core_Session::setStatus( ts('Selected Entity Role has been deleted.') );
-        } else {
-            $params = $this->controller->exportValues( $this->_name );
-            if ( $this->_id ) {
-                $params['id'] = $this->_id;
-            }
-            
-            $params['entity_table'] = 'civicrm_group';
-            CRM_ACL_BAO_EntityRole::create( $params );
-        }
+    $aclRoles = array('' => ts('- select -')) + CRM_Core_OptionGroup::values('acl_role');
+    $this->add('select', 'acl_role_id', ts('ACL Role'),
+      $aclRoles, TRUE
+    );
+
+
+
+    $label = ts('Assigned To');
+    $group = array('' => ts('- select group -')) + CRM_Core_PseudoConstant::staticGroup(FALSE, 'Access');
+    $this->add('select', 'entity_id', $label, $group, TRUE);
+
+    $this->add('checkbox', 'is_active', ts('Enabled?'));
+  }
+
+  /**
+   * Function to process the form
+   *
+   * @access public
+   *
+   * @return None
+   */
+  public function postProcess() {
+    CRM_ACL_BAO_Cache::resetCache();
+
+    if ($this->_action & CRM_Core_Action::DELETE) {
+      CRM_ACL_BAO_EntityRole::del($this->_id);
+      CRM_Core_Session::setStatus(ts('Selected Entity Role has been deleted.'));
     }
+    else {
+      $params = $this->controller->exportValues($this->_name);
+      if ($this->_id) {
+        $params['id'] = $this->_id;
+      }
+
+      $params['entity_table'] = 'civicrm_group';
+      CRM_ACL_BAO_EntityRole::create($params);
+    }
+  }
 }
-
 
