67317c948f595965a86dc9bedc8d48b18f0c78c5
Yashodha Chaku <yashodha.chaku@gmail.com>
2012-04-09 07:55:45 +0000

    webtest fixes

--- tests/phpunit/WebTest/Case/CaseDashboardTest.php
+++ tests/phpunit/WebTest/Case/CaseDashboardTest.php
@@ -27,11 +27,7 @@
 
 require_once 'CiviTest/CiviSeleniumTestCase.php';
 
-
- 
-class WebTest_Case_AddCaseTest extends CiviSeleniumTestCase {
-
-            
+class WebTest_Case_CaseDashboardTest extends CiviSeleniumTestCase {
 
   protected function setUp()
   {
