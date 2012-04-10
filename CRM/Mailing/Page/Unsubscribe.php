4598572aa29f3ea08603cc065ff21388b36024d1
Joseph Murray <joe.murray@jmaconsulting.biz>
2012-04-10 20:10:46 +0000

    CRM-9979 code reformatting

--- CRM/Mailing/Page/Unsubscribe.php
+++ CRM/Mailing/Page/Unsubscribe.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  +--------------------------------------------------------------------+
  | CiviCRM version 4.1                                                |
@@ -26,7 +25,6 @@
  +--------------------------------------------------------------------+
 */
 
-
 /**
  *
  * @package CRM
@@ -34,13 +32,10 @@
  * $Id$
  *
  */
-
-
-class CRM_Mailing_Page_Unsubscribe extends CRM_Mailing_Page_Common
-{
-    function run() {
-        $this->_type = 'unsubscribe';
-        return parent::run( );
-    }
+class CRM_Mailing_Page_Unsubscribe extends CRM_Mailing_Page_Common {
+  function run() {
+    $this->_type = 'unsubscribe';
+    return parent::run();
+  }
 }
 
