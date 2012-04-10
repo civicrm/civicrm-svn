2f638b496c90446bb118db105d1cf04596a9b8f8
Joseph Murray <joe.murray@jmaconsulting.biz>
2012-04-10 20:09:32 +0000

    CRM-9979 code reformatting

--- CRM/Grant/Config.php
+++ CRM/Grant/Config.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  +--------------------------------------------------------------------+
  | CiviCRM version 4.1                                                |
@@ -33,10 +32,5 @@
  * $Id$
  *
  */
-
-
-class CRM_Grant_Config extends CRM_Core_Component_Config {
-
-}
-
+class CRM_Grant_Config extends CRM_Core_Component_Config {}
 
