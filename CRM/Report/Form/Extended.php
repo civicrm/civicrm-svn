ee3afe2017307606587a7278ed4f18f8a8d59c97
Eileen McNaughton <eileen@mcnaughty.com>
2012-04-14 22:53:59 +0000

    CRM-9501 - remove require_once

--- CRM/Report/Form/Extended.php
+++ CRM/Report/Form/Extended.php
@@ -32,9 +32,6 @@
  * $Id$
  *
  */
-
-require_once 'CRM/Report/Form.php';
-require_once 'CRM/Contribute/PseudoConstant.php';
 class CRM_Report_Form_Extended extends CRM_Report_Form {
   protected $_addressField = FALSE;
 
