d0ece76f83ee8d897b50573925aa368c33a4a276
Eileen McNaughton <eileen@mcnaughty.com>
2012-04-26 08:07:14 +0000

    truncate ufgroup before we start so we don't get error on create.

--- tests/phpunit/api/v3/UFGroupTest.php
+++ tests/phpunit/api/v3/UFGroupTest.php
@@ -48,6 +48,14 @@ class api_v3_UFGroupTest extends CiviUnitTestCase {
   protected function setUp() {
     parent::setUp();
     $this->_apiversion = 3;
+    //  Truncate the tables
+    $op = new PHPUnit_Extensions_Database_Operation_Truncate();
+    $op->execute($this->_dbconn,
+      new PHPUnit_Extensions_Database_DataSet_FlatXMLDataSet(
+        dirname(__FILE__) . '/../../CiviTest/truncate-ufgroup.xml'
+      )
+    );
+
     $op = new PHPUnit_Extensions_Database_Operation_Insert;
     $op->execute(
       $this->_dbconn,
