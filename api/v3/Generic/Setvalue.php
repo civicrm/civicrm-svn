e49b050d7874f3e120ac0899d396fcb23ce8720c
Xavier Dutoit <xavier@tttp.eu>
2012-05-02 19:12:15 +0000

    CRM-9798 skip the unknown params instead of fataling

--- api/v3/Generic/Setvalue.php
+++ api/v3/Generic/Setvalue.php
@@ -1,5 +1,7 @@
 <?
-
+/** 
+ * params must contain at least id=xx & {one of the fields from getfields}=value
+*/
 function civicrm_api3_generic_setValue($apiRequest) {
   $entity = $apiRequest['entity'];
   $params = $apiRequest['params'];
@@ -13,38 +15,43 @@ function civicrm_api3_generic_setValue($apiRequest) {
   foreach (array ('id','entity','action','debug','sequential','check_permissions','version','IDS_request_uri','IDS_user_agent') as $f) {
     unset ($params[$f]);
   }
-  if (count($params)!=1) {
-    return civicrm_api3_create_error("wrong number of params. Must be id=xxx field=value only");
-  }
   $fields = civicrm_api ($entity,'getFields',array ("version"=>3,"sequential"));
   if ($fields['is_error'])
     return $fields;
-  
   $fields=$fields['values'];
-  foreach ($params as $field=>$value) {
-    if (!array_key_exists ($field,$fields))
-      return civicrm_api3_create_error("field $field isn't an attribute of $entity");
-    $def=$fields[$field];
-    switch ($def['type']){
-    case 1://int
-      if (!is_numeric ($value))
-        return civicrm_api3_create_error("Param '$field' must be a number");
-      break;
-    case 2://string
-      $value = substr ($value,0,$def['size']);
-      break;
-    case 16://boolean
-      $value = (boolean) $value;
-      break;
-    case 4://date
-    default:
-      return civicrm_api3_create_error("Param '$field' is of a type not managed yet. Join the API team and help us implement it");
+  $field = null;
+  foreach ($params as $f=>$value) {
+    if (array_key_exists ($f,$fields)) {
+      $field = $f; // we found the field
+      $def=$fields[$f];
+      switch ($def['type']){
+      case 1://int
+        if (!is_numeric ($value))
+          return civicrm_api3_create_error("Param '$field' must be a number");
+        break;
+      case 2://string
+        $value = substr ($value,0,$def['size']);
+        break;
+      case 16://boolean
+        $value = (boolean) $value;
+        break;
+      case 4://date
+      default:
+        return civicrm_api3_create_error("Param '$field' is of a type not managed yet. Join the API team and help us implement it");
+      }
+      continue;
     }
+  }
 
-    CRM_Core_DAO::setFieldValue(_civicrm_api3_get_DAO($entity),$id,$field,$value);
+  if (!$field) {
+    return civicrm_api3_create_error("missing 'field=value' param",$params);
+  }
+  if (CRM_Core_DAO::setFieldValue(_civicrm_api3_get_DAO($entity),$id,$field,$value)) {
     $entity=array ('id'=>$id,$field=>$value);
     CRM_Utils_Hook::post( 'edit', $entity, $id, $entity );
-
+    return civicrm_api3_create_success($entity);
+  } else {
+    return civicrm_api3_create_error("error assigning $field=$value for $entity (id=$id)",array("params"=>$params));
   }
-  return civicrm_api3_create_success($entity);
+
 }
