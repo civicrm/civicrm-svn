451ca130a9878bca28f11ecc79229ff605f90e60
Eileen McNaughton <eileen@mcnaughty.com>
2012-05-13 07:32:55 +0000

    CRM-10179 - updated examples

--- api/v3/examples/GroupContactGet.php
+++ api/v3/examples/GroupContactGet.php
@@ -34,7 +34,7 @@ function group_contact_get_expectedresult(){
           'title' => 'New Test Group Created',
           'visibility' => 'Public Pages',
           'is_hidden' => 0,
-          'in_date' => '2012-05-11 22:58:04',
+          'in_date' => '2012-05-13 07:29:12',
           'in_method' => 'API',
         ),
     ),
