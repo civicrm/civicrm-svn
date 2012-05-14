5b0df2ff63621284d7281a13c1458c50dae22e5c
Donald Lobo <lobo@civicrm.org>
2012-05-14 19:10:21 +0000

    fix CRM-10216

--- CRM/Utils/Cache.php
+++ CRM/Utils/Cache.php
@@ -68,11 +68,22 @@ class CRM_Utils_Cache {
         CIVICRM_USE_MEMCACHE
       ) {
         $settings = self::getCacheSettings();
-        self::$_singleton = new CRM_Utils_Cache_Memcache($settings['host'],
-          $settings['port'],
-          $settings['timeout'],
-          $settings['prefix']
-        );
+        if (strtolower(CIVICRM_USE_MEMCACHE) === 'memcached') {
+          self::$_singleton = new CRM_Utils_Cache_Memcached(
+            $settings['host'],
+            $settings['port'],
+            $settings['timeout'],
+            $settings['prefix']
+          );
+        }
+        else {
+          self::$_singleton = new CRM_Utils_Cache_Memcache(
+            $settings['host'],
+            $settings['port'],
+            $settings['timeout'],
+            $settings['prefix']
+          );
+        }
       }
       elseif (defined('CIVICRM_USE_ARRAYCACHE') &&
         CIVICRM_USE_ARRAYCACHE
