9a96943c9c0f6dc7723baace6824cb60637743d2
Donald Lobo <lobo@civicrm.org>
2012-05-25 17:53:11 +0000

    reverse bad commit for CRM-10268

--- CRM/Campaign/BAO/Query.php
+++ CRM/Campaign/BAO/Query.php
@@ -496,12 +496,11 @@ INNER JOIN  civicrm_custom_group grp on fld.custom_group_id = grp.id
         CRM_Core_DAO::executeQuery("DROP TABLE IF EXISTS {$tempTableName}");
 
         $query = "
-CREATE TEMPORARY TABLE {$tempTableName} (
-   id int unsigned NOT NULL AUTO_INCREMENT,
-   survey_contact_id int unsigned NOT NULL,
-   PRIMARY KEY ( id )
-);
-";
+     CREATE TEMPORARY TABLE {$tempTableName} (
+            id int unsigned NOT NULL AUTO_INCREMENT,
+            survey_contact_id int unsigned NOT NULL,
+PRIMARY KEY ( id ),
+ CONSTRAINT FK_civicrm_survey_respondent FOREIGN KEY (survey_contact_id) REFERENCES civicrm_contact(id) ON DELETE CASCADE )";
         CRM_Core_DAO::executeQuery($query);
 
         $batch = 100;
