af11dc66d72d7585acb6254d24827b6679e2c1e3
Xavier Dutoit <xavier@tttp.eu>
2012-06-02 00:08:56 +0000

    CRM-9792 ajax add templates (and an example of usage)

--- CRM/Utils/REST.php
+++ CRM/Utils/REST.php
@@ -479,6 +479,56 @@ class CRM_Utils_REST {
     );
   }
 
+  /** used to load a template "inline", eg. for ajax, without having to build a menu for each template */
+  static 
+  function loadTemplate () {
+    $request = CRM_Utils_Request::retrieve( 'q', 'String');
+    if (false !== strpos($request, '..')) {
+      die ("SECURITY FATAL: the url can't contain '..'. Please report the issue on the forum at civicrm.org");
+    }
+
+    $request = split ('/',$request);
+    $entity = _civicrm_api_get_camel_name($request[2]);
+    $tplfile=_civicrm_api_get_camel_name($request[3]);
+
+    $tpl = 'CRM/'.$entity.'/Page/Inline/'.$tplfile.'.tpl';
+    $smarty= CRM_Core_Smarty::singleton( );
+    CRM_Utils_System::setTitle( "$entity::$tplfile inline $tpl" );
+    if( !$smarty->template_exists($tpl) ){
+      header("Status: 404 Not Found"); 
+      die ("Can't find the requested template file templates/$tpl");
+    }
+    if (array_key_exists('id',$_GET)) {// special treatmenent, because it's often used
+      $smarty->assign ('id',(int)$_GET['id']);// an id is always positive
+    }
+    $pos = strpos (implode (array_keys ($_GET)),'<') ;
+
+    if ($pos !== false) {
+      die ("SECURITY FATAL: one of the param names contains &lt;");
+    }
+    $param = array_map( 'htmlentities' , $_GET);
+    unset($param['q']);
+    $smarty->assign_by_ref("request", $param);
+
+    if  ( ! array_key_exists ( 'HTTP_X_REQUESTED_WITH', $_SERVER ) ||
+      $_SERVER['HTTP_X_REQUESTED_WITH'] != "XMLHttpRequest"  )  {
+
+        $smarty->assign( 'tplFile', $tpl );
+        $config = CRM_Core_Config::singleton();
+        $content = $smarty->fetch( 'CRM/common/'. strtolower($config->userFramework) .'.tpl' );
+
+        CRM_Utils_System::appendTPLFile( $tpl, $content );
+
+        return CRM_Utils_System::theme( 'page', $content, true);
+
+      } else {
+        $content = "<!-- .tpl file embeded: $tpl -->\n";
+        CRM_Utils_System::appendTPLFile( $tpl, $content );
+        echo $content . $smarty->fetch ($tpl);
+        CRM_Utils_System::civiExit( );
+    }
+  }
+
   /** This is a wrapper so you can call an api via json (it returns json too)
    * http://example.org/civicrm/api/json?entity=Contact&action=Get"&json={"contact_type":"Individual","email.get.email":{}} to take all the emails from individuals
    * works for POST & GET (POST recommended)
