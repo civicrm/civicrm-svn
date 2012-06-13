8539af1b7eea65246a46280c863e62f91f8e4100
Pradeep Nayak <pradeep.nayak@jmaconsulting.biz>
2012-06-13 09:49:59 +0000

    #CRM-10364

--- CRM/Price/BAO/Field.php
+++ CRM/Price/BAO/Field.php
@@ -123,7 +123,7 @@ class CRM_Price_BAO_Field extends CRM_Price_DAO_Field {
 
     for ($index = 1; $index <= $maxIndex; $index++) {
 
-      if (CRM_Utils_Array::value($index, $params['option_label']) &&
+      if (CRM_Utils_Array::value($index, CRM_Utils_Array::value('option_label', $params)) &&
         !CRM_Utils_System::isNull($params['option_amount'][$index])
       ) {
         $options = array(
