f179beb75055a11a1d5580a909f19285c8c6a42f
Kurund Jalmi <kurund@civicrm.org>
2012-06-18 09:00:45 +0000

    add form validation, CRM-9908

--- CRM/Contact/Form/Inline/Phone.php
+++ CRM/Contact/Form/Inline/Phone.php
@@ -110,6 +110,48 @@ class CRM_Contact_Form_Inline_Phone extends CRM_Core_Form {
     );
 
     $this->addButtons($buttons);
+    
+    $this->addFormRule( array( 'CRM_Contact_Form_Inline_Phone', 'formRule' ) );
+  }
+
+  /**
+   * global validation rules for the form
+   *
+   * @param array $fields     posted values of the form
+   * @param array $errors     list of errors to be posted back to the form
+   *
+   * @return $errors
+   * @static
+   * @access public
+   */
+  static function formRule( $fields, $errors ) {
+    require_once 'CRM/Contact/Form/Contact.php';
+    $hasData = $hasPrimary = $errors = array( );
+    if ( CRM_Utils_Array::value( 'phone', $fields ) && is_array( $fields['phone'] ) ) {
+      foreach ( $fields['phone'] as $instance => $blockValues ) {
+        $dataExists = CRM_Contact_Form_Contact::blockDataExists( $blockValues );
+
+        if ( $dataExists ) {
+          $hasData[] = $instance;
+          if ( CRM_Utils_Array::value( 'is_primary', $blockValues ) ) {
+            $hasPrimary[] = $instance;
+            if ( !$primaryID &&
+              CRM_Utils_Array::value( 'phone', $blockValues ) ) {
+                $primaryID = $blockValues['phone'];
+            }
+          }
+        }
+      }
+
+      if ( empty( $hasPrimary ) && !empty( $hasData ) ) {
+        $errors["phone[1][is_primary]"] = ts('One phone should be marked as primary.' );
+      }
+
+      if ( count( $hasPrimary ) > 1 ) {
+        $errors["phone[".array_pop($hasPrimary)."][is_primary]"] = ts( 'Only one phone can be marked as primary.' );
+      }
+    }
+    return $errors;
   }
 
   /**
