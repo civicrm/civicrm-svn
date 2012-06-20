182fa206bf0b07116a98531198d9b880cbf6f7a0
Eileen McNaughton <eileen@mcnaughty.com>
2012-06-20 03:21:40 +0000

    CRm-10382 document skipRecentView param

--- api/v3/Contribution.php
+++ api/v3/Contribution.php
@@ -104,6 +104,14 @@ function _civicrm_api3_contribution_create_spec(&$params) {
     'description' => 'ID of Contact to be Soft credited to',
     'FKClassName' => 'CRM_Contact_DAO_Contact',
   );
+  // note this is a recommended option but not adding as a default to avoid
+  // creating unecessary changes for the dev
+  $params['skipRecentView'] = array(
+    'name' => 'skipRecentView',
+    'title' => 'Skip Recent View',
+    'type' => 1,
+    'description' => 'Do not add to recent items (improves performance)',
+  );
 }
 
 /**
@@ -349,7 +357,7 @@ function civicrm_api3_contribution_transact($params) {
  * @return array  Api result array
  * @static void
  * @access public
- * 
+ *
  */
 function civicrm_api3_contribution_sendconfirmation($params) {
   $contribution = new CRM_Contribute_BAO_Contribution();
