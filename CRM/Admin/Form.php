45e05f1dc28e3f0b55323d338d0ce955e0b7db49
Yashodha Chaku <yashodha.chaku@gmail.com>
2012-06-21 19:08:43 +0000

    minor fix

--- CRM/Admin/Form.php
+++ CRM/Admin/Form.php
@@ -64,7 +64,6 @@ class CRM_Admin_Form extends CRM_Core_Form {
     $this->_values  = array();
     if (isset($this->_id)) {
       $params = array('id' => $this->_id);
-      require_once (str_replace('_', DIRECTORY_SEPARATOR, $this->_BAOName) . ".php");
       eval($this->_BAOName . '::retrieve( $params, $this->_values );');
     }
   }
@@ -81,7 +80,6 @@ class CRM_Admin_Form extends CRM_Core_Form {
     if (isset($this->_id) && empty($this->_values)) {
       $this->_values = array();
       $params = array('id' => $this->_id);
-      require_once (str_replace('_', DIRECTORY_SEPARATOR, $this->_BAOName) . ".php");
       eval($this->_BAOName . '::retrieve( $params, $this->_values );');
     }
     $defaults = $this->_values;
--- CRM/Admin/Form/RelationshipType.php
+++ CRM/Admin/Form/RelationshipType.php
@@ -111,9 +111,7 @@ class CRM_Admin_Form_RelationshipType extends CRM_Admin_Form {
     ) {
       $defaults = $params = array();
       $params = array('id' => $this->_id);
-      require_once (str_replace('_', DIRECTORY_SEPARATOR, $this->_BAOName) . ".php");
       eval($this->_BAOName . '::retrieve( $params, $defaults );');
-
       $defaults['contact_types_a'] = $defaults['contact_type_a'];
       if (CRM_Utils_Array::value('contact_sub_type_a', $defaults)) {
         $defaults['contact_types_a'] .= CRM_Core_DAO::VALUE_SEPARATOR . $defaults['contact_sub_type_a'];
