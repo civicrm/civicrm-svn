566a0a584dca7312682791e06697e5df752756bc
Pradeep Nayak <pradeep.nayak@jmaconsulting.biz>
2012-06-22 16:43:47 +0000

    #CRM-8906 added patch of 8662 and created webtest

--- api/v3/Contribution.php
+++ api/v3/Contribution.php
@@ -179,9 +179,12 @@ function civicrm_api3_contribution_get($params) {
 
   $contribution = array();
   while ($dao->fetch()) {
-    $contribution[$dao->contribution_id] = $query->store($dao);
+    //CRM-8662
+    $contribution_details = $query->store ( $dao );
+    $soft_params = array('contribution_id' => $dao->contribution_id);
+    $soft_contribution = CRM_Contribute_BAO_Contribution::getSoftContribution ( $soft_params , true);
+    $contribution [$dao->contribution_id] = array_merge($contribution_details, $soft_contribution);
   }
-
   return civicrm_api3_create_success($contribution, $params, 'contribution', 'get', $dao);
 }
 /*
