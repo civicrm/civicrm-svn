1a5347d5dc551989e95ba29eaf01dbb6cfdd2361
Tim Otten <totten@civicrm.org>
2012-07-06 22:31:00 +0000

    CRM-10477 - Add column civicrm_extension.schema_version

--- CRM/Core/BAO/Extension.php
+++ CRM/Core/BAO/Extension.php
@@ -63,7 +63,7 @@ class CRM_Core_BAO_Extension extends CRM_Core_DAO_Extension {
     }
     return NULL;
   }
-  
+
   /**
    * Function to delete an extension
    *
@@ -79,6 +79,36 @@ class CRM_Core_BAO_Extension extends CRM_Core_DAO_Extension {
     $extension = new CRM_Core_DAO_Extension();
     $extension->id = $id;
     return $extension->delete();
+  }
+
+  /**
+   * Change the schema version of an extension
+   *
+   * @param $fullName string, the fully-qualified name (eg "com.example.myextension")
+   * @param $schemaVersion string
+   * @return void
+   */
+  static function setSchemaVersion($fullName, $schemaVersion) {
+    $sql = 'UPDATE civicrm_extension SET schema_version = %1 WHERE full_name = %2';
+    $params = array(
+      1 => array($schemaVersion, 'String'),
+      2 => array($fullName, 'String'),
+    );
+    return CRM_Core_DAO::executeQuery($sql, $params);
+  }
+
+  /**
+   * Determine the schema version of an extension
+   *
+   * @param $fullName string, the fully-qualified name (eg "com.example.myextension")
+   * @return string
+   */
+  static function getSchemaVersion($fullName) {
+    $sql = 'SELECT schema_version FROM civicrm_extension WHERE full_name = %1';
+    $params = array(
+      1 => array($fullName, 'String'),
+    );
+    return CRM_Core_DAO::singleValueQuery($sql, $params);
   }
 
 }
