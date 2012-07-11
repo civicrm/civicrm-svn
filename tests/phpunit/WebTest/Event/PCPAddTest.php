493c656bcc7a15c62d5eb85cfa559f15d3349def
Pratik Joshi <pratik.joshi@webaccess.co.in>
2012-07-11 12:30:58 +0000

    -- minor webtest minor for default fee selection

--- tests/phpunit/WebTest/Event/PCPAddTest.php
+++ tests/phpunit/WebTest/Event/PCPAddTest.php
@@ -208,6 +208,8 @@ class WebTest_Event_PCPAddTest extends CiviSeleniumTestCase {
       $this->type("value_1", "250.00");
       $this->type("label_2", "Non-member");
       $this->type("value_2", "325.00");
+      //set default
+      $this->click("xpath=//table[@id='map-field-table']/tbody/tr[2]/td[3]/input");
     }
 
     if ($discount) {
