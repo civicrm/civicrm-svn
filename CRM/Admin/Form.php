e05747713dcf464421af0e08220d11bfde9c0d0f
Donald Lobo <lobo@civicrm.org>
2012-07-22 05:14:16 +0000

    add require_once for the eval since form might be outside CRM name space

--- CRM/Admin/Form.php
+++ CRM/Admin/Form.php
@@ -58,12 +58,16 @@ class CRM_Admin_Form extends CRM_Core_Form {
    *
    * @var string
    */
-  protected $_BAOName; function preProcess() {
+  protected $_BAOName;
+
+  function preProcess() {
     $this->_id      = $this->get('id');
     $this->_BAOName = $this->get('BAOName');
     $this->_values  = array();
     if (isset($this->_id)) {
       $params = array('id' => $this->_id);
+      // this is needed if the form is outside the CRM name space
+      require_once (str_replace('_', DIRECTORY_SEPARATOR, $this->_BAOName) . ".php");
       eval($this->_BAOName . '::retrieve( $params, $this->_values );');
     }
   }
@@ -80,6 +84,7 @@ class CRM_Admin_Form extends CRM_Core_Form {
     if (isset($this->_id) && empty($this->_values)) {
       $this->_values = array();
       $params = array('id' => $this->_id);
+      require_once (str_replace('_', DIRECTORY_SEPARATOR, $this->_BAOName) . ".php");
       eval($this->_BAOName . '::retrieve( $params, $this->_values );');
     }
     $defaults = $this->_values;
