1f869a8f0b34ad0db7a3700522940bbb5ac92132
Donald Lobo <lobo@civicrm.org>
2012-09-22 21:49:55 +0000

    fix CRM-10848

--- CRM/Contact/Form/Search/Custom/Group.php
+++ CRM/Contact/Form/Search/Custom/Group.php
@@ -187,7 +187,7 @@ class CRM_Contact_Form_Search_Custom_Group extends CRM_Contact_Form_Search_Custo
 
     $where = $this->where($includeContactIDs);
 
-    $sql = "SELECT $selectClause $from WHERE  $where GROUP BY contact_id";
+    $sql = "SELECT $selectClause $from WHERE  $where GROUP BY contact_a.id";
 
     // Define ORDER BY for query in $sort, with default value
     if (!$justIDs) {
