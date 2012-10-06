004c03d17c0154a77848cffc45fc28e33950f8aa
Kurund Jalmi <kurund@civicrm.org>
2012-10-06 16:01:57 +0000

    update version to 4.2.3

--- WordPress/civicrm.php
+++ WordPress/civicrm.php
@@ -38,7 +38,7 @@
   Plugin URI: http://civicrm.org/
   Description: CiviCRM WP Plugin
   Author: CiviCRM LLC
-  Version: 4.2.2
+  Version: 4.2.3
   Author URI: http://civicrm.org/
   License: AGPL3
 */
