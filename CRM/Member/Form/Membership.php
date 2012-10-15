db491a0e67377e270cea987c2b4fc2305da544e9
Kurund Jalmi <kurund@civicrm.org>
2012-10-15 07:44:46 +0000

    restructured the code to call hook_civicrm_membershipTypeValues(), CRM-11028

--- CRM/Member/Form/Membership.php
+++ CRM/Member/Form/Membership.php
@@ -493,25 +493,29 @@ class CRM_Member_Form_Membership extends CRM_Member_Form {
     $dao = new CRM_Member_DAO_MembershipType();
     $dao->domain_id = CRM_Core_Config::domainID();
     $dao->find();
+
+    $allMemberships = CRM_Member_BAO_Membership::buildMembershipTypeValues($this);
+
     $membershipType = array();
-    while ($dao->fetch()) {
-      if ($dao->is_active) {
-        $membershipType[$dao->id] = $dao->name;
-        if ($this->_mode && !$dao->minimum_fee) {
+    foreach( $allMemberships as $key => $values ) {
+      if (CRM_Utils_Array::value('is_active', $values) ) {
+        $membershipType[$key] = CRM_Utils_Array::value('name', $values);
+        if ($this->_mode && !CRM_Utils_Array::value('minimum_fee', $values)) {
           continue;
         }
         else {
-          if (!CRM_Utils_Array::value($dao->member_of_contact_id, $selMemTypeOrg)) {
-            $selMemTypeOrg[$dao->member_of_contact_id] = CRM_Core_DAO::getFieldValue('CRM_Contact_DAO_Contact',
-              $dao->member_of_contact_id,
+          $memberOfContactId = CRM_Utils_Array::value('member_of_contact_id', $values);
+          if (!CRM_Utils_Array::value($memberOfContactId, $selMemTypeOrg)) {
+            $selMemTypeOrg[$memberOfContactId] = CRM_Core_DAO::getFieldValue('CRM_Contact_DAO_Contact',
+              $memberOfContactId,
               'display_name',
               'id'
             );
 
-            $selOrgMemType[$dao->member_of_contact_id][0] = ts('- select -');
+            $selOrgMemType[$memberOfContactId][0] = ts('- select -');
           }
-          if (!CRM_Utils_Array::value($dao->id, $selOrgMemType[$dao->member_of_contact_id])) {
-            $selOrgMemType[$dao->member_of_contact_id][$dao->id] = $dao->name;
+          if (!CRM_Utils_Array::value($key, $selOrgMemType[$memberOfContactId])) {
+            $selOrgMemType[$memberOfContactId][$key] = CRM_Utils_Array::value('name', $values);
           }
         }
       }
