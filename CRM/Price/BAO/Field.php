ea39f233111d5ec027bde6d0571424e183421fe6
Ravish Nair <ravish.nair@webaccess.co.in>
2012-10-17 08:38:18 +0000

    -- notice fix

--- CRM/Price/BAO/Field.php
+++ CRM/Price/BAO/Field.php
@@ -122,7 +122,7 @@ class CRM_Price_BAO_Field extends CRM_Price_DAO_Field {
 
     for ($index = 1; $index <= $maxIndex; $index++) {
 
-      if (CRM_Utils_Array::value($index , $params['option_amount']) && (CRM_Utils_Array::value($index, CRM_Utils_Array::value('option_label', $params)) || CRM_Utils_Array::value('is_quick_config', $params)) &&
+      if (CRM_Utils_Array::value($index , CRM_Utils_Array::value('option_amount', $params)) && (CRM_Utils_Array::value($index, CRM_Utils_Array::value('option_label', $params)) || CRM_Utils_Array::value('is_quick_config', $params)) &&
         !CRM_Utils_System::isNull($params['option_amount'][$index])
           ) {
         $options = array(
