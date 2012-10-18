6671a1bd4608d9c3b0a7e25e5af063f2cad3c7c9
Tim Otten <totten@civicrm.org>
2012-10-18 23:25:22 +0000

    CRM-11045 - CRM_Core_Resources::getUrl() should produce different URLs for different extension-containers

---
+++ CRM/Extension/Config.php
@@ -0,0 +1,144 @@
+<?php
+/*
+ +--------------------------------------------------------------------+
+ | CiviCRM version 4.2                                                |
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC (c) 2004-2012                                |
+ +--------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                    |
+ |                                                                    |
+ | CiviCRM is free software; you can copy, modify, and distribute it  |
+ | under the terms of the GNU Affero General Public License           |
+ | Version 3, 19 November 2007 and the CiviCRM Licensing Exception.   |
+ |                                                                    |
+ | CiviCRM is distributed in the hope that it will be useful, but     |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+ | See the GNU Affero General Public License for more details.        |
+ |                                                                    |
+ | You should have received a copy of the GNU Affero General Public   |
+ | License and the CiviCRM Licensing Exception along                  |
+ | with this program; if not, contact CiviCRM LLC                     |
+ | at info[AT]civicrm[DOT]org. If you have questions about the        |
+ | GNU Affero General Public License or the licensing of CiviCRM,     |
+ | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
+ +--------------------------------------------------------------------+
+*/
+
+/**
+ * This class glues together the various parts of the extension
+ * system.
+ *
+ * @package CRM
+ * @copyright CiviCRM LLC (c) 2004-2012
+ * $Id$
+ *
+ */
+class CRM_Extension_Config {
+  private static $singleton;
+
+  private $cache = NULL;
+  private $fullContainer = NULL;
+  private $defaultContainer = NULL;
+  private $mapper = NULL;
+  private $manager = NULL;
+  private $browser = NULL;
+  private $downloader = NULL;
+
+  /**
+   * @return CRM_Extension_Config
+   */
+  public static function singleton($fresh = FALSE) {
+    if (! self::$singleton) {
+      self::$singleton = new CRM_Extension_Config();
+    }
+    return self::$singleton;
+  }
+
+  /**
+   * Get a container which represents all available extensions
+   *
+   * @return CRM_Extension_Container_Interface
+   */
+  public function getFullContainer() {
+    if ($this->fullContainer === NULL) {
+      $containers = array();
+
+      if ($this->getDefaultContainer()) {
+        $containers['default'] = $this->getDefaultContainer();
+      }
+
+      $config = CRM_Core_Config::singleton();
+      global $civicrm_root;
+      $containers['civiroot']  = new CRM_Extension_Container_Basic($civicrm_root, $config->resourceBase, $this->getCache(), 'civiroot');
+
+      // TODO: CRM_Extension_Container_Basic( /sites/all/modules )
+      // TODO: CRM_Extension_Container_Basic( /sites/$domain/modules
+      // TODO: CRM_Extension_Container_Basic( /modules )
+      // TODO: CRM_Extension_Container_Basic( /vendors )
+
+      $this->fullContainer = new CRM_Extension_Container_Collection($containers, $this->getCache(), 'full');
+    }
+    return $this->fullContainer;
+  }
+
+  /**
+   * Get the container to which new extensions are installed
+   *
+   * This container should be a particular, writeable directory.
+   *
+   * @return CRM_Extension_Container_Basic|FALSE (false if not configured)
+   */
+  public function getDefaultContainer() {
+    if ($this->defaultContainer === NULL) {
+      $config = CRM_Core_Config::singleton();
+      if ($config->extensionsDir) {
+        $this->defaultContainer = new CRM_Extension_Container_Basic($config->extensionsDir, $config->extensionsURL, $this->getCache(), 'default');
+      } else {
+        $this->defaultContainer = FALSE;
+      }
+    }
+    return $this->defaultContainer;
+  }
+
+  /**
+   * @return CRM_Extension_Mapper
+   */
+  public function getMapper() {
+    if ($this->mapper === NULL) {
+      $this->mapper = new CRM_Extension_Mapper($this->getFullContainer());
+    }
+    return $this->mapper;
+  }
+
+  /**
+   * @return CRM_Extension_Manager
+   */
+  public function getManager() {
+  }
+
+  /**
+   * @return CRM_Extension_Browser
+   */
+  public function getBrowser() {
+  }
+
+  /**
+   * @return CRM_Extension_Downloader
+   */
+  public function getDownloader() {
+  }
+
+  /**
+   * @return CRM_Utils_Cache_Interface
+   */
+  protected function getCache() {
+    if ($this->cache === NULL) {
+      $this->cache = new CRM_Utils_Cache_SqlGroup(array(
+        'group' => 'ext',
+        'prefetch' => FALSE,
+      ));
+    }
+    return $this->cache;
+  }
+}
