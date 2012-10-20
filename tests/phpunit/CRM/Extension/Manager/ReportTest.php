35f15c42ac0e4add8fbdcf2e4537de98dff5da1a
Tim Otten <totten@civicrm.org>
2012-10-20 00:54:24 +0000

    CRM-11045 - Add CRM_Extension_Manager_Report (based on CRM_Core_Extensions_Report)

---
+++ tests/phpunit/CRM/Extension/Manager/ReportTest.php
@@ -0,0 +1,55 @@
+<?php
+
+require_once 'CiviTest/CiviUnitTestCase.php';
+
+class CRM_Extension_Manager_ReportTest extends CiviUnitTestCase {
+  function setUp() {
+    parent::setUp();
+    CRM_Core_DAO::executeQuery('DELETE FROM civicrm_extension WHERE full_name LIKE "test.%"');
+    //if (class_exists('test_extension_manager_reporttest')) {
+    //  test_extension_manager_reporttest::$counts = array();
+    //}
+  }
+
+  function tearDown() {
+    parent::tearDown();
+    CRM_Core_DAO::executeQuery('DELETE FROM civicrm_extension WHERE full_name LIKE "test.%"');
+  }
+
+  /**
+   * Install an extension with a valid type name
+   */
+  function testInstallDisableUninstall() {
+    $manager = CRM_Extension_System::singleton(TRUE)->getManager();
+    $this->assertDBQuery(0, 'SELECT count(*) FROM civicrm_option_value WHERE name = "test.extension.manager.reporttest"');
+
+    $manager->install('test.extension.manager.reporttest');
+    $this->assertDBQuery(1, 'SELECT count(*) FROM civicrm_option_value WHERE name = "test.extension.manager.reporttest" AND is_active = 1');
+
+    $manager->disable('test.extension.manager.reporttest');
+    $this->assertDBQuery(1, 'SELECT count(*) FROM civicrm_option_value WHERE name = "test.extension.manager.reporttest"');
+    $this->assertDBQuery(1, 'SELECT count(*) FROM civicrm_option_value WHERE name = "test.extension.manager.reporttest" AND is_active = 0');
+
+    $manager->uninstall('test.extension.manager.reporttest');
+    $this->assertDBQuery(0, 'SELECT count(*) FROM civicrm_option_value WHERE name = "test.extension.manager.reporttest"');
+  }
+
+  /**
+   * Install an extension with a valid type name
+   */
+  function testInstallDisableEnable() {
+    $manager = CRM_Extension_System::singleton(TRUE)->getManager();
+    $this->assertDBQuery(0, 'SELECT count(*) FROM civicrm_option_value WHERE name = "test.extension.manager.reporttest"');
+
+    $manager->install('test.extension.manager.reporttest');
+    $this->assertDBQuery(1, 'SELECT count(*) FROM civicrm_option_value WHERE name = "test.extension.manager.reporttest" AND is_active = 1');
+
+    $manager->disable('test.extension.manager.reporttest');
+    $this->assertDBQuery(1, 'SELECT count(*) FROM civicrm_option_value WHERE name = "test.extension.manager.reporttest"');
+    $this->assertDBQuery(1, 'SELECT count(*) FROM civicrm_option_value WHERE name = "test.extension.manager.reporttest" AND is_active = 0');
+
+    $manager->enable('test.extension.manager.reporttest');
+    $this->assertDBQuery(1, 'SELECT count(*) FROM civicrm_option_value WHERE name = "test.extension.manager.reporttest"');
+    $this->assertDBQuery(1, 'SELECT count(*) FROM civicrm_option_value WHERE name = "test.extension.manager.reporttest" AND is_active = 1');
+  }
+}
