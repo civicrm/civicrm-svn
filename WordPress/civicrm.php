d7c9c40fe5eac1dca330609ec848297e2f1e18d4
Pratik Joshi <pratik.joshi@webaccess.co.in>
2012-11-01 11:35:19 +0000

    -- civicrm wordpress plugin version update

--- WordPress/civicrm.php
+++ WordPress/civicrm.php
@@ -38,7 +38,7 @@
   Plugin URI: http://civicrm.org/
   Description: CiviCRM WP Plugin
   Author: CiviCRM LLC
-  Version: 4.2.4
+  Version: 4.2.5
   Author URI: http://civicrm.org/
   License: AGPL3
 */
