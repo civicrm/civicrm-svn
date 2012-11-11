ead68eb8c25446e122fb5b2b20de98a3d74e2104
Pradeep Nayak <pradeep.nayak@jmaconsulting.biz>
2012-11-11 14:23:31 +0000

    CRM-11236 fixes

--- CRM/Event/BAO/Event.php
+++ CRM/Event/BAO/Event.php
@@ -878,19 +878,21 @@ WHERE civicrm_event.is_active = 1
          $fieldsFix
        );
     }
-    $isQuickConfig = CRM_Price_BAO_Set::getFor('civicrm_event', $id, NULL, 1);
-    if(!$isQuickConfig && $copyEvent->is_monetary) {
-       $priceSetId = CRM_Price_BAO_Set::getFor('civicrm_event', $id);
-       $copyPriceSet = &CRM_Price_BAO_Set::copy($priceSetId);
-       CRM_Price_BAO_Set::addTo('civicrm_event', $copyEvent->id, $copyPriceSet->id);
-    } else {
-    $copyPriceSet = &CRM_Core_DAO::copyGeneric('CRM_Price_DAO_SetEntity',
-      array(
-        'entity_id' => $id,
-        'entity_table' => 'civicrm_event',
-      ),
-      array('entity_id' => $copyEvent->id)
-    );
+    $priceSetId = CRM_Price_BAO_Set::getFor('civicrm_event', $id);
+    if ($priceSetId) {
+      $isQuickConfig = CRM_Core_DAO::getFieldValue('CRM_Price_DAO_Set', $priceSetId, 'is_quick_config');
+      if(!$isQuickConfig) {
+        $copyPriceSet = &CRM_Price_BAO_Set::copy($priceSetId);
+        CRM_Price_BAO_Set::addTo('civicrm_event', $copyEvent->id, $copyPriceSet->id);
+      } else {
+        $copyPriceSet = &CRM_Core_DAO::copyGeneric('CRM_Price_DAO_SetEntity',
+          array(
+            'entity_id' => $id,
+            'entity_table' => 'civicrm_event',
+          ),
+          array('entity_id' => $copyEvent->id)
+        );
+      }
     }
 
     $copyUF = &CRM_Core_DAO::copyGeneric('CRM_Core_DAO_UFJoin',
