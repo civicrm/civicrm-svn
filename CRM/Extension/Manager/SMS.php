6318cbd3ceeb15bbc78cce7491c9b386e346093f
Deepak Srivastava <deepak.srivastava.0303@gmail.com>
2012-11-16 08:34:14 +0000

    - CRM-9914

---
+++ CRM/Extension/Manager/SMS.php
@@ -0,0 +1,83 @@
+<?php
+/*
+ +--------------------------------------------------------------------+
+ | CiviCRM version 4.2                                                |
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC (c) 2004-2012                                |
+ +--------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                    |
+ |                                                                    |
+ | CiviCRM is free software; you can copy, modify, and distribute it  |
+ | under the terms of the GNU Affero General Public License           |
+ | Version 3, 19 November 2007 and the CiviCRM Licensing Exception.   |
+ |                                                                    |
+ | CiviCRM is distributed in the hope that it will be useful, but     |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+ | See the GNU Affero General Public License for more details.        |
+ |                                                                    |
+ | You should have received a copy of the GNU Affero General Public   |
+ | License and the CiviCRM Licensing Exception along                  |
+ | with this program; if not, contact CiviCRM LLC                     |
+ | at info[AT]civicrm[DOT]org. If you have questions about the        |
+ | GNU Affero General Public License or the licensing of CiviCRM,     |
+ | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
+ +--------------------------------------------------------------------+
+*/
+
+/**
+ * This class stores logic for managing CiviCRM extensions.
+ *
+ * @package CRM
+ * @copyright CiviCRM LLC (c) 2004-2012
+ * $Id$
+ *
+ */
+class CRM_Extension_Manager_SMS extends CRM_Extension_Manager_Base {
+
+  public function __construct() {
+    parent::__construct(TRUE);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function onPreInstall(CRM_Extension_Info $info) {
+    CRM_Core_Error::fatal('sms is getting called.');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function onPostInstall(CRM_Extension_Info $info) {
+    CRM_Core_Error::fatal('sms is getting called.');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function onPreUninstall(CRM_Extension_Info $info) {
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function onPreDisable(CRM_Extension_Info $info) {
+    CRM_Core_Error::fatal('sms is getting called.');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function onPreEnable(CRM_Extension_Info $info) {
+    CRM_Core_Error::fatal('sms is getting called.');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function onPostEnable(CRM_Extension_Info $info) {
+    CRM_Core_Error::fatal('sms is getting called.');
+  }
+
+}
