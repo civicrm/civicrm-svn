a7e1a9cf755a6a9bd51cb23228e140b5f5d3350c
demeritcowboy <demeritcowboy@hotmail.com>
2012-11-22 21:30:57 +0000

    CRM-11190

--- tests/phpunit/CiviTest/CiviMailService.php
+++ tests/phpunit/CiviTest/CiviMailService.php
@@ -36,47 +36,30 @@ require_once 'ezc/autoload/mail_autoload.php';
 
 class CiviMailService {
 
-    protected $unitTest;
-
-    /**
-     * Constructor
-     *
-     * @param object ut A unit test object, e.g. the currently running test.
-     */
-    function __construct( &$ut ) {
-        $this->unitTest = &$ut;
-
-        if(!defined('CIVICRM_MAIL_LOG')) {
-          define( 'CIVICRM_MAIL_LOG', CIVICRM_TEMPLATE_COMPILEDIR . 'mail.log' );
+    function __construct() {
+        if(!defined('CIVICRM_MAIL_LOG') || empty( CIVICRM_MAIL_LOG ) ) {
+          define( 'CIVICRM_MAIL_LOG', CIVICRM_TEMPLATE_COMPILEDIR . '/mail.log' );
         }
-
-        $this->unitTest->assertFalse(is_numeric(CIVICRM_MAIL_LOG) ,'we need to be able to log email to check receipt');
+        $this->assertFalse(is_numeric(CIVICRM_MAIL_LOG) ,'we need to be able to log email to check receipt');
 
         $this->reset();
     }
 
     /**
-     * reset()
-     *
      * At the moment the "service" only really supports one email at a time,
      * so need to clear out the log each time.
      */
     function reset(){
-        if ( defined( 'CIVICRM_MAIL_LOG' ) ) {
+        if ( ! empty( CIVICRM_MAIL_LOG ) ) {
             file_put_contents(CIVICRM_MAIL_LOG, '');
         }
     }
 
-    /**
-     * stop()
-     *
-     * Can't actually stop because you can't undefine or redefine a constant.
-     * This is probably ok within the context of unit testing for now.
-     */
     function stop() {
-        if ( defined( 'CIVICRM_MAIL_LOG' ) ) {
+        if ( ! empty( CIVICRM_MAIL_LOG ) ) {
             unlink( CIVICRM_MAIL_LOG );
         }
+        define( 'CIVICRM_MAIL_LOG', '' );
     }
 
     /**
@@ -86,13 +69,13 @@ class CiviMailService {
      * object.
      */
     function &fetch( $params = null ) {
-        if ( defined( 'CIVICRM_MAIL_LOG' ) ) {
+        if ( empty( CIVICRM_MAIL_LOG ) ) {
+            return null;
+        } else {
             $set = new ezcMailFileSet( array( CIVICRM_MAIL_LOG ) );
             $parser = new ezcMailParser();
             $mail = $parser->parseMail( $set );
             return $mail[0];
-        } else {
-            return null;
         }
     }
 
@@ -108,10 +91,10 @@ class CiviMailService {
     function checkMailLog($strings, $absentStrings = array(), $prefix = ''){
         $mail = file_get_contents(CIVICRM_MAIL_LOG);
         foreach ($strings as $string) {
-          $this->unitTest->assertContains($string, $mail, "$string .  not found in  $mail  $prefix");
+          $this->assertContains($string, $mail, "$string .  not found in  $mail  $prefix");
         }
         foreach ($absentStrings as $string) {
-          $this->unitTest->assertEmpty(strstr($mail,$string),"$string  incorrectly found in $mail $prefix");;
+          $this->assertEmpty(strstr($mail,$string),"$string  incorrectly found in $mail $prefix");;
         }
         return $mail;
     }
@@ -121,6 +104,6 @@ class CiviMailService {
      */
     function assertMailLogEmpty($prefix = ''){
         $mail = file_get_contents(CIVICRM_MAIL_LOG);
-        $this->unitTest->assertEmpty($mail, 'mail sent when it should not have been ' . $prefix);
+        $this->assertEmpty($mail, 'mail sent when it should not have been ' . $prefix);
     }
 }
