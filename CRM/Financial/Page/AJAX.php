e4b72044f88998469c04343e131cd5af7348f4f9
Yashodha Chaku <yashodha.chaku@gmail.com>
2012-12-04 04:55:06 +0000

    more clean up

--- CRM/Financial/Page/AJAX.php
+++ CRM/Financial/Page/AJAX.php
@@ -37,169 +37,169 @@
 /**
  * This class contains all the function that are called using AJAX
  */
-class CRM_Financial_Page_AJAX
-{
-   
+class CRM_Financial_Page_AJAX {
 
-    /**
-     * Function for building Event Type combo box
-     */
+  /**
+   * Function for building Event Type combo box
+   */
   function financialAccount() {
-        require_once 'CRM/Utils/Type.php';
-        $name = trim( CRM_Utils_Type::escape( $_GET['s'], 'String' ) );
-        if( !$name ) {
-            $name = '%';
-        }
-        $whereClause = " f.name LIKE '$name%' ";
-        //if( CRM_Utils_Array::getValue( 'id',$_GET ) )
-        if( array_key_exists( 'parentID', $_GET ) ){
-            $parentID = $_GET['parentID'];
-            $whereClause .= " AND f.id = {$parentID} ";
-        }
-        $query ="
+    require_once 'CRM/Utils/Type.php';
+    $name = trim( CRM_Utils_Type::escape( $_GET['s'], 'String' ) );
+    if (!$name) {
+      $name = '%';
+    }
+    $whereClause = " f.name LIKE '$name%' ";
+
+    //if( CRM_Utils_Array::getValue( 'id',$_GET ) )
+    if (array_key_exists( 'parentID', $_GET )) {
+      $parentID = $_GET['parentID'];
+      $whereClause .= " AND f.id = {$parentID} ";
+    }
+
+    $query ="
 SELECT CONCAT_WS(' :: ', f.name, accounting_code) as name, f.id
 FROM   civicrm_financial_account as f
 WHERE  {$whereClause}
 ORDER by f.name";
-       
 
-        $dao = CRM_Core_DAO::executeQuery( $query );
-            while ( $dao->fetch( ) ) {
-                echo $elements = "$dao->name|$dao->id\n";
-            }
-            CRM_Utils_System::civiExit( );
+    $dao = CRM_Core_DAO::executeQuery( $query );
+    while ( $dao->fetch( ) ) {
+      echo $elements = "$dao->name|$dao->id\n";
     }
+    CRM_Utils_System::civiExit( );
+  }
 
-    /*
-     * Function to get finacial accounts of required account relationship
-     * $financialAccountType array with key account relationship and value financial account type option groups
-     *
-     */
-    function jqFinancial( $config ) {
-        if ( ! isset( $_GET['_value'] ) ||
-             empty( $_GET['_value'] ) ) {
-            CRM_Utils_System::civiExit( );
-        }
-        if( $_GET['_value'] == 'select'  ){
-            $result = CRM_Contribute_PseudoConstant::financialAccount( );        
-        } else {
+  /*
+   * Function to get finacial accounts of required account relationship
+   * $financialAccountType array with key account relationship and value financial account type option groups
+   *
+   */
+  function jqFinancial( $config ) {
+    if (!isset( $_GET['_value']) ||
+      empty( $_GET['_value'] ) ) {
+      CRM_Utils_System::civiExit( );
+    }
+    
+    if ($_GET['_value'] == 'select') {
+      $result = CRM_Contribute_PseudoConstant::financialAccount( );        
+    } else {
       $financialAccountType = array( 
         '5' => 5, //expense
-                                           '3' => 1, //AR relation
-                                           '1' => 3, //revenue
+        '3' => 1, //AR relation
+        '1' => 3, //revenue
         '6' => 1, // asset
         '7' => 4, //cost of sales
         '8' => 1, //premium inventory
         '9' => 3, //discount account is
-                                           );
- 
-            $financialAccountType = "financial_account_type_id = {$financialAccountType[$_GET['_value']]}";
+        );
+      
+      $financialAccountType = "financial_account_type_id = {$financialAccountType[$_GET['_value']]}";
       $result = CRM_Contribute_PseudoConstant::financialAccount(NULL, $financialAccountType);
-}
+    }
     $elements = array( 
       array( 
         'name'  => ts('- Select Financial Account -'),
         'value' => 'select' 
       )
     );
-        if( !empty( $result ) ){
-            foreach ( $result as $id => $name ) {
+    
+    if (!empty( $result )){
+      foreach ( $result as $id => $name ) {
         $elements[] = array( 
           'name'  => $name,
           'value' => $id 
-        );
-            }
-        }
-        echo json_encode( $elements );
-        CRM_Utils_System::civiExit( );
+         );
+      }
     }
-    
-    function jqFinancialRelation( $config ) {
-        if ( ! isset( $_GET['_value'] ) ||
-             empty( $_GET['_value'] ) ) {
-            CRM_Utils_System::civiExit( );
-        }  
-        if( $_GET['_value'] == 'select'  ){
-            $result = CRM_Core_PseudoConstant::accountOptionValues( 'account_relationship' );     
-        } else {
+    echo json_encode( $elements );
+    CRM_Utils_System::civiExit( );
+  }
+  
+  function jqFinancialRelation( $config ) {
+    if ( ! isset( $_GET['_value'] ) ||
+      empty( $_GET['_value'] ) ) {
+        CRM_Utils_System::civiExit( );
+    }  
+    if ($_GET['_value'] == 'select') {
+      $result = CRM_Core_PseudoConstant::accountOptionValues( 'account_relationship' );     
+    } else {
       $financialAccountType = array( 
         '5' => array(5), //expense
         '1' => array(3, 6, 8), //Asset
         '3' => array(1, 9), //revenue
         '4' => array(7), //cost of sales
-                                           );
-            $financialAccountTypeId = CRM_Core_DAO::getFieldValue( 'CRM_Financial_DAO_FinancialAccount', $_GET['_value'], 'financial_account_type_id' );
-            $result = CRM_Core_PseudoConstant::accountOptionValues( 'account_relationship' ); 
-        }
+      );
+      $financialAccountTypeId = CRM_Core_DAO::getFieldValue( 'CRM_Financial_DAO_FinancialAccount', $_GET['_value'], 'financial_account_type_id' );
+      $result = CRM_Core_PseudoConstant::accountOptionValues( 'account_relationship' ); 
+    }
     $elements = array( 
       array( 
         'name'  => ts('- Select Financial Account Relationship -'),
         'value' => 'select' 
         ) 
     );
-        $countResult = count( $financialAccountType[$financialAccountTypeId] );
-        if( !empty( $result ) ){
-            foreach ( $result as $id => $name ) {
-                if( in_array( $id, $financialAccountType[$financialAccountTypeId] )  && $_GET['_value'] != 'select' ){
-                    if ( $countResult != 1){
+    $countResult = count( $financialAccountType[$financialAccountTypeId] );
+    if (!empty( $result )) {
+      foreach ( $result as $id => $name ) {
+        if (in_array( $id, $financialAccountType[$financialAccountTypeId] )  && $_GET['_value'] != 'select') {
+          if ( $countResult != 1){
             $elements[] = array( 
               'name'  => $name,
               'value' => $id 
             );
-                    }else{
+          } else {
             $elements[] = array( 
               'name'     => $name,
-                                             'value'    => $id,
+              'value'    => $id,
               'selected' => 'Selected', 
             );
-                    }
-                }else if( $_GET['_value'] == 'select' ){
+          }
+        } elseif ( $_GET['_value'] == 'select' ){
           $elements[] = array( 
             'name'  => $name,
             'value' => $id 
           ); 
-                                             }
-            }
         }
-        echo json_encode( $elements );
+      }
+    }
+    echo json_encode( $elements );
+    CRM_Utils_System::civiExit( );
+  }
+  
+  function jqFinancialType( $config ) {
+    if ( ! isset( $_GET['_value'] ) ||
+      empty( $_GET['_value'] ) ) {
         CRM_Utils_System::civiExit( );
     }
- 
-    function jqFinancialType( $config ) {
-        if ( ! isset( $_GET['_value'] ) ||
-             empty( $_GET['_value'] ) ) {
-            CRM_Utils_System::civiExit( );
-}
-        require_once 'CRM/Core/DAO.php';
-        $elements = CRM_Core_DAO::getFieldValue( 'CRM_Contribute_DAO_Product', $_GET['_value'], 'financial_type_id' );
-        
-        require_once "CRM/Utils/JSON.php";
-        echo json_encode( $elements );
-        CRM_Utils_System::civiExit( );
-    }
+    require_once 'CRM/Core/DAO.php';
+    $elements = CRM_Core_DAO::getFieldValue( 'CRM_Contribute_DAO_Product', $_GET['_value'], 'financial_type_id' );
     
-    function getContactList( ) 
-    {
-        // if context is 'customfield' 
+    require_once "CRM/Utils/JSON.php";
+    echo json_encode( $elements );
+    CRM_Utils_System::civiExit( );
+  }
+  
+    function getContactList( ) {
+      // if context is 'customfield' 
         if ( CRM_Utils_Array::value( 'context', $_GET ) == 'customfield' ) {
-            return self::contactReference( );
-}
+          return self::contactReference( );
+        }
 
-        $name   = CRM_Utils_Array::value( 's', $_GET );
-        $name   = CRM_Utils_Type::escape( $name, 'String' );
+        $name  = CRM_Utils_Array::value( 's', $_GET );
+        $name  = CRM_Utils_Type::escape( $name, 'String' );
         
         $limit = 10;
         if ( CRM_Utils_Array::value( 'limit', $_GET) ) {
-            $limit = CRM_Utils_Type::escape( $_GET['limit'], 'Positive' );
+          $limit = CRM_Utils_Type::escape( $_GET['limit'], 'Positive' );
         }
 
         if ( CRM_Utils_Array::value( 'cid', $_GET) ) {
-            $cid = CRM_Utils_Type::escape( $_GET['cid'], 'Positive' );
+          $cid = CRM_Utils_Type::escape( $_GET['cid'], 'Positive' );
         }
 
         $userPerm = 0;
         if ( CRM_Utils_Array::value( 'users', $_GET) ) {
-            $userPerm = CRM_Utils_Type::escape( $_GET['users'], 'Integer' );
+          $userPerm = CRM_Utils_Type::escape( $_GET['users'], 'Integer' );
         }
         require_once 'DB.php';   
         $config = CRM_Core_Config::singleton( );
@@ -235,78 +235,75 @@ LIMIT    0, {$limit}
         
         $dao =CRM_Core_DAO::executeQuery( $query );
         while( $dao->fetch() ){
-            echo $contactList = "{$dao->data}|{$dao->id}\n";
-            
+          echo $contactList = "{$dao->data}|{$dao->id}\n";
         }
         CRM_Utils_System::civiExit( );
     } 
-
- /**
+    
+    /**
      * Function to perform remove/assign actions on record.
      *
      */
     static function assignRemove( ) {
-        $op        = CRM_Utils_Type::escape( $_POST['op'       ],  'String'   );
-        $recordID  = CRM_Utils_Type::escape( $_POST['recordID' ],  'Positive' );
-        $recordBAO = CRM_Utils_Type::escape( $_POST['recordBAO'],  'String'   );
+      $op        = CRM_Utils_Type::escape( $_POST['op'       ],  'String'   );
+      $recordID  = CRM_Utils_Type::escape( $_POST['recordID' ],  'Positive' );
+      $recordBAO = CRM_Utils_Type::escape( $_POST['recordBAO'],  'String'   );
+      
+      $entityID  = CRM_Utils_Array::value( 'entityID', $_POST );
+      $methods = array( 'assign' => 'add',
+                        'remove' => 'remove',
+                        'reopen' => 'create',
+                        'close'  => 'create' );
+      $params = array( );
+      $status = array( 'status' => 'record-updated-fail' );
+      if ( isset( $recordID ) ) { 
+        // first munge and clean the recordBAO and get rid of any non alpha numeric characters
+        $recordBAO = CRM_Utils_String::munge( $recordBAO );
+        $recordClass = explode( '_', $recordBAO );
+        $ids = null;
+        // make sure recordClass is in the CRM namespace and
+        // at least 3 levels deep
+        if ( $recordClass[0] == 'CRM' && count( $recordClass ) >= 3 ) {
+          require_once(str_replace('_', DIRECTORY_SEPARATOR, $recordBAO) . ".php");
+          switch ($op) {
+          case 'assign':
+          case 'remove': 
+            $params = array( 'entity_id' => $entityID,
+                             'entity_table' => 'civicrm_batch',
+                             'financial_item_id' => $recordID,
+                             );
+            break;
 
-        $entityID  = CRM_Utils_Array::value( 'entityID', $_POST );
-        $methods = array( 'assign' => 'add',
-                          'remove' => 'remove',
-                          'reopen' => 'create',
-                          'close'  => 'create' );
-        $params = array( );
-        $status = array( 'status' => 'record-updated-fail' );
-        if ( isset( $recordID ) ) { 
-            // first munge and clean the recordBAO and get rid of any non alpha numeric characters
-            $recordBAO = CRM_Utils_String::munge( $recordBAO );
-            $recordClass = explode( '_', $recordBAO );
-            $ids = null;
-            // make sure recordClass is in the CRM namespace and
-            // at least 3 levels deep
-            if ( $recordClass[0] == 'CRM' &&
-                 count( $recordClass ) >= 3 ) {
-                require_once(str_replace('_', DIRECTORY_SEPARATOR, $recordBAO) . ".php");
-                switch ($op){
-                case 'assign':
-                 
-                case 'remove': 
-                    $params = array( 'entity_id' => $entityID,
-                                     'entity_table' => 'civicrm_batch',
-                                     'financial_item_id' => $recordID,
-                                     );
-                    break;
-                case 'reopen': 
-                case 'close': 
-                    if( $op == 'close' ){
-                        $status = 'Closed';
-                    }
-                    else {
-                        $status = 'Open';
-                    }
-                    $ids['batchID'] = $recordID;
-                    $batchStatus = CRM_Core_PseudoConstant::accountOptionValues( 'batch_status' );
-                    $params['batch_status_id'] = CRM_Utils_Array::key( $status, $batchStatus );
-                    $session = CRM_Core_Session::singleton( );
-                    $params['modified_date'] = date('YmdHis');
-                    $params['modified_id'] = $session->get( 'userID' );
-                    break;
-                }
-                
-
-                if ( method_exists( $recordBAO, $methods[$op] ) ) {
-                    $updated = call_user_func_array( array( $recordBAO, $methods[$op] ),
-                                                     array( $params, $ids ) );
-                    if ( $updated ) {   
-                        $status = array( 'status' => 'record-updated-success' );
-                    }
-
-                    // // call hook enableDisable
-                    // CRM_Utils_Hook::enableDisable( $recordBAO, $recordID, $isActive );
-                }
+          case 'reopen': 
+          case 'close': 
+            if ( $op == 'close' ){
+              $status = 'Closed';
+            } else {
+              $status = 'Open';
             }
-            echo json_encode( $status );
-            CRM_Utils_System::civiExit( );
+            $ids['batchID'] = $recordID;
+            $batchStatus = CRM_Core_PseudoConstant::accountOptionValues( 'batch_status' );
+            $params['batch_status_id'] = CRM_Utils_Array::key( $status, $batchStatus );
+            $session = CRM_Core_Session::singleton( );
+            $params['modified_date'] = date('YmdHis');
+            $params['modified_id'] = $session->get( 'userID' );
+            break;
+
+          }
+          
+          if ( method_exists( $recordBAO, $methods[$op] ) ) {
+            $updated = call_user_func_array( array( $recordBAO, $methods[$op] ),
+              array( $params, $ids ) );
+            if ( $updated ) {   
+              $status = array( 'status' => 'record-updated-success' );
+            }
+            
+            // // call hook enableDisable
+            // CRM_Utils_Hook::enableDisable( $recordBAO, $recordID, $isActive );
+          }
         }
+        echo json_encode( $status );
+        CRM_Utils_System::civiExit( );
+      }
     }
 }
--- CRM/Financial/Page/FinancialAccount.php
+++ CRM/Financial/Page/FinancialAccount.php
@@ -39,168 +39,158 @@ require_once 'CRM/Core/Page/Basic.php';
 /**
  * Page for displaying list of financial types
  */
-class CRM_Financial_Page_FinancialAccount extends CRM_Core_Page_Basic 
-{
-    /**
-     * The action links that we need to display for the browse screen
-     *
-     * @var array
-     * @static
-     */
-    static $_links = null;
-
-    /**
-     * Get BAO Name
-     *
-     * @return string Classname of BAO.
-     */
-    function getBAOName() 
-    {
-        return 'CRM_Financial_BAO_FinancialAccount';
-    }
-
-    /**
-     * Get action Links
-     *
-     * @return array (reference) of action links
-     */
-    function &links()
-    {
-        if (!(self::$_links)) {
-            self::$_links = array(
-                                  CRM_Core_Action::UPDATE  => array(
-                                                                    'name'  => ts('Edit'),
-                                                                    'url'   => 'civicrm/admin/financial/financialAccount',
-                                                                    'qs'    => 'action=update&id=%%id%%&reset=1',
-                                                                    'title' => ts('Edit Financial Type') 
-                                                                   ),
-                                  CRM_Core_Action::DISABLE => array(
-                                                                    'name'  => ts('Disable'),
-                                                                    'extra' => 'onclick = "enableDisable( %%id%%,\''. 'CRM_Financial_BAO_FinancialAccount' . '\',\'' . 'enable-disable' . '\' );"',
-                                                                    'ref'   => 'disable-action',
-                                                                    
-                                                                    'title' => ts('Disable Financial Type') 
-                                                                   ),
-                                  CRM_Core_Action::ENABLE  => array(
-                                                                    'name'  => ts('Enable'),
-                                                                    'extra' => 'onclick = "enableDisable( %%id%%,\''. 'CRM_Financial_BAO_FinancialAccount' . '\',\'' . 'disable-enable' . '\' );"',
-                                                                    'ref'   => 'enable-action',
-                                                                    
-                                                                    'title' => ts('Enable Financial Type') 
-                                                                    ),
-                                  CRM_Core_Action::DELETE  => array(
-                                                                    'name'  => ts('Delete'),
-                                                                    'url'   => 'civicrm/admin/financial/financialAccount',
-                                                                    'qs'    => 'action=delete&id=%%id%%',
-                                                                    'title' => ts('Delete Financial Type') 
-                                                                   )
-                                 );
-        }
-        return self::$_links;
-    }
-
-    /**
-     * Run the page.
-     *
-     * This method is called after the page is created. It checks for the  
-     * type of action and executes that action.
-     * Finally it calls the parent's run method.
-     *
-     * @return void
-     * @access public
-     *
-     */
-    function run()
-    {
-        // get the requested action
-        $action = CRM_Utils_Request::retrieve('action', 'String',
-                                              $this, false, 'browse'); // default to 'browse'
-
-        // assign vars to templates
-        $this->assign('action', $action);
-        $id = CRM_Utils_Request::retrieve('id', 'Positive',
-                                          $this, false, 0);
-        
-        // what action to take ?
-        if ($action & (CRM_Core_Action::UPDATE | CRM_Core_Action::ADD)) {
-            $this->edit($action, $id) ;
-        } 
-
-        // parent run 
-        return parent::run();
-    }
-
-    /**
-     * Browse all custom data groups.
-     *  
-     * 
-     * @return void
-     * @access public
-     * @static
-     */
-    function browse()
-    {
-        // get all custom groups sorted by weight
-        $contributionType = array();
-        require_once 'CRM/Financial/DAO/FinancialAccount.php';
-        $dao = new CRM_Financial_DAO_FinancialAccount();
-        $dao->orderBy('name');
-        $dao->find();
-        $financialAccountType = CRM_Core_PseudoConstant::accountOptionValues('financial_account_type');
-
-        while ($dao->fetch()) {
-            $contributionType[$dao->id] = array();
-            CRM_Core_DAO::storeValues( $dao, $contributionType[$dao->id]);
-          $contributionType[$dao->id]['financial_account_type_id'] =  $financialAccountType[$dao->financial_account_type_id];
-            // form all action links
-            $action = array_sum(array_keys($this->links()));
-
-            // update enable/disable links depending on if it is is_reserved or is_active
-            if ($dao->is_reserved) {
-                continue;
-            } else {
-                if ($dao->is_active) {
-                    $action -= CRM_Core_Action::ENABLE;
-                } else {
-                    $action -= CRM_Core_Action::DISABLE;
-                }
-            }
-
-            $contributionType[$dao->id]['action'] = CRM_Core_Action::formLink(self::links(), $action, 
-                                                                                    array('id' => $dao->id));
-        }
-        $this->assign('rows', $contributionType);
-    }
-
-    /**
-     * Get name of edit form
-     *
-     * @return string Classname of edit form.
-     */
-    function editForm() 
-    {
-        return 'CRM_Financial_Form_FinancialAccount';
-    }
+class CRM_Financial_Page_FinancialAccount extends CRM_Core_Page_Basic {
+  /**
+   * The action links that we need to display for the browse screen
+   *
+   * @var array
+   * @static
+   */
+  static $_links = null;
     
-    /**
-     * Get edit form name
-     *
-     * @return string name of this page.
-     */
-    function editName() 
-    {
-        return 'Financial Types';
+  /**
+   * Get BAO Name
+   *
+   * @return string Classname of BAO.
+   */
+  function getBAOName() {
+    return 'CRM_Financial_BAO_FinancialAccount';
+  }
+  
+  /**
+   * Get action Links
+   *
+   * @return array (reference) of action links
+   */
+  function &links() {
+    if (!(self::$_links)) {
+      self::$_links = array(
+                            CRM_Core_Action::UPDATE  => array(
+                                                              'name'  => ts('Edit'),
+                                                              'url'   => 'civicrm/admin/financial/financialAccount',
+                                                              'qs'    => 'action=update&id=%%id%%&reset=1',
+                                                              'title' => ts('Edit Financial Type') 
+                                                              ),
+                            CRM_Core_Action::DISABLE => array(
+                                                              'name'  => ts('Disable'),
+                                                              'extra' => 'onclick = "enableDisable( %%id%%,\''. 'CRM_Financial_BAO_FinancialAccount' . '\',\'' . 'enable-disable' . '\' );"',
+                                                              'ref'   => 'disable-action',
+                                                              'title' => ts('Disable Financial Type') 
+                                                              ),
+                            CRM_Core_Action::ENABLE  => array(
+                                                              'name'  => ts('Enable'),
+                                                              'extra' => 'onclick = "enableDisable( %%id%%,\''. 'CRM_Financial_BAO_FinancialAccount' . '\',\'' . 'disable-enable' . '\' );"',
+                                                              'ref'   => 'enable-action',
+                                                              'title' => ts('Enable Financial Type') 
+                                                              ),
+                            CRM_Core_Action::DELETE  => array(
+                                                              'name'  => ts('Delete'),
+                                                              'url'   => 'civicrm/admin/financial/financialAccount',
+                                                              'qs'    => 'action=delete&id=%%id%%',
+                                                              'title' => ts('Delete Financial Type') 
+                                                              )
+                            );
     }
+    return self::$_links;
+  }
+  
+  /**
+   * Run the page.
+   *
+   * This method is called after the page is created. It checks for the  
+   * type of action and executes that action.
+   * Finally it calls the parent's run method.
+   *
+   * @return void
+   * @access public
+   *
+   */
+  function run() {
+    // get the requested action
+    $action = CRM_Utils_Request::retrieve('action', 'String',
+                                          $this, false, 'browse'); // default to 'browse'
     
-    /**
-     * Get user context.
-     *
-     * @return string user context.
-     */
-    function userContext($mode = null) 
-    {
-        return 'civicrm/admin/financial/financialAccount';
+    // assign vars to templates
+    $this->assign('action', $action);
+    $id = CRM_Utils_Request::retrieve('id', 'Positive',
+                                      $this, false, 0);
+    
+    // what action to take ?
+    if ($action & (CRM_Core_Action::UPDATE | CRM_Core_Action::ADD)) {
+      $this->edit($action, $id) ;
+    } 
+    
+    // parent run 
+    return parent::run();
+  }
+  
+  /**
+   * Browse all custom data groups.
+   *  
+   * 
+   * @return void
+   * @access public
+   * @static
+   */
+  function browse() {
+    // get all custom groups sorted by weight
+    $contributionType = array();
+    require_once 'CRM/Financial/DAO/FinancialAccount.php';
+    $dao = new CRM_Financial_DAO_FinancialAccount();
+    $dao->orderBy('name');
+    $dao->find();
+    $financialAccountType = CRM_Core_PseudoConstant::accountOptionValues('financial_account_type');
+    
+    while ($dao->fetch()) {
+      $contributionType[$dao->id] = array();
+      CRM_Core_DAO::storeValues( $dao, $contributionType[$dao->id]);
+      $contributionType[$dao->id]['financial_account_type_id'] =  $financialAccountType[$dao->financial_account_type_id];
+      // form all action links
+      $action = array_sum(array_keys($this->links()));
+      
+      // update enable/disable links depending on if it is is_reserved or is_active
+      if ($dao->is_reserved) {
+        continue;
+      } else {
+        if ($dao->is_active) {
+          $action -= CRM_Core_Action::ENABLE;
+        } else {
+          $action -= CRM_Core_Action::DISABLE;
+        }
+      }
+      
+      $contributionType[$dao->id]['action'] = CRM_Core_Action::formLink(self::links(), $action, 
+                                                                        array('id' => $dao->id));
     }
+    $this->assign('rows', $contributionType);
+  }
+  
+  /**
+   * Get name of edit form
+   *
+   * @return string Classname of edit form.
+   */
+  function editForm() {
+    return 'CRM_Financial_Form_FinancialAccount';
+  }
+  
+  /**
+   * Get edit form name
+   *
+   * @return string name of this page.
+   */
+  function editName() {
+    return 'Financial Types';
+  }
+  
+  /**
+   * Get user context.
+   *
+   * @return string user context.
+   */
+  function userContext($mode = null) {
+    return 'civicrm/admin/financial/financialAccount';
+  }
 }
 
 
