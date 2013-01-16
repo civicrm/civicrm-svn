3a4fa102206044f5e8ba2c7ebc0e193e6830bcf7
Dave Greenberg <dave@civicrm.org>
2013-01-16 18:07:58 +0000

    CRM-11678

--- CRM/Contribute/Form/ContributionView.php
+++ CRM/Contribute/Form/ContributionView.php
@@ -67,9 +67,12 @@ class CRM_Contribute_Form_ContributionView extends CRM_Core_Form {
 
     // get recieved into i.e to_financial_account_id from last trxn
     $financialTrxnId = CRM_Core_BAO_FinancialTrxn::getFinancialTrxnId($values['contribution_id'], 'civicrm_contribution', 'DESC');
+    $values['to_financial_account'] = '';
     if (CRM_Utils_Array::value('financialTrxnId', $financialTrxnId)) {
       $values['to_financial_account_id'] = CRM_Core_DAO::getFieldValue('CRM_Financial_DAO_FinancialTrxn', $financialTrxnId['financialTrxnId'], 'to_financial_account_id');   
-      $values['to_financial_account'] = CRM_Contribute_PseudoConstant::financialAccount($values['to_financial_account_id']);
+      if ($values['to_financial_account_id']) {
+        $values['to_financial_account'] = CRM_Contribute_PseudoConstant::financialAccount($values['to_financial_account_id']);        
+      }
     }
         
     if (CRM_Utils_Array::value('honor_contact_id', $values)) {
