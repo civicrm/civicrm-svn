d378f8f1e5657c470e00a392c4429a13e8fe940a
Donald Lobo <lobo@civicrm.org>
2013-02-01 00:10:52 +0000

    fix CRM-11675

--- CRM/Core/Controller.php
+++ CRM/Core/Controller.php
@@ -206,12 +206,20 @@ class CRM_Core_Controller extends HTML_QuickForm_Controller {
         $this->_print = CRM_Core_Smarty::PRINT_PDF;
       }
       elseif ($snippet == 4) {
+        // this is used to embed fragments of a form
         $this->_print = CRM_Core_Smarty::PRINT_NOFORM;
         self::$_template->assign('suppressForm', TRUE);
         $this->_generateQFKey = FALSE;
       }
       elseif ($snippet == 5) {
+        // this is used for popups and inlined ajax forms
         $this->_print = CRM_Core_Smarty::PRINT_NOFORM;
+      }
+      elseif ($snippet == 6) {
+        // this is used for tabs via the TabHeader protocol
+        // for some reason the tab header includes the form - still researching why
+        $this->_print = CRM_Core_Smarty::PRINT_NOFORM;
+        self::$_template->assign('suppressForm', TRUE);
       }
       else {
         $this->_print = CRM_Core_Smarty::PRINT_SNIPPET;
