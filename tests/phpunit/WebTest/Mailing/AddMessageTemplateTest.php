960843a03a758eaa61edf00cf8b4994569913ef4
Coleman Watts <coleman@civicrm.org>
2013-02-16 03:43:45 +0000

    Cleanup test

--- tests/phpunit/WebTest/Mailing/AddMessageTemplateTest.php
+++ tests/phpunit/WebTest/Mailing/AddMessageTemplateTest.php
@@ -33,16 +33,6 @@ class WebTest_Mailing_AddMessageTemplateTest extends CiviSeleniumTestCase {
   }
 
   function testTemplateAdd($useTokens = FALSE, $msgTitle = NULL) {
-    // This is the path where our testing install resides.
-    // The rest of URL is defined in CiviSeleniumTestCase base class, in
-    // class attributes.
-    $this->open($this->sboxPath);
-
-    // Logging in. Remember to wait for page to load. In most cases,
-    // you can rely on 30000 as the value that allows your test to pass, however,
-    // sometimes your test might fail because of this. In such cases, it's better to pick one element
-    // somewhere at the end of page and use waitForElementPresent on it - this assures you, that whole
-    // page contents loaded and you can continue your test execution.
     $this->webtestLogin();
 
     // Go directly to the URL of the screen that you will be testing (Add Message Template).
@@ -109,23 +99,8 @@ class WebTest_Mailing_AddMessageTemplateTest extends CiviSeleniumTestCase {
     $msgTitle = 'msg_' . substr(sha1(rand()), 0, 7);
     $this->testTemplateAdd(TRUE, $msgTitle);
 
-    // Go directly to the URL of the screen that you will be testing (New Group).
-    $this->open($this->sboxPath . "civicrm/group/add?reset=1");
-    $this->waitForElementPresent("_qf_Edit_upload");
-
     // create new mailing group
-    $groupName = 'group_' . substr(sha1(rand()), 0, 7);
-    $this->type("title", $groupName);
-    $this->type("description", "New mailing group for Webtest");
-    $this->click("group_type[2]");
-    $this->select("visibility", "value=Public Pages");
-
-    // Clicking save.
-    $this->click("_qf_Edit_upload");
-    $this->waitForPageToLoad("30000");
-
-    // Is status message correct?
-    $this->assertTrue($this->isTextPresent("The Group '$groupName' has been saved."));
+    $groupName = $this->WebtestAddGroup();
 
     //Create new contact and add to mailing Group
     $firstName = substr(sha1(rand()), 0, 7);
@@ -136,8 +111,7 @@ class WebTest_Mailing_AddMessageTemplateTest extends CiviSeleniumTestCase {
     $this->click("_qf_GroupContact_next");
 
     // configure default mail-box
-    $this->open($this->sboxPath . "civicrm/admin/mailSettings?action=update&id=1&reset=1");
-    $this->waitForElementPresent('_qf_MailSettings_cancel-bottom');
+    $this->openCiviPage("admin/mailSettings", "action=update&id=1&reset=1", '_qf_MailSettings_cancel-bottom');
     $this->type('name', 'Test Domain');
     $this->type('domain', 'example.com');
     $this->select('protocol', 'value=1');
@@ -145,8 +119,7 @@ class WebTest_Mailing_AddMessageTemplateTest extends CiviSeleniumTestCase {
     $this->waitForPageToLoad("30000");
 
     // Go directly to Schedule and Send Mailing form
-    $this->open($this->sboxPath . "civicrm/mailing/send?reset=1");
-    $this->waitForElementPresent("_qf_Group_cancel");
+    $this->openCiviPage("mailing/send", "reset=1", "_qf_Group_cancel");
 
     // fill mailing name
     $mailingName = substr(sha1(rand()), 0, 7);
@@ -203,15 +176,13 @@ class WebTest_Mailing_AddMessageTemplateTest extends CiviSeleniumTestCase {
     //check redirected page to Scheduled and Sent Mailings and  verify for mailing name
     $this->assertTrue($this->isTextPresent("Scheduled and Sent Mailings"));
     $this->assertTrue($this->isTextPresent("Mailing $mailingName Webtest"));
-    $this->open($this->sboxPath . "civicrm/mailing/queue?reset=1");
-    $this->waitForPageToLoad("300000");
+    $this->openCiviPage('mailing/queue', 'reset=1');
 
     // verify status
     $this->verifyText("xpath=id('Search')/table/tbody/tr[1]/td[2]", preg_quote("Complete"));
 
     //View Activity
-    $this->open($this->sboxPath . "civicrm/activity/search?reset=1");
-    $this->waitForElementPresent("_qf_Search_refresh");
+    $this->openCiviPage('activity/search', "reset=1", "_qf_Search_refresh");
     $this->type("sort_name", $firstName);
     $this->click("activity_type_id[19]");
     $this->click("_qf_Search_refresh");
