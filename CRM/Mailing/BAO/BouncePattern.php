9e675b9e96e1898f5414180ff1f2d5ca1c055582
Donald Lobo <lobo@civicrm.org>
2013-02-20 00:15:05 +0000

    fix formatting

--- CRM/Mailing/BAO/BouncePattern.php
+++ CRM/Mailing/BAO/BouncePattern.php
@@ -88,18 +88,16 @@ class CRM_Mailing_BAO_BouncePattern extends CRM_Mailing_DAO_BouncePattern {
     if (self::$_patterns == NULL) {
       self::buildPatterns();
     }
+
     foreach (self::$_patterns as $type => $re) {
       if (preg_match($re, $message, $matches)) {
         $bounce = array(
           'bounce_type_id' => $type,
           'bounce_reason' => $message,
         );
-
         return $bounce;
       }
     }
-
-
 
     $bounce = array(
       'bounce_type_id' => NULL,
