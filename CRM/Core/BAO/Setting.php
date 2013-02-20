eda6b0890f368983c3d448a6f0cd159edeb86b12
Eileen McNaughton <eileen@mcnaughty.com>
2013-02-20 01:19:16 +0000

    CRM-11927 - tweak settings api to work with chaining

--- CRM/Core/BAO/Setting.php
+++ CRM/Core/BAO/Setting.php
@@ -398,6 +398,10 @@ class CRM_Core_BAO_Setting extends CRM_Core_DAO_Setting {
       'component_id',
       'contact_id',
       'filters',
+      'entity_id',
+      'entity_table',
+      'sequential',
+      'api.has_parent',
     );
     $settingParams = array_diff_key($params, array_fill_keys($ignoredParams, TRUE));
     $getFieldsParams = array('version' => 3);
