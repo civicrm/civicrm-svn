57baad49d365b89a42399feaf1a29df41de2e37b
Deepak Srivastava <deepak.srivastava.0303@gmail.com>
2013-02-21 08:38:57 +0000

    - towards webtest fixes, do not merge

--- tests/phpunit/CRM/Contact/BAO/GroupContactTest.php
+++ tests/phpunit/CRM/Contact/BAO/GroupContactTest.php
@@ -83,7 +83,9 @@ class CRM_Contact_BAO_GroupContactTest extends CiviUnitTestCase {
     //check the group contact id is not null
     $this->assertNotNull($groupContact->id);
 
-    $this->assertEquals($groupContact->group_id, 1, 'Check for group_id');
+    $groupId = CRM_Core_DAO::singleValueQuery('select max(id) from civicrm_group');
+
+    $this->assertEquals($groupContact->group_id, $groupId, 'Check for group_id');
 
     //cleanup
     $groupContact->deleteTestObjects('CRM_Contact_DAO_GroupContact');
