a1caf00f0eecde8b9166c549b4fa71fcc4e169c4
Ravish Nair <ravish.nair@webaccess.co.in>
2013-02-26 13:47:27 +0000

    -- webtest fixes

--- tests/phpunit/WebTest/Campaign/OnlineContributionTest.php
+++ tests/phpunit/WebTest/Campaign/OnlineContributionTest.php
@@ -336,7 +336,7 @@ class WebTest_Campaign_OnlineContributionTest extends CiviSeleniumTestCase {
     $this->waitForElementPresent("_qf_ContributionView_cancel-bottom");
 
     //View Contribution Record
-    $this->verifyText("xpath=id('ContributionView')/div[2]/table[1]/tbody/tr[11]/td[2]", preg_quote($campaignTitle));
+    $this->verifyText("xpath=id('ContributionView')/div[2]/table[1]/tbody/tr[10]/td[2]", preg_quote($campaignTitle));
   }
 
   function _testVerifyRegisterPage($contributionPageTitle) {
